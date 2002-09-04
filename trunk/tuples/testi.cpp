e7dd93c1ddec97c35e1d5c59da93bf3263fff357
jajarvi <jajarvi@df6d15db-99ea-0310-92f0-9d27c566f14b>
2002-09-04 22:16:09 +0000

    sfl�d

--- trunk/tuples/testi.cpp
+++ trunk/tuples/testi.cpp
@@ -1,6 +1,98 @@
 #include <iostream>
 
-#include "boost/tuple/tuple.hpp"
+#include "tuple.h"
+
+// ----------------------------------------------------------------------------
+// helpers 
+// ----------------------------------------------------------------------------
+
+class A {}; 
+class B {}; 
+class C {};
+
+// classes with different kinds of conversions
+class AA {};
+class BB : public AA {}; 
+struct CC { CC() {} CC(const BB&) {} };
+struct DD { operator CC() const { return CC(); }; };
+
+// something to prevent warnings for unused variables
+//template<class T> void dummy(const T&) {}
+
+// no public default constructor
+class foo {
+public:
+  explicit foo(int v) : val(v) {}
+
+  bool operator==(const foo& other) const  {
+    return val == other.val;
+  }
+
+private:
+  foo() {}
+  int val;
+};
+
+// another class without a public default constructor
+class no_def_constructor {
+  no_def_constructor() {}
+public:
+  no_def_constructor(std::string) {}
+};
+
+// A non-copyable class 
+class no_copy {
+  no_copy(const no_copy&) {}
+public:
+  no_copy() {};
+};
+
+// ----------------------------------------------------------------------------
+// Testing different element types --------------------------------------------
+// ----------------------------------------------------------------------------
+
+using std::tuple;
+
+typedef tuple<int> t1;
+
+typedef tuple<double&, const double&, const double, double*, const double*> t2;
+typedef tuple<A, int(*)(char, int), C> t3;
+typedef tuple<std::string, std::pair<A, B> > t4;
+typedef tuple<A*, tuple<const A*, const B&, C>, bool, void*> t5;
+typedef tuple<volatile int, const volatile char&, int(&)(float) > t6;
+
+typedef tuple<B(A::*)(C&), A&> t7;
+
+typedef int(a_function_type)();
+typedef void(b_function_type)(int, float);
+
+// An object of this type of tuple cannot be created but the type can
+typedef tuple<a_function_type, b_function_type, void, char[10]> t8;
+
+
+// -----------------------------------------------------------------------
+// -tuple construction tests ---------------------------------------------
+// -----------------------------------------------------------------------
+void test_constructors() {
+
+  no_copy y;
+  tuple<no_copy&> x = tuple<no_copy&>(y); // ok
+
+  char cs[10];
+  tuple<char(&)[10]> v2(cs);  // ok
+
+};
+
+int main() {
+  test_constructors();
+}
+
+
+
+
+// ----------------------------------------------------------
+
+#if 0
 
 class A {
 public:
@@ -49,7 +141,7 @@ boost::tuple<const int> i;
 }
 
 
-
+#endif
 
 
 
