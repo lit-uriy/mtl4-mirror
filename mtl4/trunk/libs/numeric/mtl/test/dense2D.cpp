a7478ad83ae116e9b99cc806c67da2e37c9fc55f
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2005-07-07 22:46:56 +0000

    overwriting matrix values

--- mtl4/trunk/libs/numeric/mtl/test/dense2D.cpp
+++ mtl4/trunk/libs/numeric/mtl/test/dense2D.cpp
@@ -11,8 +11,17 @@ int main(int argc, char** argv) {
   double val[] = {1., 2., 3., 4., 5., 6.};
   dense2D<double, col_major> matrix(dim_type(2, 3), val, val+6);
 
-  for (dense_el_cursor<double> cursor = matrix.ebegin(); cursor != matrix.eend(); ++cursor)
+  dense_el_cursor<double> cursor= matrix.ebegin(), end= matrix.eend();
+
+  for (; cursor != end; ++cursor)
     cout << "matrix[" << row(matrix, *cursor) << ", " << col(matrix, *cursor)
 	 << "] = " << value(matrix, *cursor) << endl;
+
+  cursor= matrix.ebegin();
+  value(matrix, *cursor, 7.0);
+  for (; cursor != end; ++cursor)
+    cout << "matrix[" << row(matrix, *cursor) << ", " << col(matrix, *cursor)
+	 << "] = " << value(matrix, *cursor) << endl;
+
   return 0;
 } 
