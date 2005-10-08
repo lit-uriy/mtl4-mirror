91337d9bd8d301007e856d1077a0583e8aea02b1
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2005-10-08 23:35:13 +0000

    Magma checkable

--- mtl4/trunk/libs/numeric/mtl/test/scalar/age_example1.cpp
+++ mtl4/trunk/libs/numeric/mtl/test/scalar/age_example1.cpp
@@ -17,20 +17,29 @@ public:
 
     double sayAge() const 
     {
-	return myAge; 
+	return my_age; 
     }
 
-    age operator+(age y)
+    age operator+(age const y) const
     {
 	return my_age + y.my_age;
     }
-    
-    bool operator==(age y)
+
+    bool operator==(age const y) const
     {
 	return my_age == y.my_age;
     }
+    
+    bool operator!=(age const y) const
+    {
+	return my_age != y.my_age;
+    }
+
 };
 
+//namespace std { 
+//  template<> struct concept EqualityComparable<age> {};
+//}
 
 inline std::ostream& operator<< (std::ostream& stream, const age& a) 
 {
@@ -44,14 +53,14 @@ int main(int, char* [])
     age a0(0.0), a2(2.0), a3(3.0), a4(4.0), a5(5.0);
     glas::add<age>     ageAdd;
   
-    std::cout << "equalResults(a2,a5,  a3,a4, ageAdd) " 
-	      << equalResults(a2,a5,  a3,a4, ageAdd)  << std::endl;
-    std::cout << "equalResults(a2,a4,  a3,a4, ageAdd) " 
-	      << equalResults(a2,a4,  a3,a4, ageAdd)  << std::endl;
-    std::cout << "identityPair(a2,a4, ageAdd) " 
-	      << identityPair(a2,a4, ageAdd)  << std::endl;
-    std::cout << "identityPair(a0,a0, ageAdd) " 
-	      << identityPair(a0,a0, ageAdd)  << std::endl;
+    std::cout << "equal_results(a2,a5,  a3,a4, ageAdd) " 
+	      << mtl::equal_results(a2,a5,  a3,a4, ageAdd)  << std::endl;
+    std::cout << "equal_results(a2,a4,  a3,a4, ageAdd) " 
+	      << mtl::equal_results(a2,a4,  a3,a4, ageAdd)  << std::endl;
+//     std::cout << "identityPair(a2,a4, ageAdd) " 
+// 	      << mtl::identityPair(a2,a4, ageAdd)  << std::endl;
+//     std::cout << "identityPair(a0,a0, ageAdd) " 
+// 	      << mtl::identityPair(a0,a0, ageAdd)  << std::endl;
     return 0;
 }
 
