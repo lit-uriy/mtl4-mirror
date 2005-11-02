c1a55867ec1e90b8bb1fa50ab398ea6cd85b283b
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2005-11-02 21:04:02 +0000

    before removing bin_op etc

--- mtl4/trunk/libs/numeric/mtl/test/dilated_int.cpp
+++ mtl4/trunk/libs/numeric/mtl/test/dilated_int.cpp
@@ -8,28 +8,48 @@
 #include <boost/numeric/mtl/detail/dilated_int.hpp>
 
 using namespace mtl;
-using namespace mtl::dilated;
+// using namespace mtl::dilated;
 using namespace std;
 
-const unsigned EvenBits = 0x55555555;
-const unsigned OddBits = 0xaaaaaaaa;
- 
-#define EvenPlus1(j)     (((j) -EvenBits)&EvenBits)
-#define OddPlus1(j)      (((j) -OddBits)&OddBits)
-#define AntiEvenPlus1(j)    (((j) - 0xffffffff) | OddBits)
-#define AntiOddPlus1(j)     (((j) - 0xffffffff) | EvenBits)
-
 struct morton_exception {};
+
+template <typename T>
+void test_inc(string s, T dil, typename T::value_type exp_increment) 
+{ 
+    printf("%s %x, bit mask is %x, negated mask is %x\n", s.c_str(), dil.i, dil.bit_mask, dil.anti_mask);
+    if ((++dil).i != exp_increment) throw morton_exception();
+ 
+    for (typename T::value_type i = 1; i < 6; ++i) {
+	T dilated(i);
+	if (dilated.undilate() != i) throw morton_exception();
+
+	cout << "dilated(" << i << ") = " << dilated <<  "\n";
+	if (dil.i != dilated.i) throw morton_exception();
+	// check both pre and post increment
+	i & 1 ? ++dil : dil++;
+    }
+    
+    printf("dilated_zero = %x, dilated_one = %x\n", dil.dilated_zero, dil.dilated_one);
+
+    T dec_dil(6);
+    for (typename T::value_type i = 6; i > 0; --i) {
+	cout << "dilated(" << i << ") = " << dec_dil <<  "\n";
+	if (dec_dil != T(i)) throw morton_exception();
+
+	// check both pre and post decrement
+	i & 1 ? --dec_dil : dec_dil--;
+    }	
+}
 
 template <typename T>
 void test_plus1(T)
 {
     T a(1), b(3), c(4);
     cout << "a = " << a << ", b = " << b << ", a + b = " << a + b << ", c = " << c << "\n";
-    if ((a + b).i != c.i) throw morton_exception();
+    if (a + b != c) throw morton_exception();
 
     cout << "c - b = " << c - b << "\n";
-    if ((c - b).i != a.i) throw morton_exception();
+    if (c - b != a) throw morton_exception();
 }  
  
 template <typename T> 
@@ -37,43 +57,32 @@ void test_plus2(T)
 {
     T a(22), b(33), c(55);
     cout << "a = " << a << ", b = " << b << ", a + b = " << a + b << ", c = " << c << "\n";
-    if ((a + b).i != c.i) throw morton_exception();
+    if (a + b != c) throw morton_exception();
 
     cout << "c - b = " << c - b << "\n";
-    if ((c - b).i != a.i) throw morton_exception();
+    if (c - b != a) throw morton_exception();
 }    
 
 template <typename T>
-void test_dilated(string s, T dil1, typename T::value_type exp_increment) 
+void test_dilated(string s, T dil, typename T::value_type exp_increment) 
 {
-    printf("%s %x, bit mask is %x, negated mask is %x\n", s.c_str(), dil1.i, dil1.bit_mask, dil1.anti_mask);
-    printf("%x\n", (++dil1).i);
-    if (dil1.i != exp_increment) throw morton_exception();
-
-    for (int i = 1; i < 6; ++i) {
-	T dilated(i);
-	cout << "dilated(" << i << ") = " << dilated <<  "\n";
-	if (dil1.i != dilated.i) throw morton_exception();
-	++dil1;
-    }
-    test_plus1(dil1); test_plus2(dil1);
+    test_inc(s, dil, exp_increment);
+    test_plus1(dil); test_plus2(dil);
 }
- 
 
 
 int test_main(int argc, char* argv[])
-{
-    
-    dilated::dilated_int<unsigned, odd_bits<unsigned>::value, true>     dil1;
+{    
+    dilated_int<unsigned, dilated::odd_bits<unsigned>::value, true>     dil1;
     test_dilated( "Odd normalized", dil1, 2);
 
-    dilated::dilated_int<unsigned, even_bits<unsigned>::value, true>     dil2;
+    dilated_int<unsigned, dilated::even_bits<unsigned>::value, true>     dil2;
     test_dilated( "Even normalized", dil2, 1);
 
-    dilated::dilated_int<unsigned, odd_bits<unsigned>::value, false>     dil3;
+    dilated_int<unsigned, dilated::odd_bits<unsigned>::value, false>     dil3;
     test_dilated( "Odd anti-normalized", dil3, 0x55555557);
 
-    dilated::dilated_int<unsigned, even_bits<unsigned>::value, false>     dil4;
+    dilated_int<unsigned, dilated::even_bits<unsigned>::value, false>     dil4;
     test_dilated( "Even anti-normalized", dil4, 0xaaaaaaab);
 
     return 0;
