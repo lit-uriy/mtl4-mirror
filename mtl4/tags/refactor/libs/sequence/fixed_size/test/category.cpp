4142a75315771c4783e9a4d059b0a0cbea1ad98f
djowel <djowel@df6d15db-99ea-0310-92f0-9d27c566f14b>
2006-01-18 06:53:43 +0000

    initial refactoring

--- mtl4/tags/refactor/libs/sequence/fixed_size/test/category.cpp
+++ mtl4/tags/refactor/libs/sequence/fixed_size/test/category.cpp
@@ -3,7 +3,7 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 #include <boost/mpl/assert.hpp>
 #include <boost/type_traits/is_same.hpp>
-#include <boost/sequence/category.hpp>
+#include <boost/sequence/operations/category.hpp>
 
 BOOST_MPL_ASSERT(
     (boost::is_same<
