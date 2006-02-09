a2358a012b86488aa27513eb55a9cc5ee38d63fb
djowel <djowel@df6d15db-99ea-0310-92f0-9d27c566f14b>
2006-02-09 02:01:13 +0000

    added module headers

---
+++ trunk/mtl4/trunk/boost/sequence/view.hpp
@@ -0,0 +1,9 @@
+// Copyright David Abrahams 2005. Distributed under the Boost
+// Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#ifndef BOOST_SEQUENCE_VIEW_JDG20060209_HPP
+# define BOOST_SEQUENCE_VIEW_JDG20060209_HPP
+
+# include <boost/sequence/view/range.hpp>
+
+#endif // BOOST_SEQUENCE_INTRINSIC_JDG20060209_HPP
