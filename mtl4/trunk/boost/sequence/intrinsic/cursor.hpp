ad5cb5d8f1e79ba8b78b25291fc12cc50f317ad6
djowel <djowel@df6d15db-99ea-0310-92f0-9d27c566f14b>
2006-02-09 02:01:13 +0000

    added module headers

---
+++ mtl4/trunk/boost/sequence/intrinsic/cursor.hpp
@@ -0,0 +1,12 @@
+// Copyright David Abrahams 2005. Distributed under the Boost
+// Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#ifndef BOOST_SEQUENCE_INTRINSIC_CURSOR_JDG20060209_HPP
+# define BOOST_SEQUENCE_INTRINSIC_CURSOR_JDG20060209_HPP
+
+# include <boost/sequence/intrinsic/cursor/advance.hpp>
+# include <boost/sequence/intrinsic/cursor/distance.hpp>
+# include <boost/sequence/intrinsic/cursor/next.hpp>
+# include <boost/sequence/intrinsic/cursor/prev.hpp>
+
+#endif // BOOST_SEQUENCE_INTRINSIC_CURSOR_JDG20060209_HPP
