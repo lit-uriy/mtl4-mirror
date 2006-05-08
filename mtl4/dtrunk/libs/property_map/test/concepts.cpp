642cf727bd1d05340ea7992fcdf25edc5dadfd05
abrahams <abrahams@df6d15db-99ea-0310-92f0-9d27c566f14b>
2006-05-08 05:33:00 +0000

    Improve namespace utilitization.  Build tests only on demand from top level.

--- mtl4/dtrunk/libs/property_map/test/concepts.cpp
+++ mtl4/dtrunk/libs/property_map/test/concepts.cpp
@@ -4,49 +4,51 @@
 
 #include <list>
 #include <vector>
-#include <boost/property_map/concept.hpp>
+#include <boost/property_map/concepts.hpp>
 #include <boost/property_map/identity.hpp>
 #include <boost/property_map/dereference.hpp>
 #include <boost/iterator/counting_iterator.hpp>
 
 int main()
 {
-    namespace pm = boost::property_map;
+    using namespace boost::property_map::concepts;
+    using boost::property_map::identity;
+    using boost::property_map::dereference;
     
-    BOOST_CONCEPT_ASSERT((pm::ReadablePropertyMap<pm::identity,int*>));
-    BOOST_CONCEPT_ASSERT((pm::ReadablePropertyMap<pm::identity,int const*>));
-    BOOST_CONCEPT_ASSERT((pm::WritablePropertyMap<pm::identity,int*>));
-    BOOST_CONCEPT_ASSERT((pm::ReadWritePropertyMap<pm::identity,int*>));
+    BOOST_CONCEPT_ASSERT((ReadablePropertyMap<identity,int*>));
+    BOOST_CONCEPT_ASSERT((ReadablePropertyMap<identity,int const*>));
+    BOOST_CONCEPT_ASSERT((WritablePropertyMap<identity,int*>));
+    BOOST_CONCEPT_ASSERT((ReadWritePropertyMap<identity,int*>));
 #if CONCEPT_FAIL == 1
-    BOOST_CONCEPT_ASSERT((pm::WritablePropertyMap<pm::identity,int const*>));
+    BOOST_CONCEPT_ASSERT((WritablePropertyMap<identity,int const*>));
 #elif CONCEPT_FAIL == 2 
-    BOOST_CONCEPT_ASSERT((pm::ReadWritePropertyMap<pm::identity,int const*>));
+    BOOST_CONCEPT_ASSERT((ReadWritePropertyMap<identity,int const*>));
 #endif 
     
     typedef std::list<int>::iterator li;
     typedef std::list<int>::const_iterator cli;
-    BOOST_CONCEPT_ASSERT((pm::ReadablePropertyMap<pm::identity, li>));
-    BOOST_CONCEPT_ASSERT((pm::ReadablePropertyMap<pm::identity, cli>));
-    BOOST_CONCEPT_ASSERT((pm::WritablePropertyMap<pm::identity, li>));
-    BOOST_CONCEPT_ASSERT((pm::ReadWritePropertyMap<pm::identity, li>));
+    BOOST_CONCEPT_ASSERT((ReadablePropertyMap<identity, li>));
+    BOOST_CONCEPT_ASSERT((ReadablePropertyMap<identity, cli>));
+    BOOST_CONCEPT_ASSERT((WritablePropertyMap<identity, li>));
+    BOOST_CONCEPT_ASSERT((ReadWritePropertyMap<identity, li>));
 #if CONCEPT_FAIL == 3
-    BOOST_CONCEPT_ASSERT((pm::WritablePropertyMap<pm::identity, cli>));
+    BOOST_CONCEPT_ASSERT((WritablePropertyMap<identity, cli>));
 #elif CONCEPT_FAIL == 4 
-    BOOST_CONCEPT_ASSERT((pm::ReadWritePropertyMap<pm::identity, cli>));
+    BOOST_CONCEPT_ASSERT((ReadWritePropertyMap<identity, cli>));
 #endif 
 
     typedef boost::counting_iterator<li> li2;
     typedef boost::counting_iterator<cli> cli2;
     
-    BOOST_CONCEPT_ASSERT((pm::ReadablePropertyMap<pm::dereference, li2>));
-    BOOST_CONCEPT_ASSERT((pm::ReadablePropertyMap<pm::dereference, cli2>));
-    BOOST_CONCEPT_ASSERT((pm::WritablePropertyMap<pm::dereference, li2, int>));
-    BOOST_CONCEPT_ASSERT((pm::ReadWritePropertyMap<pm::dereference, li2>));
+    BOOST_CONCEPT_ASSERT((ReadablePropertyMap<dereference, li2>));
+    BOOST_CONCEPT_ASSERT((ReadablePropertyMap<dereference, cli2>));
+    BOOST_CONCEPT_ASSERT((WritablePropertyMap<dereference, li2, int>));
+    BOOST_CONCEPT_ASSERT((ReadWritePropertyMap<dereference, li2>));
     
 #if CONCEPT_FAIL == 5
-    BOOST_CONCEPT_ASSERT((pm::ReadablePropertyMap<pm::dereference,int*>));
+    BOOST_CONCEPT_ASSERT((ReadablePropertyMap<dereference,int*>));
 #endif 
-    BOOST_CONCEPT_ASSERT((pm::ReadablePropertyMap<pm::identity,std::vector<bool>::const_iterator>));
-    BOOST_CONCEPT_ASSERT((pm::ReadWritePropertyMap<pm::identity,std::vector<bool>::iterator>));
-    BOOST_CONCEPT_ASSERT((pm::WritablePropertyMap<pm::identity,std::vector<bool>::iterator>));
+    BOOST_CONCEPT_ASSERT((ReadablePropertyMap<identity,std::vector<bool>::const_iterator>));
+    BOOST_CONCEPT_ASSERT((ReadWritePropertyMap<identity,std::vector<bool>::iterator>));
+    BOOST_CONCEPT_ASSERT((WritablePropertyMap<identity,std::vector<bool>::iterator>));
 }
--- mtl4/dtrunk/libs/sequence/test/concepts.cpp
+++ mtl4/dtrunk/libs/sequence/test/concepts.cpp
@@ -1,28 +1,28 @@
 // Copyright David Abrahams 2006. Distributed under the Boost
 // Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-#include <boost/sequence/concept.hpp>
+#include <boost/sequence/concepts.hpp>
 #include <list>
 #include <vector>
 
 int main()
 {
-    namespace seq = boost::sequence;
+    using namespace boost::sequence::concepts;
 
-    BOOST_CONCEPT_ASSERT((seq::LvalueSequence<int const[4]>));
-    BOOST_CONCEPT_ASSERT((seq::LvalueSequence<int[4]>));
+    BOOST_CONCEPT_ASSERT((LvalueSequence<int const[4]>));
+    BOOST_CONCEPT_ASSERT((LvalueSequence<int[4]>));
     
-    BOOST_CONCEPT_ASSERT((seq::RandomAccessSequence<int const[4]>));
-    BOOST_CONCEPT_ASSERT((seq::Mutable_RandomAccessSequence<int[4]>));
+    BOOST_CONCEPT_ASSERT((RandomAccessSequence<int const[4]>));
+    BOOST_CONCEPT_ASSERT((Mutable_RandomAccessSequence<int[4]>));
     
-    BOOST_CONCEPT_ASSERT((seq::BidirectionalSequence<std::list<int> const>));
-    BOOST_CONCEPT_ASSERT((seq::Mutable_BidirectionalSequence<std::list<int> >));
+    BOOST_CONCEPT_ASSERT((BidirectionalSequence<std::list<int> const>));
+    BOOST_CONCEPT_ASSERT((Mutable_BidirectionalSequence<std::list<int> >));
 
-    BOOST_CONCEPT_ASSERT((seq::BidirectionalSequence<std::vector<int> const>));
-    BOOST_CONCEPT_ASSERT((seq::RandomAccessSequence<std::vector<int> const>));
-    BOOST_CONCEPT_ASSERT((seq::Mutable_RandomAccessSequence<std::vector<int> >));
+    BOOST_CONCEPT_ASSERT((BidirectionalSequence<std::vector<int> const>));
+    BOOST_CONCEPT_ASSERT((RandomAccessSequence<std::vector<int> const>));
+    BOOST_CONCEPT_ASSERT((Mutable_RandomAccessSequence<std::vector<int> >));
 
     // These tests verify that proxy references work
-    BOOST_CONCEPT_ASSERT((seq::RandomAccessSequence<std::vector<bool> const>));
-    BOOST_CONCEPT_ASSERT((seq::Mutable_RandomAccessSequence<std::vector<bool> >));
+    BOOST_CONCEPT_ASSERT((RandomAccessSequence<std::vector<bool> const>));
+    BOOST_CONCEPT_ASSERT((Mutable_RandomAccessSequence<std::vector<bool> >));
 }
