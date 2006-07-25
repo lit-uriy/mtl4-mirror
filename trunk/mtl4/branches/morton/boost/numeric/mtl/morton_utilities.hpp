8ef762adfb575c9df2ac8fec7690b796611f810e
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2006-07-25 23:31:39 +0000

    helper function for subdivision

---
+++ trunk/mtl4/branches/morton/boost/numeric/mtl/morton_utilities.hpp
@@ -0,0 +1,28 @@
+// $COPYRIGHT$
+
+#ifndef MTL_MORTON_UTILITIES_INCLUDE
+#define MTL_MORTON_UTILITIES_INCLUDE
+
+#include <limits>
+
+namespace mtl { namespace morton {
+
+// Splits a number into a next-smallest power of 2 and rest
+std::size_t inline first_part(std::size_t n)
+{
+    std::size_t  i= numeric_limits<std::size_t>::max()/2 + 1;
+
+    while(i > n) i>>= 1;
+    return i;
+}
+
+// The remainder of first part
+std::size_t inline second_part(std::size_t n)
+{
+    return n - first_part(n);
+}
+
+
+}} // namespace mtl::morton
+
+#endif // MTL_MORTON_UTILITIES_INCLUDE
