9343bf156d84ae37f604e7bcb5c64a54d8350077
lvoufo <lvoufo@df6d15db-99ea-0310-92f0-9d27c566f14b>
2006-08-28 06:33:28 +0000

    implemented itl test for gmres_quadtree.h

--- mtl4/branches/morton/quad_tree/ilu/itl_test.cpp
+++ mtl4/branches/morton/quad_tree/ilu/itl_test.cpp
@@ -2,31 +2,85 @@
 #include <vector>
 #include <numeric>
 #include <iterator>
-#include "op_env_setup.hpp"
-#include "matrix.h"
-#include "sysSolver.h" 
+#include "itl_interface.hpp"    //for 
+//#include "LU.h"
 
-#include "itl_interface.hpp"
+using namespace mtl;
+
+typedef  double Type;
 
 
-using namespace std;
-
-int main(int argc, char** argv) {
+int main(int argc, char** argv) 
+{
 
   cout << "start\n";
-  //  Matrix A;
-  setup_env(mat);
+  
+  //  Matrix mat;
+  env_init(argc, argv);
 
   mat.printRowMaj(mat.mainNode);
+/*
+  vector<double>   x(10), b(10);
+  fill(b.begin(), b.end(), 3.0);
 
-  vector<double>   x(10), y(10);
-  fill(x.begin(), x.end(), 3.0);
-
-  cout << "x: ";
-  copy(x.begin(), x.end(), ostream_iterator<double>(cout, " "));
-  cout << endl; 
-
+  cout<<"b: ";
+  copy( b.begin(), b.end(), ostream_iterator<double>(cout, " ") );
+  cout<<endl; 
+*/
+  // -- apply gmres --  
+  int max_iter = 50;
   
+  dense1D<Type> x(mat.getRows(), Type(0));
+  dense1D<Type> b(mat.getCols());
+  for (dense1D<Type>::iterator i=b.begin(); i!=b.end(); i++)
+    *i = 1.;
+  
+	//LUD
+	mat.copyMatrix(&(mat.backupNode), mat.mainNode, LEVEL_START);
+  LUD( &(mat.mainNode),
+				MTN_START, BND_PART_ALL, BND_PART_ALL , LEVEL_START );
+  
+  dense1D<Type> b2(mat.getCols());
+	solve( LUmat, b, b2);
+	
+	//iteration
+  noisy_iteration<double> iter(b2, max_iter, 1e-6);
+  int restart = 10;   //restart 10 times
+  
+  typedef dense1D<Type> Vec;
+  classical_gram_schmidt<Vec> orth(restart, size(x));
+  
+  //gmres algorithm
+     //L = left preconditioner & U= right preconditionner
+  //gmres(mat.backupNode, x, b, mat.mainNode, mat.mainNode, restart, iter, orth);
+     // left conditiooner = M = L*U & right preconditinner = I
+  gmres(mat.backupNode, x, b, mat.mainNode,  restart, iter, orth);
+  
+  //verify the result
+  dense1D<Type> b1(A.ncols());
+  mult(A, x, b1);
+  itl::add(itl::scaled(b, -1.), b1);
 
+  if ( itl::two_norm(b1) < 1.e-6) { //output
+    for (int i=0; i<5; i++) {
+      for (int j=0; j<5; j++) {
+	cout.width(6);
+	//!!! it is not recommended to use A(i,j) for large sparse matrices
+	cout << A(i, j) << " ";
+      }
+      cout << "  x  ";
+      cout.width(16);
+      cout << x[i] << "  =  ";
+      cout.width(6);
+      cout << b[i] << endl;
+    }
+  } else {
+    cout << "Residual " << iter.resid() << endl;
+  }
+  
+  //kill matrix environment
+  env_kill();
+  
   return 0;
 }
+
--- mtl4/branches/morton/quad_tree/ilu/sysSolver.h
+++ mtl4/branches/morton/quad_tree/ilu/sysSolver.h
@@ -1,78 +1,91 @@
 /*****************************************************************************
-  file: sysSolver.h
+  file: sysSolver.cpp
   -----------------
   Solving system of linear equations: Ax=b
   	--> incomplete LU +
   		fixed point iteration w/ forward and backward substitutions
+
+  contains Main function
 
   Revised on: 07/27/06
 
   D. S. Wise
   Larisse D. Voufo
 *****************************************************************************/
-#ifndef SYSSOLVER_H
-#define SYSSOLVER_H
+#ifndef SYSSOLVE_H
+#define SYSSOLVE_H
 
+#include "env_setup.h"
+#include "LU.h"
 #include "sysSolve.h"
 
+/*
+void solveSys()
+	Solves the system of linear equations:
+	- backs up a copy of the original matrix before performing the incomplete
+		LU decomposition since this is done destructively.
+	- Then, calls the iterative solver...
+	- Finally, checks the result and print out the number of iterations needed
+		to find the solution.
+	Timing is performed only during inc LUD and iteration.
+*/
+static void solveSys()
+{
+	//prepare the array that will hold the final result
+	dataType* result = (dataType*)(valloc(mat.getRows() * sizeof(dataType)));
+	int iter;
 
+	//save a copy of original matrix
+	mat.copyMatrix(&(mat.backupNode), mat.mainNode, LEVEL_START);
 
-typedef struct f{
-	char* name;
-	char* description;
-} flag;			// use for flags and their descriptions.
-				// --> helps when printing help.
+	//print input
+	if(printInput){
+		printf("\nMatrix:\n");
+		printMatrix(mat.mainNode, printINformat);
+		printf("\n");
 
-//flags and brief descriptions - necessary for printing Help
-flag flags[] = {
-	{BASE_ORDER_LG,	"Base Order Log. (default is 3)."},
-	{DAMP_FACT,	"Damping Factor for iterative solver."},
-	{PRINT_INPUT,	"print out input and Specify format. (default: unset)"},
-	{PRINT_OUTPUT,	"print out rhs w/ format= no_format. (default: unset)"},
-	{PRINT_ALL,		"print out input and rhs. (default: unset)"},
-	};
-const int nFlags = 5;
+		printf("\nRhs:\n");
+		printArray(mat.rhs, mat.getRows() );
+		printf("\n");
+  }
 
-//formats and brief descriptions - necessary for printing Help
-flag formats[] = {	{ROW_MAJ_Z,		"Row-Major Order: zero values included"},
-					{TREE_Z,		"Tree rep.: zero values included"},
-					{HB,			"Harwell-Boeing Format - unsymmetric"},
-					{HBS,			"Harwell-Boeing Format - symmetric"},
-					{NO_FORMAT,		"unspecified format"},
-					};
-const int nFormats = 5;
+	timestamp_start("");
+		//LU Decomposition with pivoting
+mat.numb_fill_in = 0;
+printf(" Number of baseblocks - start:  %d\n", numb_bblocks);
+		LUD( &(mat.mainNode),
+				MTN_START, BND_PART_ALL, BND_PART_ALL , LEVEL_START );
+printf(" Number of fill ins generated:  %d\n", mat.numb_fill_in);	
+printf(" Number of baseblocks - end:  %d\n", numb_bblocks);
+	/*			
+		printf("\nMatrix:\n");
+		printMatrix(mat.mainNode, printINformat);
+		printf("\n");
+	*/	
+		
+	//timestamp_end("LU Decomposing - TIME TAKEN:");
+		//Solve Ax = b
+		lu_sysSolver(mat.backupNode, mat.mainNode, result, (int*)(&iter));
+	timestamp_end("SOLVING SYS OF EQs. - TIME TAKEN:");
 
-//printing
-int printInput = 0;			//print input?
-int printOutput = 0;			//print rhs?
-char printINformat[MAX_FLAG_SIZE];	//format used to print input matrices
+	int nans = 0;
+	for(int i=0; i<mat.getRows(); i++){
+		if(isnan(result[i]))
+			nans++;
+	}
 
-
-//global vars
-Matrix mat;
-Memory<quadNode> quadStock(1);
-Memory<dataType> bBlockStock;
-int gRowIndex = 0;
-int gColIndex = 0;
-int alloc_step;
-int baseOrderLg = 3;
-int baseOrder;
-int baseOrder2;
-int baseSize;
-int baseSize2;
-double OMEGA = 1 ;
-int numb_bblocks = 0;
-
-//Function prototypes
-void solveSys();
-void getUserArgs(int nargc, char** nargv);
-void setFlag(char* flagName, char *value);
-void checkFormat(char* fmt);
-void printHelp();
-void buildTree(char* fmt);
-void printMatrix(Both* node, char* printfmt);
+	//print result and number of iterations
+	printf(
+		"\n\tResult returned after %d iterations - nans: %d\n\n", iter, nans);
+	if(printOutput){
+		printArray(result, mat.getRows() );
+		printf("\n\n");
+	}
+	free(result);
+}
 
 
 #endif
-/////////////////////////////////////////////////////////////////////////////
+//________________________________ END ____________________________________________________
+///////////////////////////////////////////////////////////////////////////////////////////
 
