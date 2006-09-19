61993d4dae695eccd0389b827732c735167510c2
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2006-09-19 23:14:32 +0000

    recursive for each

---
+++ trunk/mtl4/trunk/boost/numeric/mtl/recursion/base_case_test.hpp
@@ -0,0 +1,71 @@
+// $COPYRIGHT$
+
+#ifndef MTL_BASE_CASE_TEST_INCLUDE
+#define MTL_BASE_CASE_TEST_INCLUDE
+
+#include <algorithm>
+
+namespace mtl { namespace recursion {
+
+// Minimum of dimensions is less or equal to the reference value
+struct min_dim_test
+{
+    min_dim_test(std::size_t comp) : comp(comp) {}
+
+    template <typename Recurator>
+    bool operator() (Recurator const& recurator) const
+    {
+	return std::min(recurator.get_value().num_rows(), 
+			recurator.get_value().num_cols()) 
+	       <= comp;
+    }
+
+private:
+    std::size_t  comp;
+};
+
+
+// Minimum of dimensions is less or equal to the reference value
+//   and it can't be split into 2 sub-matrices less or equal the ref value
+struct undivisible_min_dim_test
+{
+    undivisible_min_dim_test(std::size_t comp) : comp(comp) {}
+
+    template <typename Recurator>
+    bool operator() (Recurator const& recurator) const
+    {
+	std::size_t min_dim= std::min(recurator.get_value().num_rows(), 
+				      recurator.get_value().num_cols()),
+	            max_dim= std::max(recurator.get_value().num_rows(),
+				      recurator.get_value().num_cols());
+
+	return min_dim <= comp && 2 * min_dim > max_dim;
+    }
+
+private:
+    std::size_t  comp;
+};
+
+
+// Maximum of dimensions is less or equal to the reference value
+struct max_dim_test
+{
+    max_dim_test(std::size_t comp) : comp(comp) {}
+
+    template <typename Recurator>
+    bool operator() (Recurator const& recurator) const
+    {
+	return std::max(recurator.get_value().num_rows(), 
+			recurator.get_value().num_cols()) 
+	       <= comp;
+    }
+
+private:
+    std::size_t  comp;
+};
+
+
+
+}} // namespace mtl::recursion
+
+#endif // MTL_BASE_CASE_TEST_INCLUDE
