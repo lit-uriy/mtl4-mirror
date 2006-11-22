0a0a33138dce6cc08bbdc502d3fcccf0c9118684
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2006-11-22 20:22:23 +0000

    meta functions for bit manipulations, i.e. related to powers of 2

---
+++ trunk/mtl4/trunk/boost/numeric/meta_math/log_2.hpp
@@ -0,0 +1,32 @@
+// $COPYRIGHT$
+
+#ifndef META_MATH_LOG_2_INCLUDE
+#define META_MATH_LOG_2_INCLUDE
+
+#include <boost/numeric/meta_math/is_power_of_2.hpp>
+
+namespace meta_math {
+
+// Computes the logarithm to the basis 2
+// Without testing if power of 2 it rounds values down to next integer
+template <unsigned long X>
+struct log_2
+{
+    // BOOST_STATIC_ASSERT(is_power_of_2_meta<X>::value);
+    static const unsigned long value= log_2< X >> 1 >::value + 1;
+};
+
+template <> struct log_2<1>
+{
+    static const unsigned long value= 0;
+};
+
+template <> struct log_2<0>
+{
+#error "Logarithm of 0 is undefined"
+};
+
+
+} // namespace meta_math
+
+#endif // META_MATH_LOG_2_INCLUDE
