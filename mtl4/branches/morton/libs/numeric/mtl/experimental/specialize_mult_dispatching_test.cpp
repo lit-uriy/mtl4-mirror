6344443eb909ac3f58094130480f89e51145a30d
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2006-12-07 18:20:56 +0000

    add timing code for assembly; some results

--- mtl4/branches/morton/libs/numeric/mtl/experimental/specialize_mult_dispatching_test.cpp
+++ mtl4/branches/morton/libs/numeric/mtl/experimental/specialize_mult_dispatching_test.cpp
@@ -8,65 +8,17 @@
  
 #include <boost/numeric/mtl/dense2D.hpp>
 #include <boost/numeric/mtl/morton_dense.hpp>
-#include <boost/numeric/mtl/operations/set_to_0.hpp>
 #include <boost/numeric/mtl/operations/print_matrix.hpp>
-#include <boost/numeric/mtl/operations/specialize_mult_type.hpp>
 #include <boost/numeric/mtl/recursion/bit_masking.hpp>
-#include <boost/numeric/mtl/operations/opteron/mult_add_base_case_32_shark_2.hpp>
-#include <boost/numeric/mtl/operations/matrix_mult.hpp>
-#include <boost/numeric/mtl/recursion/recursive_matrix_mult.hpp>
-#include <boost/numeric/mtl/recursion/matrix_recurator.hpp>
-#include <boost/numeric/mtl/recursion/base_case_matrix.hpp>
 #include <boost/numeric/mtl/operations/hessian_matrix_utilities.hpp>
-
-
-namespace mtl {
-
- 
-template <typename MatrixA, typename MatrixB, typename MatrixC>
-void specialized_mult_add(MatrixA const& a, MatrixB const& b, MatrixC& c) 
-{
-    typedef recursion::bound_test_static<32>                      BaseCaseTest;
-
-    using recursion::base_case_matrix;
-    typedef typename base_case_matrix<MatrixA, BaseCaseTest>::type base_a_type;
-    typedef typename base_case_matrix<MatrixB, BaseCaseTest>::type base_b_type;
-    typedef typename base_case_matrix<MatrixC, BaseCaseTest>::type base_c_type;
-
-    typedef typename mtl::specialize_mult_type<
-        MatrixA, MatrixB, MatrixC
-      , BaseCaseTest
-      , functor::mult_add_simple_t<base_a_type, base_b_type, base_c_type>
-    >::type                                                       mult_type;
-
-    // std::cout << "Mult type " << typeid(mult_type()).name() << "\n";
-
-    using recursion::matrix_recurator;
-    matrix_recurator<MatrixA>    rec_a(a);
-    matrix_recurator<MatrixB>    rec_b(b);
-    matrix_recurator<MatrixC>    rec_c(c);
-    equalize_depth(rec_a, rec_b, rec_c);
-
-    using recursion::recurator_mult_add;
-    recurator_mult_add(rec_a, rec_b, rec_c, mult_type(), BaseCaseTest());
-}
- 
-template <typename MatrixA, typename MatrixB, typename MatrixC>
-void specialized_matrix_mult(MatrixA const& a, MatrixB const& b, MatrixC& c)
-{
-    set_to_0(c);
-    specialized_mult_add(a, b, c);
-}
-
-} // namespace mtl
-
+#include <boost/numeric/mtl/recursion/specialized_matrix_mult.hpp>
 
  
 using namespace std;
 using namespace mtl;
 
 template <typename MatrixA, typename MatrixB, typename MatrixC>
-void test(MatrixA& a, MatrixB& b, MatrixC& c, const char* name, bool check)
+void test(MatrixA& a, MatrixB& b, MatrixC& c, const char* name)
 {
     std::cout << "\n" << name << "  --- calling specializing mult:\n";
  
@@ -84,9 +36,9 @@ int test_main(int argc, char* argv[])
 {
 
 #if defined MTL_USE_OPTERON_OPTIMIZATION && defined __GNUC__ && !defined __INTEL_COMPILER
- cout << "optimized on gcc\n";
+    cout << "optimized on gcc\n";
 #else
-  cout << "not optimized on gcc\n";
+    cout << "not optimized on gcc\n";
 #endif
 
   // return 0; 
@@ -118,15 +70,15 @@ int test_main(int argc, char* argv[])
     morton_dense<float, doppler_32_row_mask>       mraf(size, size), mrbf(size, size), mrcf(size, size);
 
     std::cout << "Testing base case optimization\n";
-    test(da, db, dc, "dense2D", false);
-    test(mda, mdb, mdc, "pure Morton", false);
-    test(mca, mcb, mcc, "Hybrid col-major", false);
-    test(mra, mrb, mrc, "Hybrid row-major", false);
-    test(mrans, mcbns, mrcns, "Hybrid col-major and row-major, no shark tooth", false);
-    test(mraf, mcbf, mrcf, "Hybrid col-major and row-major with float", false);
-    test(mra, mcb, mrc, "Hybrid col-major and row-major", true);
-    test(mzra, mzcb, mzrc, "Hybrid col-major and row-major, Z-order", true);
-    test(mzra, mzcb, mzrc, "Hybrid col-major and row-major, Z and E-order", true);
+    test(da, db, dc, "dense2D");
+    test(mda, mdb, mdc, "pure Morton");
+    test(mca, mcb, mcc, "Hybrid col-major");
+    test(mra, mrb, mrc, "Hybrid row-major");
+    test(mrans, mcbns, mrcns, "Hybrid col-major and row-major, no shark tooth");
+    test(mraf, mcbf, mrcf, "Hybrid col-major and row-major with float");
+    test(mra, mcb, mrc, "Hybrid col-major and row-major");
+    test(mzra, mzcb, mzrc, "Hybrid col-major and row-major, Z-order");
+    test(mzra, mzcb, mzrc, "Hybrid col-major and row-major, Z and E-order");
 
     return 0;
 }
