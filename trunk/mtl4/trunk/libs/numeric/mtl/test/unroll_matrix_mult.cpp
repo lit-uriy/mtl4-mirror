4042152217288c6297981f39cdf29a1a2959b8de
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2007-01-12 01:02:30 +0000

    templated unrolling

--- trunk/mtl4/trunk/libs/numeric/mtl/test/unroll_matrix_mult.cpp
+++ trunk/mtl4/trunk/libs/numeric/mtl/test/unroll_matrix_mult.cpp
@@ -8,9 +8,90 @@
 
 #include <boost/numeric/mtl/dense2D.hpp>
 #include <boost/numeric/mtl/operations/hessian_matrix_utilities.hpp>
+#include <boost/numeric/mtl/operations/set_to_0.hpp>
 
 using namespace std;
 using namespace mtl;
+
+
+template <unsigned Outer, unsigned OuterMax, unsigned Middle, unsigned MiddleMax,
+	  unsigned Inner, unsigned InnerMax>
+struct triple_unroll
+{
+    static unsigned const outer= Outer - 1, next_outer= Outer,
+	                  middle= Middle - 1, next_middle= Middle,
+            	          inner= Inner - 1, next_inner= Inner + 1;
+    void operator() ()
+    {
+	cout << outer << " : " << middle << " : " << inner << "\n";
+	triple_unroll<next_outer, OuterMax, next_middle, MiddleMax, next_inner, InnerMax>() ();
+    }
+};
+
+
+template <unsigned Outer, unsigned OuterMax, unsigned Middle, unsigned MiddleMax, unsigned InnerMax>
+struct triple_unroll<Outer, OuterMax, Middle, MiddleMax, InnerMax, InnerMax>
+{
+    static unsigned const outer= Outer - 1, next_outer= Outer,
+	                  middle= Middle - 1, next_middle= Middle + 1,
+            	          inner= InnerMax - 1, next_inner= 1;
+    void operator() ()
+    {
+	cout << outer << " : " << middle << " : " << inner << "\n";
+	triple_unroll<next_outer, OuterMax, next_middle, MiddleMax, next_inner, InnerMax>() ();
+    }
+};
+
+
+template <unsigned Outer, unsigned OuterMax, unsigned MiddleMax, unsigned InnerMax>
+struct triple_unroll<Outer, OuterMax, MiddleMax, MiddleMax, InnerMax, InnerMax>
+{
+    static unsigned const outer= Outer - 1, next_outer= Outer + 1,
+	                  middle= MiddleMax - 1, next_middle= 1,
+            	          inner= InnerMax - 1, next_inner= 1;
+    void operator() ()
+    {
+	cout << outer << " : " << middle << " : " << inner << "\n";
+	triple_unroll<next_outer, OuterMax, next_middle, MiddleMax, next_inner, InnerMax>() ();
+    }
+};
+
+
+template <unsigned OuterMax, unsigned MiddleMax, unsigned InnerMax>
+struct triple_unroll<OuterMax, OuterMax, MiddleMax, MiddleMax, InnerMax, InnerMax>
+{
+    static unsigned const outer= OuterMax - 1,
+	                  middle= MiddleMax - 1,
+            	          inner= InnerMax - 1;
+    void operator() ()
+    {
+	cout << outer << " : " << middle << " : " << inner << "\n";
+    }
+};
+
+
+template <unsigned Outer, unsigned OuterMax, unsigned Middle, unsigned MiddleMax,
+	  unsigned Inner, unsigned InnerMax>
+struct triple_wrapper : public triple_unroll<Outer, OuterMax, Middle, MiddleMax, Inner, InnerMax>
+{
+    void operator() ()
+    {
+	cout << this->outer << " : " << this->middle << " : " << this->inner << "\n";
+	triple_wrapper<this->next_middle, OuterMax, this->next_middle, MiddleMax, this->next_inner, InnerMax>() ();
+    }  
+};
+
+
+template <unsigned OuterMax, unsigned MiddleMax, unsigned InnerMax>
+struct triple_wrapper<OuterMax, OuterMax, MiddleMax, MiddleMax, InnerMax, InnerMax>
+    : public triple_unroll<OuterMax, OuterMax, MiddleMax, MiddleMax, InnerMax, InnerMax>
+{
+    void operator() ()
+    {
+	cout << this->outer << " : " << this->middle << " : " << this->inner << "\n";
+    }  
+};
+
 
 
 void print_time_and_mflops(double time, double size)
@@ -84,6 +165,7 @@ void mult_simple_p(dense2D<double>& a, dense2D<double>& b, dense2D<double>& c)
 }
 
 typedef dense2D<double, matrix_parameters<col_major> >   cm_type;
+typedef dense2D<double, matrix_parameters<row_major> >   rm_type;
 
 void mult_simple_pt(dense2D<double>& a, cm_type& b, dense2D<double>& c)
 {
@@ -286,10 +368,105 @@ void mult_simple_ptu224g(dense2D<double>& a, cm_type& b, dense2D<double>& c)
 	}
 }
 
+// i, k, j == Outer, Middle, Inner
+// Outer == row of A and C
+// Middle == column of B and C
+// Inner == column of A and row of B 
+template <unsigned Outer, unsigned OuterMax, unsigned Middle, unsigned MiddleMax,
+	  unsigned Inner, unsigned InnerMax>
+struct double_matmat_mult_block 
+    : public triple_unroll<Outer, OuterMax, Middle, MiddleMax, Inner, InnerMax>
+{
+    typedef triple_unroll<Outer, OuterMax, Middle, MiddleMax, Inner, InnerMax> base;
+    typedef double_matmat_mult_block<base::next_outer, OuterMax, base::next_middle, MiddleMax, base::next_inner, InnerMax> next_t;
+    typedef double       v_t;
+    typedef std::size_t  s_t;
 
+    void operator() (v_t &tmp00, v_t &tmp01, v_t &tmp02, v_t &tmp03, v_t &tmp04, 
+		     v_t &tmp05, v_t &tmp06, v_t &tmp07, v_t &tmp08, v_t &tmp09, 
+		     v_t &tmp10, v_t &tmp11, v_t &tmp12, v_t &tmp13, v_t &tmp14, v_t &tmp15, 
+		     v_t *begin_a, s_t ari, s_t aci, v_t *begin_b, s_t bri, s_t bci)
+    {
+	tmp00+= begin_a[ this->outer * ari + this->inner * aci ] * begin_b[ this->inner * bri + this->middle * bci ];
+	next_t()(tmp01, tmp02, tmp03, tmp04, tmp05, tmp06, tmp07, tmp08, tmp09, 
+		 tmp10, tmp11, tmp12, tmp13, tmp14, tmp15, tmp00, 
+		 begin_a, ari, aci, begin_b, bri, bci); 
+    }
+
+    template<typename MatrixC>
+    void update(v_t &tmp00, v_t &tmp01, v_t &tmp02, v_t &tmp03, v_t &tmp04, 
+		v_t &tmp05, v_t &tmp06, v_t &tmp07, v_t &tmp08, v_t &tmp09, 
+		v_t &tmp10, v_t &tmp11, v_t &tmp12, v_t &tmp13, v_t &tmp14, v_t &tmp15, 
+		MatrixC& c, s_t i, s_t k)
+    {
+	c[i + this->outer][k + this->middle]+= tmp00;
+	next_t().update(tmp01, tmp02, tmp03, tmp04, tmp05, tmp06, tmp07, tmp08, tmp09, 
+			tmp10, tmp11, tmp12, tmp13, tmp14, tmp15, tmp00, 
+			c, i, k);
+    }
+};
+
+template <unsigned OuterMax, unsigned MiddleMax, unsigned InnerMax>
+struct double_matmat_mult_block<OuterMax, OuterMax, MiddleMax, MiddleMax, InnerMax, InnerMax>
+    : public triple_unroll<OuterMax, OuterMax, MiddleMax, MiddleMax, InnerMax, InnerMax>
+{
+    typedef double       v_t;
+    typedef std::size_t  s_t;
+
+    void operator() (v_t &tmp00, v_t &, v_t &, v_t &, v_t &, 
+		     v_t &, v_t &, v_t &, v_t &, v_t &, 
+		     v_t &, v_t &, v_t &, v_t &, v_t &, v_t &, 
+		     v_t *begin_a, s_t ari, s_t aci, v_t *begin_b, s_t bri, s_t bci)
+    {
+	tmp00+= begin_a[ this->outer * ari + this->inner * aci ] * begin_b[ this->inner * bri + this->middle * bci ];
+    }
+
+    template<typename MatrixC>
+    void update(v_t &tmp00, v_t &, v_t &, v_t &, v_t &, 
+		v_t &, v_t &, v_t &, v_t &, v_t &, 
+		v_t &, v_t &, v_t &, v_t &, v_t &, v_t &, 
+		MatrixC& c, s_t i, s_t k)
+    {
+	c[i + this->outer][k + this->middle]+= tmp00;
+   }
+
+};
+
+template<unsigned Outer, unsigned Middle, unsigned Inner, typename MatrixA, typename MatrixB, typename MatrixC>
+void double_matmat_mult_template(MatrixA& a, MatrixB& b, MatrixC& c)
+{
+    BOOST_STATIC_ASSERT(Outer * Middle * Inner <= 16);
+ 
+    set_to_0(c);
+    double_matmat_mult_block<1, Outer, 1, Middle, 1, Inner> block;
+    using std::size_t;
+    for (size_t i= 0; i < c.num_rows(); i+= Outer)
+	for (size_t k= 0; k < c.num_cols(); k+= Middle) {
+	    size_t ari= a.c_offset(1, 0), // how much is the offset of A's entry increased by incrementing row
+		   aci= a.c_offset(0, 1), bri= b.c_offset(1, 0), bci= b.c_offset(0, 1);
+	    double tmp00= 0.0, tmp01= 0.0, tmp02= 0.0, tmp03= 0.0, tmp04= 0.0,
+                   tmp05= 0.0, tmp06= 0.0, tmp07= 0.0, tmp08= 0.0, tmp09= 0.0,
+ 		   tmp10= 0.0, tmp11= 0.0, tmp12= 0.0, tmp13= 0.0, tmp14= 0.0, tmp15= 0.0;
+	    double *begin_a= &a[i][0], *end_a= &a[i][a.num_cols()], *begin_b= &b[0][k];
+
+	    for (; begin_a != end_a; begin_a+= Inner*aci, begin_b+= Inner*bri)
+		block(tmp00, tmp01, tmp02, tmp03, tmp04, tmp05, tmp06, tmp07, tmp08, tmp09, 
+		      tmp10, tmp11, tmp12, tmp13, tmp14, tmp15, 
+		      begin_a, ari, aci, begin_b, bri, bci); 
+	    block.update(tmp00, tmp01, tmp02, tmp03, tmp04, tmp05, tmp06, tmp07, tmp08, tmp09, 
+			 tmp10, tmp11, tmp12, tmp13, tmp14, tmp15, 
+			 c, i, k);
+	}
+}
 
 int test_main(int argc, char* argv[])
 {
+    triple_wrapper<1, 4, 1, 4, 1, 4>() ();
+    //triple_unroll<4, 4, 4, 4, 4, 4>() ();
+    //triple_unroll<3, 3, 6, 6, 4, 4>() ();
+
+    //return 0;
+
     unsigned steps= 32, max_size= 128, size= 32; 
     if (argc > 2) {
 	steps= atoi(argv[1]); max_size= atoi(argv[2]);
@@ -300,6 +477,17 @@ int test_main(int argc, char* argv[])
     fill_hessian_matrix(da, 1.0);
     fill_hessian_matrix(db, 2.0); 
     fill_hessian_matrix(dbt, 2.0); 
+
+    time_series(da, dbt, dc, double_matmat_mult_template<2, 2, 2, rm_type, cm_type, rm_type>, 
+		"Simple mult (pointers trans unrolled 2x2x2 template)", steps, max_size);
+    time_series(da, dbt, dc, double_matmat_mult_template<1, 1, 1, rm_type, cm_type, rm_type>, 
+		"Simple mult (pointers trans unrolled 1x1x1 template)", steps, max_size);
+    time_series(da, dbt, dc, double_matmat_mult_template<2, 4, 1, rm_type, cm_type, rm_type>, 
+		"Simple mult (pointers trans unrolled 2x1x4 template)", steps, max_size);
+    time_series(da, db, dc, double_matmat_mult_template<2, 4, 1, rm_type, rm_type, rm_type>, 
+		"Simple mult (pointers unrolled 2x1x4 template)", steps, max_size);
+    time_series(da, dbt, dc, double_matmat_mult_template<2, 4, 2, rm_type, cm_type, rm_type>, 
+		"Simple mult (pointers trans unrolled 2x2x4 template)", steps, max_size);
 
     time_series(da, dbt, dc, mult_simple_ptu224g, "Simple mult (pointers trans unrolled 2x2x4 generic)", steps, max_size);
     time_series(da, dbt, dc, mult_simple_ptu224, "Simple mult (pointers trans unrolled 2x2x4)", steps, max_size);
