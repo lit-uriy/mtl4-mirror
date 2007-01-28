d557bde21bd20b3b614e12f2bc5727237f0ba641
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2007-01-28 15:02:17 +0000

    meta-programming support functions for move semantics

---
+++ trunk/mtl4/trunk/boost/numeric/mtl/draft/add_rvalue.hpp
@@ -0,0 +1,56 @@
+// $COPYRIGHT$
+
+#ifndef MTL_ADD_RVALUE_INCLUDE
+#define MTL_ADD_RVALUE_INCLUDE
+
+#include <boost/mpl/if.hpp>
+
+namespace mtl {
+
+
+  // Unknow (matrix) types are themselves
+template <typename Matrix>
+struct add_rvalue
+{
+    typedef Matrix     type;
+};
+
+
+namespace detail {
+    template <typename Para>
+    struct add_rvalue_parameters
+    {
+	// If matrix is stored on stack it can't be a rvalue
+	typedef boost::if_c<
+	    Para::on_stack
+	  , Para
+	  , matrix_parameters<typename Para::orientation, typename Para::index, typename Para::dimensions, false, true>
+	>::type type;
+    };
+}
+
+
+template <typename Value, typename Para>
+struct add_rvalue<dense2D<Value, Para> >
+{
+    typedef dense2D<Value, typename detail::add_rvalue_parameters<Para>::type>   type;
+}
+
+template <typename Value, typename Para>
+struct add_rvalue<compressed2D<Value, Para> >
+{
+    typedef compressed2D<Value, typename detail::add_rvalue_parameters<Para>::type>   type;
+}
+
+
+template <typename Value, unsigned long Mask, typename Para>
+struct add_rvalue<morton_dense<Value, Mask, Para> >
+{
+    typedef morton_dense<Value, Mask, typename detail::add_rvalue_parameters<Para>::type>   type;
+}
+
+
+
+} // namespace mtl
+
+#endif // MTL_ADD_RVALUE_INCLUDE
