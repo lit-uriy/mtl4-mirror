e909d9808be68be8139f77c1e5662d52d38bf37d
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2007-01-30 20:48:42 +0000

    meta-programming loop unrolling

---
+++ mtl4/trunk/boost/numeric/meta_math/loop.hpp
@@ -0,0 +1,10 @@
+// $COPYRIGHT$
+
+#ifndef META_MATH_LOOP_INCLUDE
+#define META_MATH_LOOP_INCLUDE
+
+#include <boost/numeric/meta_math/loop1.hpp>
+#include <boost/numeric/meta_math/loop2.hpp>
+#include <boost/numeric/meta_math/loop3.hpp>
+
+#endif // META_MATH_LOOP_INCLUDE
