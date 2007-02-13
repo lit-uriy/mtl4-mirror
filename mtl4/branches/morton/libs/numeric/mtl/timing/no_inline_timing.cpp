cb56429a8e951ebcacd04ba789f02e0a8c582454
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2007-02-13 15:22:11 +0000

    timing with external functions

--- mtl4/branches/morton/libs/numeric/mtl/timing/no_inline_timing.cpp
+++ mtl4/branches/morton/libs/numeric/mtl/timing/no_inline_timing.cpp
@@ -131,6 +131,7 @@ struct no_inline3<Functor, void, Arg1, Arg2, Arg3>
 void hybrid_ext_mult_44(const morton_dense<double,  doppler_64_row_mask>& a, 
 			const morton_dense<double,  doppler_64_col_mask>& b,
 			morton_dense<double,  doppler_64_row_mask>& c);
+#endif
 
 void dense_ext_mult_44(const dense2D<double>& a,
 		       const dense2D<double, matrix_parameters<col_major> >& b,
@@ -155,7 +156,7 @@ struct ext_mult_44
     }
 };
 
-#endif
+
 
 template <typename Matrix, typename MatrixB> 
 void measure_unrolling(unsigned size, std::vector<int>& enabled, Matrix& matrix, MatrixB& matrixb)
@@ -174,7 +175,7 @@ void measure_unrolling(unsigned size, std::vector<int>& enabled, Matrix& matrix,
       tiling_44_t;
     typedef no_inline3<tiling_44_t, void, const Matrix, const MatrixB, Matrix> tiling_44_no_inline_t;
 
-    // gen_recursive_dense_mat_mat_mult_t<ext_mult_44>           rec_ext_mult_44;
+    gen_recursive_dense_mat_mat_mult_t<ext_mult_44>           rec_ext_mult_44;
 
     typedef typename base_case_matrix<Matrix, test64_t>::type    BaseMatrix;
     typedef typename base_case_matrix<MatrixB, test64_t>::type   BaseMatrixB;
@@ -187,7 +188,7 @@ void measure_unrolling(unsigned size, std::vector<int>& enabled, Matrix& matrix,
     single_measure(matrix, matrixb, matrix, mult_44, size, enabled, 2);
     single_measure(matrix, matrixb, matrix, tiling_22_no_inline_t(), size, enabled, 3);
     single_measure(matrix, matrixb, matrix, tiling_44_no_inline_t(), size, enabled, 4);
-    // single_measure(matrix, matrixb, matrix, rec_ext_mult_44, size, enabled, 5);
+    single_measure(matrix, matrixb, matrix, rec_ext_mult_44, size, enabled, 5);
     single_measure(matrix, matrixb, matrix, rec_no_inline_mult_44, size, enabled, 5);
 
     std::cout << "0\n";  std::cout.flush();
@@ -242,167 +243,4 @@ int main(int argc, char* argv[])
 
 }
  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#if 0
-
-    series(10, 30, measure_morton_order, "Comparing Z-, N-order and mixed with recursive multiplication:");
-    series(10, 30, measure_cast, "Comparing base case cast (64) for Z-order, hybrid 32, hybrid 64 row and col-major:");
-    series(10, 30, measure_with_unroll, "Using unrolled mult on hybrid row- and column-major matrices:");
-    series(10, 30, measure_base_size, "Comparing base case sizes for corresponding hybrid row-major matrices:");
-    series(10, 30, measure_unrolling_hybrid, "Comparing different unrolling for hybrid row-major matrices:");
-    series(10, 30, measure_unrolling_dense, "Comparing different unrolling for row-major dense matrices:");
-
-
-
-
-
-    using std::string;
-    std::vector<std::string> scenarii;
-    scenarii.push_back(string("Morton Z-order"));
-    scenarii.push_back(string("Morton E-order"));
-    scenarii.push_back(string("Morton Z/E-order (A,C in Z, B in E-order)"));
-    scenarii.push_back(string("Dense row-major"));
-    scenarii.push_back(string("Dense col-major"));
-    scenarii.push_back(string("Dense row/col-major (R, C, R)"));
-    scenarii.push_back(string("Hybrid row-major"));
-    scenarii.push_back(string("Hybrid row/col-major (R, C, R)"));
-    scenarii.push_back(string("Z/E Hybrid row/col-major (Z-R, Z-C, E-R)"));
-    scenarii.push_back(string("Hybrid 64 row-major"));
-    scenarii.push_back(string("Hybrid 64 row/col-major (R, C, R)"));
-    scenarii.push_back(string("Z/E Hybrid 64 row/col-major (Z-R, Z-C, E-R)"));
-    scenarii.push_back(string("Morton Z-order and Dense col-major"));
-    scenarii.push_back(string("Hybrid 64 row-major and dense col-major"));
-
-    using std::cout;
-    if (argc < 4) {
-	cerr << "usage: recursive_mult_timing <scenario> <steps> <max_size>\nScenarii:\n"; 
-	for (unsigned i= 0; i < scenarii.size(); i++)
-	    cout << i << ": " << scenarii[i] << "\n";
-	exit(1);
-    }
-    unsigned int scenario= atoi(argv[1]), steps= atoi(argv[2]), max_size= atoi(argv[3]), size= 32; 
-
-
-
-
-    // Matrices are actually only place holders for the type
-    mtl::dense2D<double>                           da(size, size), db(size, size), dc(size, size);
-    mtl::dense2D<double, matrix_parameters<col_major> >  dca(size, size), dcb(size, size), dcc(size, size);
-
-    morton_dense<double,  morton_mask>             mda(size, size), mdb(size, size), mdc(size, size);
-    morton_dense<double,  morton_z_mask>           mzda(size, size), mzdb(size, size), mzdc(size, size);
-
-    morton_dense<double, doppler_32_col_mask>      mca(size, size), mcb(size, size), mcc(size, size);
-    morton_dense<double, doppler_32_row_mask>      mra(size, size), mrb(size, size), mrc(size, size);
-
-    morton_dense<double, doppler_z_32_col_mask>    mzca(size, size), mzcb(size, size), mzcc(size, size);
-    morton_dense<double, doppler_z_32_row_mask>    mzra(size, size), mzrb(size, size), mzrc(size, size);
-
-    cout << "Measuring block-recursive computations\n";
-    switch (scenario) {
-      case 0: 	time_series(mzda, mzdb, mzdc, scenarii[0], steps, max_size); break;
-      case 1: 	time_series(mda, mdb, mdc, scenarii[1], steps, max_size); break;
-      case 2: 	time_series(mzda, mdb, mzdc, scenarii[2], steps, max_size); break;
-      case 3: 	time_series(da, db, dc, scenarii[3], steps, max_size); break;
-      case 4: 	time_series(dca, dcb, dcc, scenarii[4], steps, max_size); break;
-      case 5: 	time_series(da, dcb, dc, scenarii[5], steps, max_size); break;
-      case 6: 	time_series(mra, mrb, mrc, scenarii[6], steps, max_size); break;
-      case 7: 	time_series(mra, mcb, mrc, scenarii[7], steps, max_size); break;
-	  //case 8: 	time_series(mzra, mzcb, mrc, scenarii[8], steps, max_size); break;
-	//case 8: 	time_series(mzda, dcb, dcc, scenarii[8], steps, max_size); break;
-    }
-
-    return 0;
-#endif
-
-
-
-
-
-#if 0
-
-// Matrices are only placeholder to provide the type
-template <typename MatrixA, typename MatrixB, typename MatrixC>
-double time_measure(MatrixA&, MatrixB&, MatrixC&, unsigned size, std::vector<int> enabled)
-{
-    using std::cout;
-
-    const double max_time= 1200.0;
-
-    MatrixA a(size, size);
-    MatrixB b(size, size);
-    MatrixC c(size, size);
-
-    fill_hessian_matrix(a, 1.0);
-    fill_hessian_matrix(b, 2.0); 
-
-    typedef recursion::bound_test_static<32>    test32;
-    typedef recursion::bound_test_static<64>    test64;
-
-    cout << size << ", ";
-
-    if (enabled[0]) {
-	boost::timer start;	
-	// do some mult
-	print_time_and_mflops(start.elapsed(), a.num_rows());
-	if (start.elapsed() > max_time)
-	    enabled[0]= false;
-    } else
-	cout << ", , ";
-
-
-    recursion::bound_test_static<32>    base_case_test;
-    std::cout << size << ", ";
-    boost::timer start1;
-    recursive_matrix_mult_simple(a, b, c, base_case_test); 
-    print_time_and_mflops(start1.elapsed(), a.num_rows());
-
-    boost::timer start2;
-    recursive_matrix_mult_fast_inner(a, b, c, base_case_test);
-    print_time_and_mflops(start2.elapsed(), a.num_rows());
-
-    boost::timer start3;
-    recursive_matrix_mult_fast_middle<4, 8>(a, b, c, base_case_test);
-    print_time_and_mflops(start3.elapsed(), a.num_rows());
-
-    boost::timer start4;
-    recursive_matrix_mult_fast_outer<2, 2, 8>(a, b, c, base_case_test);
-    print_time_and_mflops(start4.elapsed(), a.num_rows());
-
-    std::cout << "0\n"; // to not finish with comma
-    std::cout.flush();
-    return start1.elapsed(); // total time (approx)
-}
-     
- 
-template <typename MatrixA, typename MatrixB, typename MatrixC>
-void time_series(MatrixA& a, MatrixB& b, MatrixC& c, const string& name, unsigned steps, unsigned max_size)
-{
-    // Maximal time per measurement 20 min
-    double max_time= 1200.0;
-    std::vector<int> enabled(10, true);
-
-    std::cout << "# " << name << "  --- with dispatching recursive multiplication:\n";
-    std::cout << "# Gnu-Format size, time, MFlops\n";
-    std::cout.flush();
-
-    for (unsigned i= steps; i <= max_size; i+= steps) {
-	double elapsed= time_measure(a, b, c, i, enabled);
-	if (elapsed > max_time) break;
-    }
-}
-#endif
 
