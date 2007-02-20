a97e554773830264ba33cf20189f60fc5a34d875
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2007-02-20 17:00:45 +0000

    intermediate

--- trunk/mtl4/trunk/boost/numeric/mtl/traits.hpp
+++ trunk/mtl4/trunk/boost/numeric/mtl/traits.hpp
@@ -3,20 +3,13 @@
 #ifndef MTL_TRAITS_INCLUDE
 #define MTL_TRAITS_INCLUDE
 
-#include <boost/numeric/mtl/tag.hpp>
+#include <boost/numeric/mtl/utility/tag.hpp>
 
 namespace mtl { namespace traits {
 
-// typetrait to check if certain type is MTL type
-// then it provides additional types and variables
-template <class T> struct is_mtl_type 
-{ 
-    static bool const value= false; 
-};
-
-// Get matrix tag for dispatching
-// Has to be specialized for each matrix
-template <class Matrix> struct matrix_category 
+// Get tag for dispatching matrices, vectors, ...
+// Has to be specialized for each matrix, vector, ...
+template <class Collection> struct category 
 {
     typedef tag::unknown type;
 };
