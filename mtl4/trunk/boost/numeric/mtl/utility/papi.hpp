f873f2f05a792334ff0cbf97757a80d70c586456
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2007-03-01 01:52:33 +0000

    figured out how to use PAPI, BLAS, LAPACK

---
+++ mtl4/trunk/boost/numeric/mtl/utility/papi.hpp
@@ -0,0 +1,145 @@
+// $COPYRIGHT$
+
+#ifndef MTL_PAPI_INCLUDE
+#define MTL_PAPI_INCLUDE
+
+#ifdef MTL_HAS_PAPI
+
+#include <papi.h>
+
+#endif // MTL_HAS_PAPI
+
+namespace mtl { namespace utility {
+
+
+struct papi_error {};
+struct papi_version_mismatch : papi_error {};
+struct papi_no_counters : papi_error {};
+struct papi_create_eventset_error : papi_error {};
+struct papi_name_to_code_error : papi_error {};
+struct papi_query_event_error : papi_error {};
+struct papi_start_event_error : papi_error {};
+struct papi_add_event_error : papi_error {};
+struct papi_reset_error : papi_error {};
+struct papi_read_error : papi_error {};
+struct papi_index_range_error : papi_error {};
+
+#ifdef MTL_HAS_PAPI
+
+class papi_t 
+{
+    void init_papi()
+    {
+	static bool initialized= false;
+	if (!initialized) {
+
+	    if ( PAPI_library_init(PAPI_VER_CURRENT) != PAPI_VER_CURRENT )
+		throw papi_version_mismatch();
+
+	    num_counters = PAPI_get_opt(PAPI_MAX_HWCTRS, NULL);
+	    if (num_counters <= 0) throw papi_no_counters();
+
+	    counters= new long_long[num_counters];
+
+	    if (PAPI_create_eventset(&event_set) != PAPI_OK) throw papi_create_eventset_error();
+	    initialized= true;
+	}
+    }
+
+public:
+    const static bool true_papi = true;
+
+    papi_t() : event_set(PAPI_NULL), active_events(0)
+    {
+	init_papi();
+    }
+
+
+    ~papi_t()
+    {
+	delete[](counters);
+    }
+
+
+    // returns index of added event
+    int add_event(const char* name)
+    {
+	int code;
+	if (PAPI_event_name_to_code(const_cast<char*>(name), &code) != PAPI_OK) 
+	    throw papi_name_to_code_error();
+	std::cout << "add event " << const_cast<char*>(name) << " " << code << "\n";
+	if (PAPI_query_event(code) != PAPI_OK)
+	    throw papi_query_event_error();
+	if (PAPI_add_event(event_set, code) != PAPI_OK) 
+	    throw papi_add_event_error();
+	list_events();
+	return active_events++;
+    }
+
+    void start() 
+    {
+	if (PAPI_start(event_set) != PAPI_OK)
+	    throw papi_start_event_error();
+	reset();
+    }
+
+    void list_events()
+    {
+#if 0
+	int evv[8], num= 8;
+	PAPI_list_events(event_set, evv, &num);
+	for (int i= 0; i < num; i++ ) std::cout << evv[i] << "\n";
+#endif
+    }
+
+    bool is_event_supported(const char* name) const
+    {
+	int code;
+	return PAPI_event_name_to_code(const_cast<char*>(name), &code) == PAPI_OK 
+	       && PAPI_query_event(code) == PAPI_OK;
+    }
+
+    void reset()
+    {
+	if (PAPI_reset(event_set) != PAPI_OK) throw papi_reset_error();
+    }
+
+    void read()
+    {
+	list_events();
+	if (PAPI_read(event_set, counters) != PAPI_OK) throw papi_read_error();
+	// std::cout << "counters read, first value: " << *counters << "\n";
+    }
+
+    long_long operator[](int index) const
+    {
+	if (index < 0 || index >= active_events) throw papi_index_range_error();
+	return counters[index];
+    }
+
+    //private:
+    int num_counters, event_set, active_events;
+    long_long *counters;
+};
+
+#else // no papi
+
+// Faked papi type:
+
+struct papi_t
+{
+    const static bool true_papi = false;
+    int add_event(const char* name) { return 0;}
+    void start() {}
+    bool is_event_supported(const char* name) const { return false;}
+    void reset() {}
+    void read() {}
+    long long operator[](int index) const { return 0; }
+};
+ 
+#endif
+
+}} // namespace mtl::utility
+
+
+#endif // MTL_PAPI_INCLUDE
--- mtl4/trunk/libs/numeric/mtl/timing/no_inline_papi_timing.cpp
+++ mtl4/trunk/libs/numeric/mtl/timing/no_inline_papi_timing.cpp
@@ -6,6 +6,10 @@
 #include <vector>
 #include <boost/timer.hpp>
 
+#ifdef MTL_HAS_PAPI
+#  include <papi.h>
+#endif
+
 #include <boost/numeric/mtl/matrix/parameter.hpp>
 #include <boost/numeric/mtl/matrix/dense2D.hpp>
 #include <boost/numeric/mtl/matrix/morton_dense.hpp>
@@ -13,19 +17,34 @@
 #include <boost/numeric/mtl/operation/matrix_mult.hpp>
 #include <boost/numeric/mtl/operation/hessian_matrix_utility.hpp>
 #include <boost/numeric/mtl/operation/assign_mode.hpp>
-// #include <boost/numeric/mtl/recursion/recursive_matrix_mult.hpp>
+#include <boost/numeric/mtl/utility/papi.hpp>
 
 
-#ifdef __INTEL_COMPILER
-#  define MTL_DECL16  __declspec(align(16))
-#else
-#  define MTL_DECL16
-#endif
+
+// /san/intel/cce/9.0/bin/icc -g no_inline_papi_timing.cpp -o no_inline_papi_timing -I${MTL_BOOST_ROOT} -I${BOOST_ROOT} -I/usr/local/include -L/usr/local/lib -lpapi -DMTL_HAVE_PAPI -DMTL_HAS_BLAS -lblas -DMTL_HAS_LAPACK -llapack
+
+/* **** on odin ****
+setenv LD_LIBRARY_PATH /usr/local/lib64
+-I/usr/local/include -L/usr/local/lib -lpapi
+*  ****         ****/
+
+extern "C" {
+void dgemm_(const char* transa, const char* transb, 
+	    const int* m, const int* n, const int* k,
+	    const double* alpha,  const double *da,  const int* lda,
+	    const double *db, const int* ldb, const double* dbeta,
+	    double *dc, const int* ldc);
+
+// Cholesky factorization
+void dpotrf_(const char* uplo, const int* n, double *a, const int* ld, int* info);
+} // extern "C"
 
 
 using namespace mtl;
 using namespace mtl::recursion; 
 using namespace std;  
+
+
 
 // Maximum time for a single measurement
 // is 20 min
@@ -68,6 +87,15 @@ typedef gen_tiling_44_dense_mat_mat_mult_t<assign::plus_sum>  tiling_44_base_mul
 typedef gen_tiling_dense_mat_mat_mult_t<2, 4, assign::plus_sum>  tiling_24_base_mult_t;
 
 
+#ifdef MTL_HAS_PAPI
+int retval, Counters, EventSet=PAPI_NULL, aktivEvents=0, EventCode, l2_code, tlb_code, l2_index, tlb_index;
+long_long *values;
+#endif
+
+utility::papi_t papi;
+int tlbi= papi.add_event("PAPI_TLB_TL");
+int l2i= papi.add_event("PAPI_L2_TCM");
+
 
 void print_time_and_mflops(double time, double size)
 {
@@ -80,23 +108,65 @@ void print_time_and_mflops(double time, double size)
 template <typename MatrixA, typename MatrixB, typename MatrixC, typename Mult>
 void single_measure(MatrixA&, MatrixB&, MatrixC&, Mult mult, unsigned size, std::vector<int>& enabled, int i)
 {
+
+
     MatrixA a(size, size);
     MatrixB b(size, size);
     MatrixC c(size, size);
     fill_hessian_matrix(a, 1.0);
     fill_hessian_matrix(b, 1.0);
 
-    if (enabled[i]) {
+    if (enabled[i]) 
+    {
+
+
 	int reps= 0;
-	boost::timer start;	
+	boost::timer start;
+	papi.reset();
+
 	for (; start.elapsed() < 1; reps++)
 	    mult(a, b, c);
+	papi.read();
 	double time= start.elapsed() / double(reps);
+
 	print_time_and_mflops(time, a.num_rows());
+	std::cout << papi[tlbi] << ", " << papi[l2i] << ", ";
+
+
+#if 0
+
+	int evv[8], num= 8;
+	PAPI_list_events(EventSet, evv, &num);
+	for (int i= 0; i < num; i++ ) std::cout << evv[i] << "\n";
+
+	int reps= 0;
+	boost::timer start;
+#ifdef MTL_HAS_PAPI
+	if ((retval = PAPI_reset(EventSet)) != PAPI_OK) { printf("papi reset error\n"); }
+#endif
+
+	for (; start.elapsed() < 1; reps++)
+	    mult(a, b, c);
+
+#ifdef MTL_HAS_PAPI
+	if ((retval = PAPI_read(EventSet, values)) != PAPI_OK) { printf("papi read error\n"); }
+#endif
+
+	double time= start.elapsed() / double(reps);
+
+	print_time_and_mflops(time, a.num_rows());
+#ifdef MTL_HAS_PAPI
+	std::cout << *values << ", " << values[1] << ", ";
+#else
+	std::cout << ", , ";
+#endif
+
+#endif
+
 	if (time > max_time)
 	    enabled[i]= 0;
     } else
-	std::cout << ", , ";
+	std::cout << ", , , , ";
 }
 
 
@@ -136,34 +206,6 @@ struct no_inline3<Functor, void, Arg1, Arg2, Arg3>
 };
 #endif
 
-#if 0
-void hybrid_ext_mult_44(const morton_dense<double,  doppler_64_row_mask>& a, 
-			const morton_dense<double,  doppler_64_col_mask>& b,
-			morton_dense<double,  doppler_64_row_mask>& c);
-#endif
-
-void dense_ext_mult_44(const dense2D<double>& a,
-		       const dense2D<double, matrix_parameters<col_major> >& b,
-		       dense2D<double>& c);
- 
-struct ext_mult_44
-{
-#if 0
-    void operator()(const morton_dense<double,  doppler_64_row_mask>& a, 
-		    const morton_dense<double,  doppler_64_col_mask>& b,
-		    morton_dense<double,  doppler_64_row_mask>& c)
-    {
-	hybrid_ext_mult_44(a, b, c);
-    }
-#endif
-
-    void operator()(const dense2D<double>& a,
-		    const dense2D<double, matrix_parameters<col_major> >& b,
-		    dense2D<double>& c)
-    {
-	dense_ext_mult_44(a, b, c);
-    }
-};
 
 typedef dense2D<double> rmt;
 typedef dense2D<double, matrix_parameters<col_major> > cmt;
@@ -189,8 +231,8 @@ void mult_simple_ptu22t(const MatrixA& a, const MatrixB& b, MatrixC& c)
 	    int ld= b.num_rows();
 	    value_type tmp00= z, tmp01= z, tmp10= z, tmp11= z;
 
-	    MTL_DECL16 const value_type *begin_a= &aref[i][0], *end_a= &aref[i][a.num_cols()];
-	    MTL_DECL16 const value_type *begin_b= &bref[0][k];
+	    const value_type *begin_a= &aref[i][0], *end_a= &aref[i][a.num_cols()];
+	    const value_type *begin_b= &bref[0][k];
 	    for (; begin_a != end_a; begin_a+= aci, begin_b+= bri) {
 		tmp00+= *begin_a * *begin_b;
 		tmp01+= *begin_a * *(begin_b+bci);
@@ -208,7 +250,6 @@ void mult_simple_ptu22t(const MatrixA& a, const MatrixB& b, MatrixC& c)
 #endif
 	}
 }
-
 
 template <typename Matrix, typename MatrixB> 
 void measure_unrolling(unsigned size, std::vector<int>& enabled, Matrix& matrix, MatrixB& matrixb)
@@ -231,25 +272,20 @@ void measure_unrolling(unsigned size, std::vector<int>& enabled, Matrix& matrix,
       tiling_44_t;
     typedef no_inline3<tiling_44_t, void, const Matrix, const MatrixB, Matrix> tiling_44_no_inline_t;
 
-    gen_recursive_dense_mat_mat_mult_t<ext_mult_44>           rec_ext_mult_44;
+    // gen_recursive_dense_mat_mat_mult_t<ext_mult_44>           rec_ext_mult_44;
 
     typedef typename base_case_matrix<Matrix, test64_t>::type    BaseMatrix;
     typedef typename base_case_matrix<MatrixB, test64_t>::type   BaseMatrixB;
     typedef no_inline3<tiling_44_t, void, const BaseMatrix, const BaseMatrixB, BaseMatrix> tiling_44_base_t;
     gen_recursive_dense_mat_mat_mult_t<tiling_44_base_mult_t>    rec_no_inline_mult_44;
     
-
+    
+    
     single_measure(matrix, matrixb, matrix, mult, size, enabled, 0);
     single_measure(matrix, matrixb, matrix, mult_22, size, enabled, 1);
     single_measure(matrix, matrixb, matrix, mult_44, size, enabled, 2);
-    single_measure(matrix, matrixb, matrix, tiling_22_no_inline_t(), size, enabled, 3);
-    single_measure(matrix, matrixb, matrix, tiling_44_no_inline_t(), size, enabled, 4);
-    single_measure(matrix, matrixb, matrix, rec_ext_mult_44, size, enabled, 5);
-    single_measure(matrix, matrixb, matrix, rec_no_inline_mult_44, size, enabled, 6);
-    single_measure(dense, denseb, dense, tiling_22_base_mult_t(), size, enabled, 7);
-    single_measure(dense, denseb, dense, tiling_44_base_mult_t(), size, enabled, 8);
     single_measure(dense, denseb, dense, mult_simple_ptu22t<rmt, cmt, rmt>, size, enabled, 9);
-    //single_measure(matrix, matrixb, matrix, mult_24, size, enabled, 10);
+
     std::cout << "0\n";  std::cout.flush();
 }
 
@@ -282,9 +318,78 @@ void series(unsigned steps, unsigned max_size, Measure measure, const string& co
 }
 
 
+void init_papi()
+{
+#ifdef MTL_HAS_PAPI
+
+  retval = PAPI_library_init(PAPI_VER_CURRENT);
+  if (retval != PAPI_VER_CURRENT && retval > 0) {
+    printf("PAPI Library version mismatch! Library not initialized\n");
+    exit(1);
+  }
+  
+  if ((retval = PAPI_get_opt(PAPI_MAX_HWCTRS, NULL)) <= PAPI_OK) { printf("papi error\n");  exit(1);}
+  else {
+    Counters =retval;
+    printf("%i Counters found = OK\n",Counters);
+  }
+  if ((values = (long_long*)calloc((size_t)Counters,sizeof(long_long))) == NULL ) {
+    fprintf(stderr,"Allocation memory for countervalues failed");  exit(1);}
+
+
+  retval = PAPI_create_eventset(&EventSet);
+  if (retval != PAPI_OK) { printf("papi error (create_eventset)\n"); exit(1);}
+  else printf("Create EventSet = OK (%i)\n", EventSet);
+
+
+  if ((retval = PAPI_event_name_to_code("PAPI_TLB_TL",&tlb_code)) != PAPI_OK ) 
+    {printf("event_anme_to_code papi error\n"); exit(1);}
+  else if ((retval = PAPI_query_event(tlb_code)) != PAPI_OK) { printf("Can't add event : ->"); }
+  else if ((retval = PAPI_add_event(EventSet,tlb_code)) != PAPI_OK) {printf("papi add_event error\n"); exit(1);}
+  else {printf("added to Eventset = OK\n"); aktivEvents++;}
+  tlb_index= 0;
+
+  if ((retval = PAPI_event_name_to_code("PAPI_L2_TCM",&l2_code)) != PAPI_OK ) 
+    {printf("event_anme_to_code papi error\n"); exit(1);}
+  else if ((retval = PAPI_query_event(l2_code)) != PAPI_OK) { printf("Can't add event : ->"); }
+  else if ((retval = PAPI_add_event(EventSet, l2_code)) != PAPI_OK) {printf("papi add_event error\n"); exit(1);}
+  else {printf("added to Eventset = OK\n"); l2_index= aktivEvents++;}
+  
+
+  if ((retval = PAPI_start(EventSet)) != PAPI_OK) { printf("papi start error\n");exit(1);}
+  
+  if ((retval = PAPI_reset(EventSet)) != PAPI_OK) { printf("papi reset error\n");exit(1);}
+
+#endif
+}
+
+
+
+void test_blas()
+{
+    dense2D<double> a(4, 4), c(4, 4);
+    dense2D<double, matrix_parameters<col_major> >    b(4, 4);
+    int size= 4; double one= 1.0;
+
+#ifdef MTL_HAS_BLAS
+    // cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, 4, 4, 4, 1.0, &a[0][0], 4, &b[0][0], 4, 1.0, &c[0][0], 4);
+    dgemm_("N", "T", &size, &size, &size, &one, &a[0][0], &size, &b[0][0], &size, &one, &c[0][0], &size);
+
+#endif
+
+#ifdef MTL_HAS_LAPACK
+    int info;
+    dpotrf_("U", &size, &a[0][0], &size, &info);
+#endif
+}
+
 
 int main(int argc, char* argv[])
 {
+    //init_papi();
+    papi.start();
+
+    test_blas();
 
     std::vector<std::string> scenarii;
     scenarii.push_back(string("Comparing different unrolling for hybrid row-major matrices"));
