8a5b3da0de98d33fe5e9338b70f5a738beab4066
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2007-04-03 12:46:30 +0000

    Standard concepts for result_type of operations; macros for exceptions

---
+++ mtl4/trunk/boost/numeric/mtl/concept/std_concept.hpp
@@ -0,0 +1,126 @@
+// $COPYRIGHT$
+
+#ifndef MTL_STD_CONCEPT_INCLUDE
+#define MTL_STD_CONCEPT_INCLUDE
+
+#ifdef __GXX_CONCEPTS__
+#  include <concepts>
+#else
+// Use Joel de Guzman's return type deduction
+#  include <boost/numeric/ublas/detail/returntype_deduction.hpp>
+#  include <boost/mpl/at.hpp>
+#endif
+
+namespace mtl {
+
+#ifdef __GXX_CONCEPTS__
+
+using std::Addable;
+using std::Subtractable;
+using std::Multiplicable;
+
+auto concept Divisible<typename T, typename U = T>
+{
+  typename result_type;
+  result_type operator/(const T& t, const U& u);
+};
+
+#else // without concepts
+
+// Use Joel de Guzman's return type deduction
+// Adapted from uBLAS
+// Differences: 
+//   - Separate types for all operations
+//   - result_type like in concept
+
+
+template<class X, class Y>
+class Addable
+{
+    typedef boost::numeric::ublas::type_deduction_detail::base_result_of<X, Y> base_type;
+    static typename base_type::x_type x;
+    static typename base_type::y_type y;
+    static const std::size_t size = sizeof (
+               boost::numeric::ublas::type_deduction_detail::test<
+                    typename base_type::x_type
+                  , typename base_type::y_type
+                >(x + y)     
+            );
+
+    static const std::size_t index = (size / sizeof (char)) - 1;
+    typedef typename boost::mpl::at_c<
+	typename base_type::types, index>::type id;
+public:
+    typedef typename id::type result_type;
+};
+
+
+template<class X, class Y>
+class Subtractable
+{
+    typedef boost::numeric::ublas::type_deduction_detail::base_result_of<X, Y> base_type;
+    static typename base_type::x_type x;
+    static typename base_type::y_type y;
+    static const std::size_t size = sizeof (
+               boost::numeric::ublas::type_deduction_detail::test<
+                    typename base_type::x_type
+                  , typename base_type::y_type
+                >(x - y)     
+            );
+
+    static const std::size_t index = (size / sizeof (char)) - 1;
+    typedef typename boost::mpl::at_c<
+	typename base_type::types, index>::type id;
+public:
+    typedef typename id::type result_type;
+};
+
+template<class X, class Y>
+class Multiplicable
+{
+    typedef boost::numeric::ublas::type_deduction_detail::base_result_of<X, Y> base_type;
+    static typename base_type::x_type x;
+    static typename base_type::y_type y;
+    static const std::size_t size = sizeof (
+               boost::numeric::ublas::type_deduction_detail::test<
+                    typename base_type::x_type
+                  , typename base_type::y_type
+                >(x * y)     
+            );
+
+    static const std::size_t index = (size / sizeof (char)) - 1;
+    typedef typename boost::mpl::at_c<
+	typename base_type::types, index>::type id;
+public:
+    typedef typename id::type result_type;
+};
+
+template<class X, class Y>
+class Divisible
+{
+    typedef boost::numeric::ublas::type_deduction_detail::base_result_of<X, Y> base_type;
+    static typename base_type::x_type x;
+    static typename base_type::y_type y;
+    static const std::size_t size = sizeof (
+               boost::numeric::ublas::type_deduction_detail::test<
+                    typename base_type::x_type
+                  , typename base_type::y_type
+                >(x * y)     
+            );
+
+    static const std::size_t index = (size / sizeof (char)) - 1;
+    typedef typename boost::mpl::at_c<
+	typename base_type::types, index>::type id;
+public:
+    typedef typename id::type result_type;
+};
+
+
+
+
+
+#endif;
+
+} // namespace mtl
+
+#endif // MTL_STD_CONCEPT_INCLUDE
--- mtl4/trunk/boost/numeric/mtl/utility/sfunctor.hpp
+++ mtl4/trunk/boost/numeric/mtl/utility/sfunctor.hpp
@@ -3,13 +3,14 @@
 #ifndef MTL_SFUNCTOR_INCLUDE
 #define MTL_SFUNCTOR_INCLUDE
 
+#include <boost/numeric/mtl/utility/std_concept.hpp>
+
 namespace mtl { namespace sfunctor {
 
 template <typename Value1, typename Value2>
 struct plus
 {
-    // temporary solution
-    typedef Value1 result_type;
+    typedef typename Addable<Value1, Value2>::result_type result_type;
 
     static inline result_type apply(const Value1& v1, const Value2& v2)
     {
@@ -20,8 +21,7 @@ struct plus
 template <typename Value1, typename Value2>
 struct minus
 {
-    // temporary solution
-    typedef Value1 result_type;
+    typedef typename Subtractable<Value1, Value2>::result_type result_type;
 
     static inline result_type apply(const Value1& v1, const Value2& v2)
     {
@@ -30,9 +30,30 @@ struct minus
 };
 
 template <typename Value1, typename Value2>
+struct times
+{
+    typedef typename Multiplicable<Value1, Value2>::result_type result_type;
+
+    static inline result_type apply(const Value1& v1, const Value2& v2)
+    {
+	return v1 * v2;
+    }
+};
+
+template <typename Value1, typename Value2>
+struct divide
+{
+    typedef typename Divisible<Value1, Value2>::result_type result_type;
+
+    static inline result_type apply(const Value1& v1, const Value2& v2)
+    {
+	return v1 / v2;
+    }
+};
+
+template <typename Value1, typename Value2>
 struct assign
 {
-    // temporary solution
     typedef Value1& result_type;
 
     static inline result_type apply(Value1& v1, const Value2& v2)
@@ -60,6 +81,28 @@ struct minus_assign
     static inline result_type apply(Value1& v1, const Value2& v2)
     {
 	return v1-= v2;
+    }
+};
+
+template <typename Value1, typename Value2>
+struct times_assign
+{
+    typedef Value1& result_type;
+
+    static inline result_type apply(Value1& v1, const Value2& v2)
+    {
+	return v1*= v2;
+    }
+};
+
+template <typename Value1, typename Value2>
+struct divide_assign
+{
+    typedef Value1& result_type;
+
+    static inline result_type apply(Value1& v1, const Value2& v2)
+    {
+	return v1/= v2;
     }
 };
 
