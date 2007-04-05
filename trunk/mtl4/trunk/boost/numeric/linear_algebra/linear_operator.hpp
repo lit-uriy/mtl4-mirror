61ed309a61e47e85cc20a339e864995d4232cd68
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2007-04-05 00:31:02 +0000

    more linear operators

--- trunk/mtl4/trunk/boost/numeric/linear_algebra/linear_operator.hpp
+++ trunk/mtl4/trunk/boost/numeric/linear_algebra/linear_operator.hpp
@@ -27,15 +27,15 @@ namespace math {
 	typename minus_assign_type;
 	minus_assign_type operator+= (VectorImage, result_type);
 
-	axiom Addability(Operator op, VectorDomain x, VectorDomain y)
+	axiom Addability(Operator a, VectorDomain x, VectorDomain y)
 	{
-	    op(x + y) == op(x) + op(y);
+	    a * (x + y) == a*x  + a*y;
 	}
 
 	// The two vector spaces must be scalable with the same scalar types
-	axiom Scalability(Operator op, VectorSpace<VectorDomain>::scalar_type a, VectorDomain x)
+	axiom Scalability(Operator a, VectorSpace<VectorDomain>::scalar_type alpha, VectorDomain x)
 	{
-	    op( a * x ) == a * op(x);
+	    a * (alpha * x) == alpha * (a * x);
 	}
     };
 #else
@@ -51,38 +51,140 @@ namespace math {
 	- VectorSpace <VectorDomain>
 	- VectorSpace <VectorImage>
 
+	\par Associated Types:
+	- result_type
+
         \par Notation:
         <table summary="notation">
           <tr>
-            <td>op</td>
+            <td>a</td>
 	    <td>Object of type Operation</td>
 	  </tr>
           <tr>
             <td>x, y</td>
 	    <td>Objects of type VectorDomain</td>
           </tr>
+          <tr>
+            <td>u</td>
+	    <td>Object of type VectorImage</td>
+          </tr>
+        </table>
+
+        \par Valid Expressions:
+        <table>
+          <tr>
+            <td>Assign product:</td>
+	    <td>u= a * x</td>
+	  </tr>
+          <tr>
+            <td>Add product:</td>
+	    <td>u+= a * x</td>
+	  </tr>
+          <tr>
+            <td>Subtract product:</td>
+	    <td>u-= a * x</td>
+	  </tr>
         </table>
 
         \invariant
         <table summary="invariants">
           <tr>
             <td>Addability</td>
-	    <td>op(x + y) == op(x) + op(y)</td>
+	    <td>a * (x + y) == a*x + a*y</td>
           </tr>
           <tr>
             <td>Scalability</td>
-	    <td>op( a * x ) == a * op(x)</td>
+	    <td>alpha * (a * x) == a * (alpha * x)</td>
           </tr>
         </table>
 	
+	\note
+	-# Using matrix vector products in arbitrary expressions requires
+	   storing it in temporary objects to avoid redundant computation.
+	   On the other hand, it is not always obvious to choose an appropriate
+	   type for such temporary depending on arbitrary operator and vector types.
+	   Using the products directly in assignments allows implementation without
+	   temporaries, e.g., by calling a function mult(a, x, u) internally.
      */
-   template <typename Operator, typename VectorDomain, typename VectorImage>
+    template <typename Operator, typename VectorDomain, typename VectorImage>
     struct LinearOperator
     {
 	typename result_type;
 	result_type operator* (Operator, VectorDomain);
 
-    }
+    };
+#endif
+
+
+#ifdef __GXX_CONCEPTS__
+    concept SelfAdjointOperator<typename Operator, typename VectorDomain, typename VectorImage>
+      : LinearOperator<Operator, VectorDomain, VectorImage>
+    {};
+#else
+    //! Concept SelfAdjointOperator
+    /*!
+        
+
+        \param Operator The type of the operator, e.g., some matrix type
+	\param VectorDomain The the type of a vector in the domain vector space
+	\param VectorImage The the type of a vector in the image vector space
+	
+        \par Refinement of:
+	- LinearOperator <Operator, VectorDomain, VectorImage>
+    */
+    template <typename Operator, typename VectorDomain, typename VectorImage>
+    struct SelfAdjointOperator
+      : LinearOperator<Operator, VectorDomain, VectorImage>
+    {};
+#endif
+
+
+#ifdef __GXX_CONCEPTS__
+    concept RealOperator<typename Operator, typename VectorDomain, typename VectorImage>
+      : LinearOperator<Operator, VectorDomain, VectorImage>
+    {};
+#else
+    //! Concept RealOperator
+    /*!
+        
+
+        \param Operator The type of the operator, e.g., some matrix type
+	\param VectorDomain The the type of a vector in the domain vector space
+	\param VectorImage The the type of a vector in the image vector space
+	
+        \par Refinement of:
+	- LinearOperator <Operator, VectorDomain, VectorImage>
+    */
+    template <typename Operator, typename VectorDomain, typename VectorImage>
+    struct RealOperator
+      : LinearOperator<Operator, VectorDomain, VectorImage>
+    {};
+#endif
+
+
+#ifdef __GXX_CONCEPTS__
+    concept SymmetricOperator<typename Operator, typename VectorDomain, typename VectorImage>
+      : SelfAdjointOperator<Operator, VectorDomain, VectorImage>,
+        RealOperator<Operator, VectorDomain, VectorImage>
+    {};
+#else
+    //! Concept SymmetricOperator
+    /*!
+        
+
+        \param Operator The type of the operator, e.g., some matrix type
+	\param VectorDomain The the type of a vector in the domain vector space
+	\param VectorImage The the type of a vector in the image vector space
+	
+        \par Refinement of:
+	- SelfAdjointOperator <Operator, VectorDomain, VectorImage>
+	- RealOperator <Operator, VectorDomain, VectorImage>
+    */
+    template <typename Operator, typename VectorDomain, typename VectorImage>
+    struct SymmetricOperator
+      : SelfAdjointOperator<Operator, VectorDomain, VectorImage>,
+        RealOperator<Operator, VectorDomain, VectorImage>
+    {};
 #endif
 
 } // namespace math
