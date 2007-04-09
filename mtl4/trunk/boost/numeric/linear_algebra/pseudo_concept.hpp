e2349c6fa408df7127a7bddba3b0da5844832cf9
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2007-04-09 23:28:10 +0000

    types to fake concept syntas for documentation

---
+++ mtl4/trunk/boost/numeric/linear_algebra/pseudo_concept.hpp
@@ -0,0 +1,19 @@
+// $COPYRIGHT$
+
+#ifndef PSEUDO_CONCEPT_INCLUDE
+#define PSEUDO_CONCEPT_INCLUDE
+
+
+#ifndef __GXX_CONCEPTS__
+
+//! Pseudo type for invariants in concepts
+/// Pseudo type used to document invariants in concepts
+struct axiom {};
+
+//! Pseudo type used to document associated types in concepts
+/// Pseudo type for associated types in concepts
+struct associated_type {};
+
+#endif // __GXX_CONCEPTS__
+
+#endif // PSEUDO_CONCEPT_INCLUDE
