ef691b0ddb11c7412272e1a1496dab9ad9e70ee3
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2007-05-09 15:54:36 +0000

    replaced +1 by -1

--- trunk/mtl4/trunk/boost/numeric/mtl/matrix/laplacian_setup.hpp
+++ trunk/mtl4/trunk/boost/numeric/mtl/matrix/laplacian_setup.hpp
@@ -18,13 +18,13 @@ inline void laplacian_setup(Matrix& matrix, unsigned dim1, unsigned dim2)
 
     for (unsigned i= 0; i < dim1; i++)
 	for (unsigned j= 0; j < dim2; j++) {
-	    typename Collection<Matrix>::value_type four(4.0), one(1.0);
+	    typename Collection<Matrix>::value_type four(4.0), minus_one(-1.0);
 	    unsigned row= i * dim2 + j;
 	    ins(row, row) << four;
-	    if (j < dim2-1) ins(row, row+1) << one;
-	    if (i < dim1-1) ins(row, row+dim2) << one;
-	    if (j > 0) ins(row, row-1) << one;
-	    if (i > 0) ins(row, row-dim2) << one;
+	    if (j < dim2-1) ins(row, row+1) << minus_one;
+	    if (i < dim1-1) ins(row, row+dim2) << minus_one;
+	    if (j > 0) ins(row, row-1) << minus_one;
+	    if (i > 0) ins(row, row-dim2) << minus_one;
 	}
 }
 
