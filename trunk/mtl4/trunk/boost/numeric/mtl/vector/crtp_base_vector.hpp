037c83ff9566a158670715aaa366bacbeaf929bf
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2007-05-17 19:52:03 +0000

    Matrix vector product works in operator notation

--- trunk/mtl4/trunk/boost/numeric/mtl/vector/crtp_base_vector.hpp
+++ trunk/mtl4/trunk/boost/numeric/mtl/vector/crtp_base_vector.hpp
@@ -3,7 +3,11 @@
 #ifndef MTL_CRTP_BASE_VECTOR_INCLUDE
 #define MTL_CRTP_BASE_VECTOR_INCLUDE
 
+#include <boost/numeric/mtl/mtl_fwd.hpp>
 #include <boost/numeric/mtl/vector/all_vec_expr.hpp>
+#include <boost/numeric/mtl/operation/mat_cvec_times_expr.hpp>
+#include <boost/numeric/mtl/operation/mult.hpp>
+#include <boost/numeric/mtl/operation/mult_assign_mode.hpp>
 
 namespace mtl { namespace vector {
 
@@ -12,7 +16,7 @@ namespace mtl { namespace vector {
 template <typename Vector, typename ValueType, typename SizeType>
 struct crtp_vector_assign
 {
-
+    /// Assign vector expression
     template <class E>
     vec_vec_asgn_expr<Vector, E> operator=( vec_expr<E> const& e )
     {
@@ -20,6 +24,17 @@ struct crtp_vector_assign
 	return vec_vec_asgn_expr<Vector, E>( static_cast<Vector&>(*this), e.ref );
     }
 
+    /// Assign matrix vector product by calling mult
+    /** Note that this does not work for arbitrary expressions. **/
+    template <typename E1, typename E2>
+    Vector& operator=(const mat_cvec_times_expr<E1, E2>& src)
+    {
+	mult(src.first, src.second, static_cast<Vector&>(*this));
+
+	return static_cast<Vector&>(*this);
+    }
+
+    /// Assign-add vector expression
     template <class E>
     vec_vec_plus_asgn_expr<Vector, E> operator+=( vec_expr<E> const& e )
     {
@@ -27,6 +42,18 @@ struct crtp_vector_assign
 	return vec_vec_plus_asgn_expr<Vector, E>( static_cast<Vector&>(*this), e.ref );
     }
 
+    /// Assign-add matrix vector product by calling mult
+    /** Note that this does not work for arbitrary expressions. **/
+    template <typename E1, typename E2>
+    Vector& operator+=(const mat_cvec_times_expr<E1, E2>& src)
+    {
+	gen_mult(src.first, src.second, static_cast<Vector&>(*this),
+		 assign::plus_sum(), tag::matrix(), tag::vector(), tag::vector());
+
+	return static_cast<Vector&>(*this);
+    }
+
+    /// Assign-subtract vector expression
     template <class E>
     vec_vec_minus_asgn_expr<Vector, E> operator-=( vec_expr<E> const& e )
     {
@@ -34,7 +61,16 @@ struct crtp_vector_assign
 	return vec_vec_minus_asgn_expr<Vector, E>( static_cast<Vector&>(*this), e.ref );
     }
 
+    /// Assign-subtract matrix vector product by calling mult
+    /** Note that this does not work for arbitrary expressions. **/
+    template <typename E1, typename E2>
+    Vector& operator-=(const mat_cvec_times_expr<E1, E2>& src)
+    {
+	gen_mult(src.first, src.second, static_cast<Vector&>(*this),
+		 assign::minus_sum(), tag::matrix(), tag::vector(), tag::vector());
 
+	return static_cast<Vector&>(*this);
+    }
 };
 
 
