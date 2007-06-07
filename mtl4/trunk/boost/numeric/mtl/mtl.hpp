6cee84d7a9c5e4007082ace6472af29e23df30c2
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2007-06-07 18:56:12 +0000

    some files to bundle includes

---
+++ mtl4/trunk/boost/numeric/mtl/mtl.hpp
@@ -0,0 +1,9 @@
+// $COPYRIGHT$
+
+#ifndef MTL_MTL_INCLUDE
+#define MTL_MTL_INCLUDE
+
+#include <boost/numeric/mtl/types.hpp>
+#include <boost/numeric/mtl/operations.hpp>
+
+#endif // MTL_MTL_INCLUDE
---
+++ mtl4/trunk/boost/numeric/mtl/types.hpp
@@ -0,0 +1,9 @@
+// $COPYRIGHT$
+
+#ifndef MTL_TYPES_INCLUDE
+#define MTL_TYPES_INCLUDE
+
+#include <boost/numeric/mtl/vectors.hpp>
+#include <boost/numeric/mtl/matrices.hpp>
+
+#endif // MTL_TYPES_INCLUDE
