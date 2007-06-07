8b17a4535a124fe6174f24de77e0a1a65d84be47
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2007-06-07 18:56:12 +0000

    some files to bundle includes

---
+++ trunk/mtl4/trunk/boost/numeric/mtl/vectors.hpp
@@ -0,0 +1,9 @@
+// $COPYRIGHT$
+
+#ifndef MTL_VECTORS_INCLUDE
+#define MTL_VECTORS_INCLUDE
+
+#include <boost/numeric/mtl/vector/dense_vector.hpp>
+#include <boost/numeric/mtl/vector/parameters.hpp>
+
+#endif // MTL_VECTORS_INCLUDE
