26c27292ab3b3ac718e333884c8653813f42815d
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2007-06-08 20:22:15 +0000

    documenting expression templates and rich expression templates

---
+++ mtl4/trunk/libs/numeric/mtl/examples/dot.cpp
@@ -0,0 +1,23 @@
+// File: dot.cpp
+
+#include <iostream>
+#include <boost/numeric/mtl/mtl.hpp>
+
+int main(int argc, char* argv[])
+{
+    using namespace mtl;
+
+    typedef std::complex<double>  cdouble;
+    dense_vector<cdouble>         v(10000);
+    dense_vector<double>          w(10000);
+
+    for (int i= 0; i < size(v); i++)
+	v[i]= cdouble(i+1, 10000-i), w[i]= 2 * i + 2;
+
+    std::cout << "dot(v, w) is " << dot(v, w)<< "\n";
+    
+    std::cout << "dot<6>(v, w) is " <<  dot<6>(v, w)<< "\n";
+
+    return 0;
+}
+
