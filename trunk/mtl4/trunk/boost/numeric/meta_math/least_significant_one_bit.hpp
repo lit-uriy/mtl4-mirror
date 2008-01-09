9d5f6a627fd1294a357715c54de1c0c2bb2f0154
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2008-01-09 18:34:01 +0000

    License info is already in repository

--- trunk/mtl4/trunk/boost/numeric/meta_math/least_significant_one_bit.hpp
+++ trunk/mtl4/trunk/boost/numeric/meta_math/least_significant_one_bit.hpp
@@ -1,4 +1,11 @@
-// $COPYRIGHT$
+// Software License for MTL
+// 
+// Copyright (c) 2007 The Trustees of Indiana University. All rights reserved.
+// Authors: Peter Gottschling and Andrew Lumsdaine
+// 
+// This file is part of the Matrix Template Library
+// 
+// See also license.mtl.txt in the distribution.
 
 #ifndef META_MATH_LEAST_SIGNIFICANT_ONE_BIT_INCLUDE
 #define META_MATH_LEAST_SIGNIFICANT_ONE_BIT_INCLUDE
--- trunk/mtl4/trunk/boost/numeric/mtl/recursion/matrix_recursator.hpp
+++ trunk/mtl4/trunk/boost/numeric/mtl/recursion/matrix_recursator.hpp
@@ -1,4 +1,11 @@
-// $COPYRIGHT$
+// Software License for MTL
+// 
+// Copyright (c) 2007 The Trustees of Indiana University. All rights reserved.
+// Authors: Peter Gottschling and Andrew Lumsdaine
+// 
+// This file is part of the Matrix Template Library
+// 
+// See also license.mtl.txt in the distribution.
 
 #ifndef MTL_MATRIX_RECURATOR_INCLUDE
 #define MTL_MATRIX_RECURATOR_INCLUDE
