45e2e02d22f5a024e20aaec23a9b030d151139a8
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2008-04-15 14:23:45 +0000

    Dead code removed.

--- trunk/mtl4/trunk/libs/numeric/linear_algebra/test/accumulation.hpp
+++ trunk/mtl4/trunk/libs/numeric/linear_algebra/test/accumulation.hpp
@@ -1,3 +1,7 @@
+#ifndef MTL_ACCUMULATION_TEST_INCLUDE
+#define MTL_ACCUMULATION_TEST_INCLUDE
+
+
 #include <iostream>
 #include <algorithm>
 #include <concepts>
@@ -35,6 +39,7 @@ Value inline accumulate(Iter first, Iter last, Value init, Op op)
     }
     for (; i < size; i++)
 	t0= op(t0, Value(first[i]));
+
     t0= op(t0, t1), t2= op(t2, t3), t0= op(t0, t2);
     return t0;
 }
@@ -54,118 +59,6 @@ Value inline accumulate(Iter first, Iter last, Value init, Op op)
     return init;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#if 0
-
-template <std::ForwardIterator Iter, typename Value, typename Op>
-     requires std::Convertible<Value, std::ForwardIterator<Iter>::value_type>
-//&& math::Magma<Op, std::ForwardIterator<Iter>::value_type> )
-typename std::ForwardIterator<Iter>::value_type 
-inline accumulate_simple(Iter first, Iter last, Value init, Op op)
-{
-    typedef typename std::RandomAccessIterator<Iter>::value_type value_type;
-    value_type        t0= init;
-    
-    // std::cout << "accumulate_simple\n";
-    for (; first != last; ++first)
-	t0= op(t0, *first);
-    return t0;
-}
-#endif
-
-#if 0
-template <std::RandomAccessIterator Iter, typename Value, typename Op>
-  requires std::Convertible<Value, std::RandomAccessIterator<Iter>::value_type>
-        && std::Callable2<Op, std::RandomAccessIterator<Iter>::value_type, 
-			  std::RandomAccessIterator<Iter>::value_type>
-        && std::MoveAssignable<std::RandomAccessIterator<Iter>::value_type,
-			       std::Callable2<Op, std::RandomAccessIterator<Iter>::value_type, 
-					      std::RandomAccessIterator<Iter>::value_type
-			       >::result_type>
-        && std::CopyConstructible<std::RandomAccessIterator<Iter>::value_type>
-        && math::Commutative<Op, std::RandomAccessIterator<Iter>::value_type> 
-        && math::Monoid<Op, std::RandomAccessIterator<Iter>::value_type> 
-        // && math::Commutative<Op, Value> 
-        // && math::Monoid<Op, Value> 
-        && std::Convertible<math::Monoid<Op, std::RandomAccessIterator<Iter>::value_type>::identity_result_type,
-			    std::RandomAccessIterator<Iter>::value_type>
-typename std::RandomAccessIterator<Iter>::value_type 
-inline accumulate_unrolled(Iter first, Iter last, Value init, Op op)
-{
-    std::cout << "Unrolled accumulate\n";
-
-    typedef typename std::RandomAccessIterator<Iter>::value_type value_type;
-    typedef typename std::RandomAccessIterator<Iter>::difference_type difference_type;
-    value_type        t0= identity(op, init), t1= identity(op, init), 
-	              t2= identity(op, init), t3= init;
-    difference_type   size= last - first, bsize= size >> 2 << 2, i;
-    
-    for (i= 0; i < bsize; i+= 4) {
-	t0= op(t0, first[i]);
-	t1= op(t1, first[i+1]);
-	t2= op(t2, first[i+2]);
-	t3= op(t3, first[i+3]);
-    }
-    for (; i < size; i++)
-	t0= op(t0, first[i]);
-    t0= op(t0, t1), t2= op(t2, t3), t0= op(t0, t2);
-    return t0;
-}
-#endif
-
-
-#if 0
-template <typename Iter, typename Value, typename Op>
-  requires std::RandomAccessIterator<Iter> 
-	          && std::Convertible<Value, std::RandomAccessIterator<Iter>::value_type>
-		  && math::CommutativeMonoid<Op, std::RandomAccessIterator<Iter>::value_type> )
-typename std::RandomAccessIterator<Iter>::value_type 
-inline accumulate(Iter first, Iter last, Value init, Op op)
-{
-    std::cout << "Unrolled accumulate\n";
-    return accumulate_unrolled(first, last, init, op);
-}
-
-#endif
-
-#if 0
-// Dispatching between simple and unrolled version
-template <std::ForwardIterator Iter, typename Value, typename Op>
-     requires std::Convertible<Value, std::ForwardIterator<Iter>::value_type>
-           && std::Callable2<Op, std::ForwardIterator<Iter>::value_type, std::ForwardIterator<Iter>::value_type>
-           && std::MoveAssignable<std::ForwardIterator<Iter>::value_type,
-				  std::Callable2<Op, std::ForwardIterator<Iter>::value_type, 
-						 std::ForwardIterator<Iter>::value_type
-				                >::result_type>
-           && std::CopyConstructible<std::ForwardIterator<Iter>::value_type>
-typename std::ForwardIterator<Iter>::value_type 
-inline accumulate(Iter first, Iter last, Value init, Op op)
-{
-    std::cout << "Simple accumulate\n";
-    typedef typename std::ForwardIterator<Iter>::value_type value_type;
-    value_type        t0= init;
-    
-    for (; first != last; ++first)
-	t0= op(t0, *first);
-    return t0;
-}
-#endif
-
-
 } // namespace mtl
+
+#endif // MTL_ACCUMULATION_TEST_INCLUDE
