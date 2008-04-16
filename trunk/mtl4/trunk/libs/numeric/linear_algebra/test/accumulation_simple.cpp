8867e6b3892a47e9d7bf8950f1f11ecf8353c9ba
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2008-04-16 07:50:56 +0000

    Refactoring accumulation.

--- trunk/mtl4/trunk/libs/numeric/linear_algebra/test/accumulation_simple.cpp
+++ trunk/mtl4/trunk/libs/numeric/linear_algebra/test/accumulation_simple.cpp
@@ -9,21 +9,19 @@
 template <typename Element>
 void test_accumulate(const char* name)
 {
-    using math::accumulate; using std::cout;
-
     const int   array_size= 10;
     Element     array[array_size];
     for (int i= 0; i < array_size; i++) 
     	array[i]= Element(i);
     
     std::cout << '\n' << name << '\n' << " Add: ";
-    mtl::accumulate(&array[0], array+array_size, Element(0), math::add<Element>());
+    math::accumulate(&array[0], array+array_size, Element(0), math::add<Element>());
     std::cout << "Mult: ";
-    mtl::accumulate(array, array+array_size, Element(1), math::mult<Element>());
+    math::accumulate(array, array+array_size, Element(1), math::mult<Element>());
     std::cout << " Min: ";
-    mtl::accumulate(array, array+array_size, Element(1), math::min<Element>());
+    math::accumulate(array, array+array_size, Element(1), math::min<Element>());
     std::cout << " Max: ";
-    mtl::accumulate(array, array+array_size, Element(1), math::max<Element>());
+    math::accumulate(array, array+array_size, Element(1), math::max<Element>());
 }
 
 
