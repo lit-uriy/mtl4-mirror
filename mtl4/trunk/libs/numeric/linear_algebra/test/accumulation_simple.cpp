9760b8d5e967077ad0fbab8c9ecb88355d617811
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2008-04-16 08:52:02 +0000

    Added list example.

--- mtl4/trunk/libs/numeric/linear_algebra/test/accumulation_simple.cpp
+++ mtl4/trunk/libs/numeric/linear_algebra/test/accumulation_simple.cpp
@@ -1,9 +1,19 @@
+// Software License for MTL
+// 
+// Copyright (c) 2007-2008 The Trustees of Indiana University. All rights reserved.
+// Authors: Peter Gottschling and Andrew Lumsdaine
+// 
+// This file is part of the Matrix Template Library
+// 
+// See also license.mtl.txt in the distribution.
+
+#include <boost/timer.hpp>
+#include <iostream>
+#include <list>
 
 #include <boost/numeric/linear_algebra/accumulate.hpp>
 #include <boost/numeric/linear_algebra/concept_maps.hpp>
 #include <boost/numeric/linear_algebra/operators.hpp>
-
-#include <boost/timer.hpp>
 
 
 template <typename Element>
@@ -13,15 +23,21 @@ void test_accumulate(const char* name)
     Element     array[array_size];
     for (int i= 0; i < array_size; i++) 
     	array[i]= Element(i);
+
+    std::list<Element> l;
+    for (int i= 0; i < array_size; i++) 
+    	l.push_back(Element(i));
     
     std::cout << '\n' << name << '\n' << " Add: ";
     math::accumulate(&array[0], array+array_size, Element(0), math::add<Element>());
     std::cout << "Mult: ";
     math::accumulate(array, array+array_size, Element(1), math::mult<Element>());
+    std::cout << "Mult [with a list]: ";
+    math::accumulate(l.begin(), l.end(), Element(1), math::mult<Element>());
     std::cout << " Min: ";
-    math::accumulate(array, array+array_size, Element(1), math::min<Element>());
+    math::accumulate(array, array+array_size, Element(1000), math::min<Element>());
     std::cout << " Max: ";
-    math::accumulate(array, array+array_size, Element(1), math::max<Element>());
+    math::accumulate(array, array+array_size, Element(-1000), math::max<Element>());
 }
 
 
@@ -30,6 +46,7 @@ int main(int, char* [])
     test_accumulate<int>("int");
     test_accumulate<float>("float");
     test_accumulate<double>("double");
+    std::cout << '\n';
 
     return 0;
 }
