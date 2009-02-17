32ad5dc21a9fcdad2320b1c1da71d910c16dfc55
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2009-02-17 11:31:54 +0000

    Flexible complex numbers.

---
+++ mtl4/branches/parallel/libs/numeric/complex/test/conceptg++_test.cpp
@@ -0,0 +1,16 @@
+// $COPYRIGHT$
+
+#include <iostream>
+#include <boost/test/minimal.hpp>
+#include <concepts>
+
+
+
+int test_main(int argc, char* argv[])
+{
+    complex<int> cc;
+
+    std::cout << "Hey concepts.\n";
+
+    return 0;
+}
