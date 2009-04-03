081d0355ef2d032f2d931dcd644eaefc21405c04
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2009-04-03 14:31:08 +0000

    Blocked insertion works but is not faster.

---
+++ mtl4/trunk/libs/numeric/mtl/test/compressed2D_inserter3_test.cpp
@@ -0,0 +1,71 @@
+// Software License for MTL
+// 
+// Copyright (c) 2007 The Trustees of Indiana University.
+//               2008 Dresden University of Technology and the Trustees of Indiana University.
+// All rights reserved.
+// Authors: Peter Gottschling and Andrew Lumsdaine
+// 
+// This file is part of the Matrix Template Library
+// 
+// See also license.mtl.txt in the distribution.
+
+#include <iostream>
+#include <boost/test/minimal.hpp>
+#include <boost/numeric/mtl/mtl.hpp>
+
+
+using namespace std;
+
+void test(int n, int m, int order)
+{
+    assert(n%3 == 0);
+    mtl::dense2D<double>           block(3, m);
+    mtl::dense_vector<std::size_t> rows(3), columns(m);
+
+    for (int i= 0; i < 3; i++) rows[i]= i;
+
+    switch(order) {
+    case 0: for (int i= 0; i < m; i++) columns[i]= i; break;
+    case 1: for (int i= 0; i < m; i++) columns[i]= m-i-1; break;
+    case 2: for (int i= 0; i < m; i++) columns[i]= (m/2+i)%m; break;
+    }
+    //columns[3]= columns[1]; columns[2]= columns[0];
+
+    for (int i= 0; i < m; i++) {
+	block[0][i]= 10+i;
+	block[1][i]= 20+i;
+	block[2][i]= 30+i;
+    }
+
+    mtl::compressed2D<double> A(n, n);
+    {
+        mtl::matrix::inserter<mtl::compressed2D<double>, mtl::update_plus<double> > ins(A, 5);
+	for (unsigned r= 0; r < num_rows(A); r+= 3) {
+	    ins << element_matrix(block, rows, columns);
+	    for (int i= 0; i < 3; i++) rows[i]+= 3;
+	    for (int i= 0; i < m; i++) ++columns[i];
+	}
+    }
+    if (n < 11) cout << "A is \n" << A;
+    switch(order) {
+    case 0:
+	if (A[1][2] != 22) throw "Wrong value";
+	if (A[4][2] != 21) throw "Wrong value";	break;
+    case 1:
+	if (A[1][2] != 22) throw "Wrong value";
+	if (A[4][2] != 23) throw "Wrong value";	break;
+    case 2:
+	if (A[1][2] != 20) throw "Wrong value";
+	if (A[4][2] != 24) throw "Wrong value";	break;
+    }
+}
+
+ 
+int test_main(int argc, char* argv[])
+{
+    test(9, 5, 0);
+    test(9, 5, 1);
+    test(9, 5, 2);
+
+    return 0;
+}
