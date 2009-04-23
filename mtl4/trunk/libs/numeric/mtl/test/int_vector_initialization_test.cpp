29ffc9bef0df0c350be472585871a1ee3e9974db
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2009-04-23 09:11:03 +0000

    Example on disambigation.

---
+++ mtl4/trunk/libs/numeric/mtl/test/int_vector_initialization_test.cpp
@@ -0,0 +1,24 @@
+// Software License for MTL
+// 
+// Copyright (c) 2007 The Trustees of Indiana University. 
+//               2008 Dresden University of Technology and the Trustees of Indiana University. 
+// All rights reserved.
+// Authors: Peter Gottschling and Andrew Lumsdaine
+// 
+// This file is part of the Matrix Template Library
+// 
+// See also license.mtl.txt in the distribution.
+
+#include <iostream>
+#include <boost/test/minimal.hpp>
+#include <boost/numeric/mtl/mtl.hpp>
+
+
+using namespace std;
+
+int test_main(int argc, char* argv[])
+{
+    mtl::dense_vector<uint> vec(5, 0u);
+    return 0;
+}
+
