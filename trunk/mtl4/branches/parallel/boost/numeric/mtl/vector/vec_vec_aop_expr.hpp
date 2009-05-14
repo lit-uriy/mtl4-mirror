0798dbafe8663d804f0ed27c7c12c72fbfe0fc47
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2009-05-14 13:50:01 +0000

    u= v; u= scalar; u= v + w; work now ...

--- trunk/mtl4/branches/parallel/boost/numeric/mtl/vector/vec_vec_aop_expr.hpp
+++ trunk/mtl4/branches/parallel/boost/numeric/mtl/vector/vec_vec_aop_expr.hpp
@@ -54,30 +54,32 @@ struct vec_vec_aop_expr
     // Non-distributed version
     void destroy(tag::universe)
     {
-	if (!delayed_assign) {
-	    // If target is constructed by default it takes size of source
-	    if (first.size() == 0) first.change_dim(second.size());
+	// If target is constructed by default it takes size of source
+	if (first.size() == 0) first.change_dim(second.size());
 
-	    // If sizes are different for any other reason, it's an error
-	    // std::cerr << "~vec_vec_aop_expr() " << first.size() << "  " << second.size() << "\n";
-	    MTL_DEBUG_THROW_IF(first.size() != second.size(), incompatible_size());
-
-	    for (size_type i= 0; i < first.size(); ++i)
-		SFunctor::apply( first(i), second(i) );
-	}
+	// If sizes are different for any other reason, it's an error
+	std::cout << "~vec_vec_aop_expr() " << first.size() << "  " << second.size() << std::endl;
+	MTL_DEBUG_THROW_IF(first.size() != second.size(), incompatible_size());
+	
+	for (size_type i= 0; i < first.size(); ++i)
+	    SFunctor::apply( first(i), second(i) );
     }
 
     // Distributed version
     void destroy(tag::distributed)
     {
+	// Check distribution !!!
+
 	typedef typename DistributedCollection<E1>::local_type LocalE1;
 	typedef typename DistributedCollection<E2>::local_type LocalE2;
 	// Create and destroy local expression so that local operation is performed here
+	std::cout << "local(first) is " << local(first) << ", local(second).size() is " << local(second).size() << std::endl;
+
 	vec_vec_aop_expr<LocalE1, LocalE2, SFunctor>(local(first), local(second));
     }
 
   public:
-    ~vec_vec_aop_expr() { destroy(typename mtl::traits::category<E1>::type()); }
+    ~vec_vec_aop_expr() { if (!delayed_assign) destroy(typename mtl::traits::category<E1>::type()); }
     
     void delay_assign() const { delayed_assign= true; }
 
