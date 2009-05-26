034f36ff364eddc7b19b01e78c1df600e7b039b5
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2009-05-26 09:36:23 +0000

    Suppress multiple outputs of residuum in iteration; print only every 5th iteration.

--- trunk/mtl4/trunk/boost/numeric/itl/iteration/cyclic_iteration.hpp
+++ trunk/mtl4/trunk/boost/numeric/itl/iteration/cyclic_iteration.hpp
@@ -21,47 +21,50 @@
 namespace itl {
 
   template <class Real>
-  class cyclic_iteration : public basic_iteration<Real> {
-    typedef basic_iteration<Real> super;
+  class cyclic_iteration : public basic_iteration<Real> 
+  {
+      typedef basic_iteration<Real> super;
 
-    void print_resid()
-    {
-      if (this->i % cycle == 0)
-	  std::cout << "iteration " << this->i << ": resid " << this->resid() << std::endl;
-    }
+      void print_resid()
+      {
+	  if (this->i % cycle == 0)
+	      if (this->i != last_print) { // Avoid multiple print-outs in same iteration
+		  std::cout << "iteration " << this->i << ": resid " << this->resid() << std::endl;
+		  last_print= this->i;
+	      }
+      }
 
-  public:
+    public:
   
-    template <class Vector>
-    cyclic_iteration(const Vector& b, int max_iter_, 
-		     Real tol_, Real atol_ = Real(0), int cycle_ = 100)
-      : super(b, max_iter_, tol_, atol_), cycle(cycle_) { }
+      template <class Vector>
+      cyclic_iteration(const Vector& b, int max_iter_, Real tol_, Real atol_ = Real(0), int cycle_ = 100)
+	: super(b, max_iter_, tol_, atol_), cycle(cycle_), last_print(-1) 
+      {}
 
 
-    template <typename T>
-    bool finished(const T& r) 
-    {
-      bool ret= super::finished(r);
-      print_resid();
-      return ret;
-    }
+      template <typename T>
+      bool finished(const T& r) 
+      {
+	  bool ret= super::finished(r);
+	  print_resid();
+	  return ret;
+      }
 
-    operator int() { return error_code(); }
+      operator int() { return error_code(); }
 
-    int error_code() {
-      using std::cout;
-      using std::endl;
+      int error_code() {
+	  using std::cout; using std::endl;
 
-      cout << "finished! error code = " << this->error << '\n';
-      cout << this->iterations() << " iterations\n";
-      cout << this->resid() << " is actual final residual. \n"
-	   << this->resid()/this->normb() << " is actual relative tolerance achieved. \n";
-      cout << "Relative tol: " << this->rtol_ << "  Absolute tol: " << this->atol_ << '\n';
-      cout << "Convergence:  " << pow(this->resid()/this->normb(), 1.0 / double(this->iterations())) << endl;
-      return this->error;
-    }
-  protected:
-    int cycle;
+	  cout << "finished! error code = " << this->error << '\n';
+	  cout << this->iterations() << " iterations\n";
+	  cout << this->resid() << " is actual final residual. \n"
+	       << this->resid()/this->normb() << " is actual relative tolerance achieved. \n";
+	  cout << "Relative tol: " << this->rtol_ << "  Absolute tol: " << this->atol_ << '\n';
+	  cout << "Convergence:  " << pow(this->resid()/this->normb(), 1.0 / double(this->iterations())) << endl;
+	  return this->error;
+      }
+    protected:
+      int cycle, last_print;
   };
 
 
