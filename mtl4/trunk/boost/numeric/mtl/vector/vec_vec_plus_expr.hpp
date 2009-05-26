ba2dce1d7973d708cf0306a8a8de50cb19f583bf
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2009-05-26 12:37:02 +0000

    Removed vec_vec_plus_expr and vec_vec_minus_expr entirely; compute_summand copies pm expressions now.

--- mtl4/trunk/boost/numeric/mtl/vector/vec_vec_plus_expr.hpp
+++ mtl4/trunk/boost/numeric/mtl/vector/vec_vec_plus_expr.hpp
@@ -23,24 +23,15 @@
 
 namespace mtl { namespace vector {
 
-// Model of VectorExpression
-template <class E1, class E2>
-struct vec_vec_plus_expr 
-  : vec_vec_pmop_expr< E1, E2, mtl::sfunctor::plus<typename E1::value_type, typename E2::value_type> >
-{
-    typedef vec_vec_pmop_expr< E1, E2, mtl::sfunctor::plus<typename E1::value_type, typename E2::value_type> > base;
-    vec_vec_plus_expr( E1 const& v1, E2 const& v2 ) : base(v1, v2) {}
-};
-
-
 template <typename E1, typename E2>
-inline vec_vec_plus_expr<E1, E2>
+inline vec_vec_pmop_expr< E1, E2, mtl::sfunctor::plus<typename E1::value_type, typename E2::value_type> >
 operator+ (const vec_expr<E1>& e1, const vec_expr<E2>& e2)
 {
     // do not add row and column vectors (or inconsistent value types)
     BOOST_STATIC_ASSERT((boost::is_same<typename ashape::ashape<E1>::type, 
 			                typename ashape::ashape<E2>::type>::value));
-    return vec_vec_plus_expr<E1, E2>(static_cast<const E1&>(e1), static_cast<const E2&>(e2));
+    typedef vec_vec_pmop_expr< E1, E2, mtl::sfunctor::plus<typename E1::value_type, typename E2::value_type> > type;
+    return type(static_cast<const E1&>(e1), static_cast<const E2&>(e2));
 }
 
 } } // Namespace mtl::vector
