7cf4d653cae6cc623827131381af2ea1f0f9de6d
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2009-06-05 09:28:13 +0000

    Parallel CG works!!!

---
+++ trunk/mtl4/branches/parallel/libs/numeric/itl/test/mpi_2_cg_test.cpp
@@ -0,0 +1,41 @@
+// Software License for MTL
+// 
+// Copyright (c) 2007 The Trustees of Indiana University.
+//               2008 Dresden University of Technology and the Trustees of Indiana University.
+// All rights reserved.
+// Authors: Peter Gottschling and Andrew Lumsdaine
+// 
+// This file is part of the Matrix Template Library
+// 
+// See also license.mtl.txt in the distribution.
+
+#include <boost/numeric/mtl/mtl.hpp>
+#include <boost/numeric/itl/itl.hpp>
+
+#include <boost/test/minimal.hpp>
+#include <boost/mpi.hpp>
+
+namespace mpi = boost::mpi;
+
+int test_main(int argc, char* argv[])
+{
+  // For a more realistic example set size to 1000 or larger
+  const int size = 10, N = size * size;
+
+  typedef mtl::matrix::distributed<mtl::compressed2D<double> > matrix_type;
+  matrix_type          A(N, N);
+  laplacian_setup(A, size, size);
+
+  //itl::pc::diagonal<matrix_type>     P(A);
+  itl::pc::identity<matrix_type>     P(A);
+
+  mtl::vector::distributed<mtl::dense_vector<double> > x(N, 1.0), b(N);
+
+  b = A * x;
+  x= 0;
+
+  itl::cyclic_iteration<double> iter(b, 500, 1.e-6, 0.0, 5);
+  cg(A, x, b, P, iter);
+
+  return 0;
+}
