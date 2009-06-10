21b50a1f39e1ab3b27fa7625f60db44c84669116
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2009-06-10 08:46:13 +0000

    CGS works now when map_view expressions are copied.

--- mtl4/branches/parallel/boost/numeric/mtl/operation/compute_summand.hpp
+++ mtl4/branches/parallel/boost/numeric/mtl/operation/compute_summand.hpp
@@ -13,6 +13,7 @@
 #define MTL__OPERATION_COMPUTE_SUMMAND_INCLUDE
 
 #include <boost/numeric/mtl/mtl_fwd.hpp>
+#include <boost/numeric/mtl/utility/copy_expression_const_ref_container.hpp>
 
 namespace mtl { namespace operation {
 
@@ -30,7 +31,8 @@ struct compute_summand
 {
     typedef Expr type;
     compute_summand(const Expr& expr) : value(expr) {}
-    const Expr& value;
+    // value is a const& if Expr is a true vector and a copy if it is an expression
+    typename mtl::traits::copy_expression_const_ref_container<Expr>::type value;
 };
 
 
@@ -41,7 +43,7 @@ struct compute_summand< mat_cvec_times_expr<Matrix, CVector> >
     typedef CVector    type;
 
     compute_summand(const mat_cvec_times_expr<Matrix, CVector>& expr) 
-	: value(num_rows(expr.first))
+      : value(num_rows(expr.first))
     {
 	value= expr.first * expr.second;
     }
@@ -49,22 +51,6 @@ struct compute_summand< mat_cvec_times_expr<Matrix, CVector> >
     CVector value;
 };
 	
-template <class E1, class E2, typename SFunctor>
-struct compute_summand< vector::vec_vec_pmop_expr<E1, E2, SFunctor> >
-{
-    typedef vector::vec_vec_pmop_expr<E1, E2, SFunctor> type;
-    compute_summand(const type& expr) : value(expr) {}
-    type value;
-};
-
-template <typename Functor, typename Vector>
-struct compute_summand< vector::map_view<Functor, Vector> >
-{
-    typedef vector::map_view<Functor, Vector> type;
-    compute_summand(const type& expr) : value(expr) {}
-    type value;
-};
-
 }} // namespace mtl::operation
 
 #endif // MTL__OPERATION_COMPUTE_SUMMAND_INCLUDE
