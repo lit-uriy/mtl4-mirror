0d04372a6c0544101466ae7fdc7ef6950f5e0832
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2009-08-27 10:32:50 +0000

    Beautified.

--- mtl4/trunk/boost/numeric/itl/krylov/gmres.hpp
+++ mtl4/trunk/boost/numeric/itl/krylov/gmres.hpp
@@ -27,14 +27,14 @@ template < typename Matrix, typename Vector, typename Iteration >
 int gmres(const Matrix &A, Vector &x, const Vector &b,
 	  Iteration& iter, typename mtl::Collection<Vector>::size_type kmax_in)
 {
-    using mtl::irange; using mtl::imax; using mtl::matrix::strict_upper; using std::abs;
+    using mtl::irange; using mtl::iall; using mtl::matrix::strict_upper; using std::abs;
     typedef typename mtl::Collection<Vector>::value_type Scalar;
     typedef typename mtl::Collection<Vector>::size_type  Size;
 
     if (size(b) == 0) throw mtl::logic_error("empty rhs vector");
 
     const Scalar                zero= math::zero(b[0]);
-    Scalar                      rho, hr, w1, w2, nu;
+    Scalar                      rho, w1, w2, nu;
     Size                        k, n(size(x)), kmax(std::min(size(x), kmax_in));
 
     Vector                      r(n),r0(n),s(kmax+1), c(kmax+1), g(kmax+1), vi(n), va(n);
@@ -51,55 +51,38 @@ int gmres(const Matrix &A, Vector &x, const Vector &b,
     // GMRES iteration
     for (k= 0; !iter.finished(rho) && k < kmax; k++, ++iter) {
 
-	// vi= v.vector(k)[range_n];
-        for (Size i= 0; i < n; i++)
-	    vi[i]= v[i][k];
-
-        //v.vector(k+1)[irange(0, n)]= va= A * vi;
-	va= A * vi;
-        for (Size i= 0; i < n; i++)
-	    v[i][k+1]=va[i];
+        v.vector(k+1)= va= A * v.vector(k);
 
         // modified Gram Schmidt method
         for (Size j= 0; j < k+1; j++) {
-            for (Size i= 0; i < n; i++)
-                vi[i]= v[i][j];
-	    // h[j][k]= dot(v.vector(j)[range_n], va);
-            h[j][k]= dot(vi,va);
-	    // v.vector(k+1)[range_n]-= ele_prod(h.vector(k), v.vector(j)[range_n]);
-            for (Size i= 0;i < n; i++)
-                v[i][k+1]-= h[j][k]*vi[i];
+	    vi= v.vector(j);
+            h[j][k]= dot(vi, va);
+	    v.vector(k+1)-= h[j][k] * vi;
         }
 
-	// h[k+1][k]= two_norm(v.vector(k+1)[range_n]);
-        for(Size i= 0; i < n; i++)
-            vi[i]=v[i][k+1];
+	vi= v.vector(k+1); 
         h[k+1][k]= two_norm(vi);
 
         //reorthogonalize
-        for(Size j=0; j < k+1;j++){
-            for(Size i= 0; i < n;i++)
-                va[i]= v[i][j];
-            hr= dot(vi,va);
-            h[j][k]= h[j][k] + hr;
-            vi= vi - hr*va;
+        for(Size j=0; j < k+1;j++) {
+            Scalar hr= dot(vi, v.vector(j));
+            h[j][k]+= hr;
+            vi-=  hr * v.vector(j);
         }
         h[k+1][k]= two_norm(vi);
 
         //watch for breakdown
-        if(h[k+1][k] != zero)
-            vi = vi/h[k+1][k];
-        for(Size i= 0; i < n;i++)
-            v[i][k+1]=vi[i];
+        if (h[k+1][k] != zero)
+            vi*= 1. / h[k+1][k];
+	v.vector(k+1)= vi;
 
         //k givensrotationen
-        if(k>0)
-            for(Size i=0; i < k;i++) {
-                w1= c[i]*h[i][k]-s[i]*h[i+1][k];
-                w2= s[i]*h[i][k]+c[i]*h[i+1][k];
-                h[i][k]= w1;
-                h[i+1][k]= w2;
-            }
+	for(Size i= 0; i < k; i++) {
+	    w1= c[i]*h[i][k]-s[i]*h[i+1][k];
+	    w2= s[i]*h[i][k]+c[i]*h[i+1][k];
+	    h[i][k]= w1;
+	    h[i+1][k]= w2;
+	}
         nu= sqrt(h[k][k]*h[k][k]+h[k+1][k]*h[k+1][k]);
 
         if(nu != zero){
@@ -116,34 +99,24 @@ int gmres(const Matrix &A, Vector &x, const Vector &b,
 	rho= abs(g[k]);
     }
 
-    //iterator is finish -> compute x
-    // solve H*y=g
-    Size        max_iter= k;
-    irange      range_iter(0, max_iter);
-    // dense2D     h_a(h[range_iter][range_iter]), v_a(v[iall][range_iter]);
-    // Vector      g_a(g[range_iter]), y(lu_solve_new(h_a, g_a))
+    // iteration is finished -> compute x: solve H*y=g
+    irange                  range_k(0, k);
+    // mtl::dense2D<Scalar>    h_a(h[range_k][range_k]), v_a(v[iall][range_k]);
+    mtl::dense2D<Scalar>   h_a(k, k), v_a(n, k);
 
-
-    mtl::dense2D<Scalar>   h_a(max_iter, max_iter), v_a(n, max_iter);
-    Vector                 g_a(max_iter), y(max_iter);
-    for (Size i = 0; i < max_iter;i++){
-        g_a[i]=g[i];
-        for(Size j = 0 ; j< max_iter ;j++)
+    for(Size j = 0 ; j< k ;j++) {
+	for (Size i = 0; i < k;i++)
             h_a[i][j]= h[i][j];
-    }
-    y=lu_solve_new(h_a, g_a);
-
-    for(Size i = 0; i < n ;i++){
-        for(Size j = 0 ; j< max_iter ;j++)
+	for(Size i = 0; i < n ;i++)
             v_a[i][j]=v[i][j];
-    }
-    //calculation of solution
-    // x+= v[iall][range_iter] * lu_solve_new(h[range_iter][range_iter], g[range_iter]);
+    }	
+    Vector                  g_a(g[range_k]), y(lu_solve_new(h_a, g_a));
+
+    // solve
     x+= v_a * y;
     r= b - A*x;
-    if(two_norm(r0)<two_norm(r))
+    if (two_norm(r0) < two_norm(r))
         return iter.fail(2, "GMRES does not converge");
-
     return iter;
 }
 
