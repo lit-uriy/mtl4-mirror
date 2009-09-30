09188d428eaddc568f11e072788f2865b2de3149
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2009-09-30 19:32:42 +0000

    Fast serialization.

--- trunk/mtl4/branches/parallel/libs/numeric/mtl/test/mpi_2_vector_send_test.cpp
+++ trunk/mtl4/branches/parallel/libs/numeric/mtl/test/mpi_2_vector_send_test.cpp
@@ -17,6 +17,38 @@
 
 namespace mpi = boost::mpi;
 
+struct ss 
+{
+    ss() : v(0) {}
+    ss(int v) : v(v) {}
+    operator int() { return v; }
+
+    template <typename Archive>
+    void serialize(Archive& ar, const unsigned version) { ar & v; }
+
+    friend std::ostream& operator<<(std::ostream& os, ss x) { return os << x.v; }
+
+    int v;
+};
+
+template <typename T>
+void test()
+{
+    mpi::communicator world;
+
+    mtl::dense_vector<T> v(world.rank() ? 0 : 2);
+
+    if (world.rank() == 0) {
+	v[0]= 4 - world.rank(), v[1]= 9;
+	world.send(1, 0, v);
+    } else {
+	std::cout << "v on proc 1 is " << v << std::endl;
+	world.recv(0, 0, v);
+	std::cout << "v on proc 1 is " << v << std::endl;
+	if (v[0] != 4) throw "v[0] should be 4!";
+    }
+}
+
 
 int test_main(int argc, char* argv[]) 
 {
@@ -28,17 +60,8 @@ int test_main(int argc, char* argv[])
 	env.abort(87);
     }
 
-    mtl::dense_vector<int> v(world.rank() ? 0 : 2);
-
-    if (world.rank() == 0) {
-	v[0]= 4 - world.rank(), v[1]= 9;
-	world.send(1, 0, v);
-    } else {
-	std::cout << "v on proc 1 is " << v << std::endl;
-	world.recv(0, 0, v);
-	std::cout << "v on proc 1 is " << v << std::endl;
-	if (v[0] != 4) throw "v[0] should be 4!";
-    }
+    test<int>();
+    test<ss>();
 
     return 0;
 }
