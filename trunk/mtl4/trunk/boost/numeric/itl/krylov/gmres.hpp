b379dd206831a263946f7c7922781a75605e1843
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2009-09-30 10:28:06 +0000

    GMRES with restart.

--- trunk/mtl4/trunk/boost/numeric/itl/krylov/gmres.hpp
+++ trunk/mtl4/trunk/boost/numeric/itl/krylov/gmres.hpp
@@ -22,10 +22,10 @@
 
 namespace itl {
 
-
-template < typename Matrix, typename Vector, typename Iteration >
-int gmres(const Matrix &A, Vector &x, const Vector &b,
-	  Iteration& iter, typename mtl::Collection<Vector>::size_type kmax_in)
+template < typename Matrix, typename Vector, typename LeftPreconditioner, typename RightPreconditioner, typename Iteration >
+int gmres_full(const Matrix &A, Vector &x, const Vector &b,
+               LeftPreconditioner &L, RightPreconditioner &R,
+               Iteration& iter, typename mtl::Collection<Vector>::size_type kmax_in)
 {
     using mtl::irange; using mtl::iall; using mtl::matrix::strict_upper; using std::abs;
     typedef typename mtl::Collection<Vector>::value_type Scalar;
@@ -34,45 +34,41 @@ int gmres(const Matrix &A, Vector &x, const Vector &b,
     if (size(b) == 0) throw mtl::logic_error("empty rhs vector");
 
     const Scalar                zero= math::zero(b[0]);
-    Scalar                      rho, w1, w2, nu;
+    Scalar                      rho, w1, w2, nu, hr;
     Size                        k, n(size(x)), kmax(std::min(size(x), kmax_in));
-
-    Vector                      r(n), r0(b - A * x),s(kmax+1), c(kmax+1), g(kmax+1), va(n);
+    Vector                      r0(b - A *x), r(solve(L,r0)), s(kmax+1),
+                                c(kmax+1), g(kmax+1), va(n), va0(n), va00(n);
     mtl::multi_vector<Vector>   v(kmax+1, Vector(n, zero)), h(kmax, Vector(kmax+1, zero));
     irange                      range_n(0, n);
 
-    if (iter.finished(r0))
+    rho= g[0]= two_norm(r);
+    if (iter.finished(rho)){
 	return iter;
-
-    rho= g[0]= two_norm(r0);
-    v.vector(0)= r0 / rho;
+    }
+    v.vector(0)= r / rho;
 
     // GMRES iteration
     for (k= 0; !iter.finished(rho) && k < kmax; k++, ++iter) {
-
-        v.vector(k+1)= va= A * v.vector(k);
-
+        va0= A * solve(R, v.vector(k));
+        v.vector(k+1)= va= solve(L,va0);
+	// orth(v, v[k+1], false); 
         // modified Gram Schmidt method
         for (Size j= 0; j < k+1; j++) {
-	    Vector& vi= v.vector(j);
-            h[j][k]= dot(vi, va);
-	    v.vector(k+1)-= h[j][k] * vi;
+	    h[j][k]= dot(v.vector(j), v.vector(k+1));
+	    v.vector(k+1)-= h[j][k] * v.vector(j);
         }
 
-	Vector& vi= v.vector(k+1); 
-        h[k+1][k]= two_norm(vi);
-
+        h[k+1][k]= two_norm(v.vector(k+1));
         //reorthogonalize
-        for(Size j=0; j < k+1;j++) {
-            h[j][k]+= dot(vi, v.vector(j));
-            vi-= h[j][k] * v.vector(j);
+        for(Size j= 0; j < k+1; j++) {
+	    hr= dot(v.vector(k+1), v.vector(j));
+            h[j][k]+= hr;
+            v.vector(k+1)-= hr * v.vector(j);
         }
-        h[k+1][k]= two_norm(vi);
-
+        h[k+1][k]= two_norm(v.vector(k+1));
         //watch for breakdown
         if (h[k+1][k] != zero)
-            vi*= 1. / h[k+1][k];
-	v.vector(k+1)= vi;
+            v.vector(k+1)*= 1. / h[k+1][k];
 
         //k givensrotationen
 	for(Size i= 0; i < k; i++) {
@@ -82,19 +78,17 @@ int gmres(const Matrix &A, Vector &x, const Vector &b,
 	    h[i+1][k]= w2;
 	}
         nu= sqrt(h[k][k]*h[k][k]+h[k+1][k]*h[k+1][k]);
-
         if(nu != zero){
             c[k]=  h[k][k]/nu;
             s[k]= -h[k+1][k]/nu;
             h[k][k]=c[k]*h[k][k]-s[k]*h[k+1][k];
             h[k+1][k]=0;
-            w1= c[k]*g[k]-s[k]*g[k+1];//givensrotation on solutionparameters
+	    w1= c[k]*g[k]-s[k]*g[k+1];//givensrotation on solutionparameters
             w2= s[k]*g[k]+c[k]*g[k+1];//givensrotation on solutionparameters
             g[k]= w1;
             g[k+1]= w2;
         }
-
-	rho= abs(g[k]);
+	rho= abs(g[k+1]);
     }
 
     // iteration is finished -> compute x: solve H*y=g
@@ -102,22 +96,36 @@ int gmres(const Matrix &A, Vector &x, const Vector &b,
     // mtl::dense2D<Scalar>    h_a(h[range_k][range_k]), v_a(v[iall][range_k]);
     mtl::dense2D<Scalar>   h_a(k, k), v_a(n, k);
 
-    for(Size j = 0 ; j< k ;j++) {
-	for (Size i = 0; i < k;i++)
+    for(Size j = 0 ; j < k; j++) {
+	for (Size i = 0; i < k; i++)
             h_a[i][j]= h[i][j];
-	for(Size i = 0; i < n ;i++)
-            v_a[i][j]=v[i][j];
-    }	
-    Vector                  g_a(g[range_k]), y(lu_solve(h_a, g_a));
-
-    // solve
-    x+= v_a * y;
+	for(Size i = 0; i < n; i++)
+            v_a[i][j]= v[i][j];
+    }
+    Vector                  g_a(g[range_k]), y(lu_solve(h_a, g_a)), y1(v_a*y);
+    // solve  x+= v_a * y;
+    x+= solve(R,y1);
     r= b - A*x;
     if (two_norm(r0) < two_norm(r))
         return iter.fail(2, "GMRES does not converge");
     return iter;
 }
 
+
+template < typename Matrix, typename Vector, typename LeftPreconditioner,
+           typename RightPreconditioner, typename Iteration >
+int gmres(const Matrix &A, Vector &x, const Vector &b,
+          LeftPreconditioner &L, RightPreconditioner &R,
+	  Iteration& iter, typename mtl::Collection<Vector>::size_type restart)
+{
+    while (!iter.finished()) 
+	gmres_full(A, x, b, L, R, iter, restart);
+    return iter;
+}
+
+
+
 } // namespace itl
 
 #endif // ITL_GMRES_INCLUDE
+
