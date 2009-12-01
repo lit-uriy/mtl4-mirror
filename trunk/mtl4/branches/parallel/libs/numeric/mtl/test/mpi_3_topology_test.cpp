bbebc327636a91b1a1a009dd24b7b3c1234ee5b0
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2009-12-01 08:22:02 +0000

    More cleaning.

--- trunk/mtl4/branches/parallel/libs/numeric/mtl/test/mpi_3_topology_test.cpp
+++ trunk/mtl4/branches/parallel/libs/numeric/mtl/test/mpi_3_topology_test.cpp
@@ -13,87 +13,22 @@
 #include <iostream>
 #include <boost/test/minimal.hpp>
 
-
-
 #if defined(MTL_HAS_PARMETIS) && defined(MTL_HAS_MPI)
 
-#include <map>
-#include <utility>
-#include <vector>
-#include <algorithm>
-
-
-#define MTL_HAS_STD_OUTPUT_OPERATOR // to print std::vector and std::pair
 #include <boost/numeric/mtl/mtl.hpp>
-
-#include <parmetis.h>
 #include <boost/mpi.hpp>
-#include <boost/serialization/string.hpp>
-
-namespace mpi = boost::mpi;
-
-template <typename Inserter>
-struct ins
-{
-    typedef typename Inserter::size_type  size_type;
-    ins(Inserter& i, int start) : i(i), v(start) {}
-    void operator()(size_type r, size_type c) {	i[r][c] << double(v++);  }
-    Inserter& i;
-    int       v;
-};
-
-template <typename Matrix>
-void test(Matrix& A,  const char* name, int version)
-{
-    typedef typename mtl::Collection<Matrix>::size_type size_type;
-    typedef std::pair<size_type, size_type>             entry_type;
-    typedef std::vector<entry_type>                     vec_type;
-
-    mtl::par::single_ostream sout;
-    mtl::par::multiple_ostream<> mout;
-
-    mpi::communicator comm(communicator(A));
-    {
-	mtl::matrix::inserter<Matrix> mins(A);
-	ins<mtl::matrix::inserter<Matrix> > i(mins, 10*(comm.rank()+1));
-        switch (version) {
-          case 1: 
-	    switch (comm.rank()) {
-	      case 0: i(0, 1); i(0, 2); i(1, 2); i(1, 3); i(2, 3); i(2, 5); std::cout << "version 1\n"; break;
-    	      case 1: i(3, 4); i(3, 5); i(4, 5); i(4, 6); break;
-    	      case 2: i(5, 6); i(6, 4); i(6, 5);
-    	    }; break;
-          case 2: 
-    	    switch (comm.rank()) {
-	      case 0: i(0, 1); i(1, 2); i(2, 3); std::cout << "\n\nversion 2\n"; break;
-    	      case 1: i(3, 4); i(4, 5); break;
-    	      case 2: i(5, 6); i(6, 0);
-    	  }; break;
-        }
-    }
-
-    sout << "Matrix is:" << '\n' << A;
-
-    mtl::par::block_migration migration= parmetis_migration(A);
-    Matrix B(num_rows(A), num_cols(A), migration.new_distribution());
-    migrate_matrix(A, B, migration);
-
-    sout << "Migrated matrix is:\n" << B;
-
-    Matrix C(A, parmetis_migration(A)); 
-    sout << "Migrated matrix (in constructor) is:\n" << C;
-}
 
 
 int test_main(int argc, char* argv[]) 
 {
     using namespace mtl;
+    typedef matrix::distributed<compressed2D<double> > matrix_type;
 
     mpi::environment env(argc, argv);
     mpi::communicator world;
     
-    mtl::matrix::distributed<mtl::matrix::compressed2D<double> > C(mtl::io::matrix_market("matrix.mtx"));
-    mtl::matrix::distributed<mtl::matrix::compressed2D<double> > D(C, parmetis_migration(C));
+    //matrix_type C(mtl::io::matrix_market("matrix.mtx")); // The file is not there!!!
+    //matrix_type D(C, parmetis_migration(C));
 
     return 0;
 }
