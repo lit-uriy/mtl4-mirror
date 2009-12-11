9cb849c1905ef911c9e24fc4590f95c64f3690d8
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2009-12-11 16:09:10 +0000

    New branch to work on multigrid.

---
+++ mtl4/branches/multigrid/libs/sequence/test/minus.cpp
@@ -0,0 +1,30 @@
+// Copyright David Abrahams 2005. Distributed under the Boost
+// Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <boost/sequence/arithmetic/minus.hpp>
+#include <boost/test/unit_test.hpp>
+
+using namespace boost::sequence;
+namespace mpl = boost::mpl;
+
+int test_main(int, char*[] )
+{
+    BOOST_REQUIRE(
+        minus(3,5) == (3-5)
+    );
+
+    BOOST_REQUIRE(
+        minus(mpl::int_<3>(), mpl::int_<5>()) == mpl::int_<(3-5)>()
+    );
+
+    BOOST_REQUIRE(
+        minus(mpl::int_<3>(), 5) == 3-5
+    );
+
+    BOOST_REQUIRE(
+        minus(3, mpl::int_<5>()) == 3-5
+    );
+    
+    return 0;
+}
