ad2e46e109680bc28a97a829b61c2b914115bb49
cornelius <cornelius@df6d15db-99ea-0310-92f0-9d27c566f14b>
2010-02-23 16:20:32 +0000

    start of cuda init tests; first check caracteristic of grafic hardware

---
+++ mtl4/branches/gpu/libs/numeric/mtl/test/check_cuda.cu
@@ -0,0 +1,93 @@
+// Software License for MTL
+// 
+// Copyright (c) 2007 The Trustees of Indiana University. 
+//               2008 Dresden University of Technology and the Trustees of Indiana University. 
+// All rights reserved.
+// Authors: Peter Gottschling and Andrew Lumsdaine
+// 
+// This file is part of the Matrix Template Library
+// 
+// See also license.mtl.txt in the distribution.
+
+#include <stdio.h>
+
+int main( int argc, char** argv) 
+{
+    printf("CUDA Device Query (Runtime API) version (CUDART static linking)\n");
+
+    int deviceCount;
+
+    cudaGetDeviceCount(&deviceCount);
+
+    // This function call returns 0 if there are no CUDA capable devices.
+    if (deviceCount == 0)
+        printf("There is no device supporting CUDA\n");
+    int dev;
+    for (dev = 0; dev < deviceCount; ++dev) {
+        cudaDeviceProp deviceProp;
+        cudaGetDeviceProperties(&deviceProp, dev);
+
+        if (dev == 0) {
+		// This function call returns 9999 for both major & minor fields, if no CUDA capable devices are present
+	        if (deviceProp.major == 9999 && deviceProp.minor == 9999)
+                printf("There is no device supporting CUDA.\n");
+            else if (deviceCount == 1)
+                printf("There is 1 device supporting CUDA\n");
+            else
+                printf("There are %d devices supporting CUDA\n", deviceCount);
+        }
+        printf("\nDevice %d: \"%s\"\n", dev, deviceProp.name);
+    #if CUDART_VERSION >= 2020
+		int driverVersion = 0, runtimeVersion = 0;
+		cudaDriverGetVersion(&driverVersion);
+		printf("  CUDA Driver Version:                           %d.%d\n", driverVersion/1000, driverVersion%100);
+		cudaRuntimeGetVersion(&runtimeVersion);
+		printf("  CUDA Runtime Version:                          %d.%d\n", runtimeVersion/1000, runtimeVersion%100);
+    #endif
+
+        printf("  CUDA Capability Major revision number:         %d\n", deviceProp.major);
+        printf("  CUDA Capability Minor revision number:         %d\n", deviceProp.minor);
+
+		printf("  Total amount of global memory:                 %lu bytes\n", deviceProp.totalGlobalMem);
+    #if CUDART_VERSION >= 2000
+        printf("  Number of multiprocessors:                     %d\n", deviceProp.multiProcessorCount);
+        printf("  Number of cores:                               %d\n", 8 * deviceProp.multiProcessorCount);
+    #endif
+        printf("  Total amount of constant memory:               %lu bytes\n", deviceProp.totalConstMem); 
+        printf("  Total amount of shared memory per block:       %lu bytes\n", deviceProp.sharedMemPerBlock);
+        printf("  Total number of registers available per block: %d\n", deviceProp.regsPerBlock);
+        printf("  Warp size:                                     %d\n", deviceProp.warpSize);
+        printf("  Maximum number of threads per block:           %d\n", deviceProp.maxThreadsPerBlock);
+        printf("  Maximum sizes of each dimension of a block:    %d x %d x %d\n",
+               deviceProp.maxThreadsDim[0],
+               deviceProp.maxThreadsDim[1],
+               deviceProp.maxThreadsDim[2]);
+        printf("  Maximum sizes of each dimension of a grid:     %d x %d x %d\n",
+               deviceProp.maxGridSize[0],
+               deviceProp.maxGridSize[1],
+               deviceProp.maxGridSize[2]);
+        printf("  Maximum memory pitch:                          %lu bytes\n", deviceProp.memPitch);
+        printf("  Texture alignment:                             %lu bytes\n", deviceProp.textureAlignment);
+        printf("  Clock rate:                                    %.2f GHz\n", deviceProp.clockRate * 1e-6f);
+    #if CUDART_VERSION >= 2000
+        printf("  Concurrent copy and execution:                 %s\n", deviceProp.deviceOverlap ? "Yes" : "No");
+    #endif
+    #if CUDART_VERSION >= 2020
+        printf("  Run time limit on kernels:                     %s\n", deviceProp.kernelExecTimeoutEnabled ? "Yes" : "No");
+        printf("  Integrated:                                    %s\n", deviceProp.integrated ? "Yes" : "No");
+        printf("  Support host page-locked memory mapping:       %s\n", deviceProp.canMapHostMemory ? "Yes" : "No");
+        printf("  Compute mode:                                  %s\n", deviceProp.computeMode == cudaComputeModeDefault ?
+			                                                            "Default (multiple host threads can use this device simultaneously)" :
+		                                                                deviceProp.computeMode == cudaComputeModeExclusive ?
+																		"Exclusive (only one host thread at a time can use this device)" :
+		                                                                deviceProp.computeMode == cudaComputeModeProhibited ?
+																		"Prohibited (no host thread can use this device)" :
+																		"Unknown");
+    #endif
+}
+    printf("\nTest PASSED\n");
+
+   // CUT_EXIT(argc, argv);
+   return 0;
+}
+
