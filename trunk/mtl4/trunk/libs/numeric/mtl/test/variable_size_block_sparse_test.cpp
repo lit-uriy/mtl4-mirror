5524770dd46a2e9d648001676478913b3a9eb4dd
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2010-03-03 12:12:37 +0000

    Got all tests to compile and run properly.

--- trunk/mtl4/trunk/libs/numeric/mtl/test/variable_size_block_sparse_test.cpp
+++ trunk/mtl4/trunk/libs/numeric/mtl/test/variable_size_block_sparse_test.cpp
@@ -42,49 +42,49 @@ int test_main(int argc, char* argv[])
     using namespace std;
 
 #if 0  
-	cout << typeid(as<int>::type).name() << endl;
-	cout << typeid(as<mtl::compressed2D<int> >::type).name() << endl;
-	cout << typeid(as<mtl::compressed2D<mtl::dense2D<int> > >::type).name() << endl;
+    cout << typeid(as<int>::type).name() << endl;
+    cout << typeid(as<mtl::compressed2D<int> >::type).name() << endl;
+    cout << typeid(as<mtl::compressed2D<mtl::dense2D<int> > >::type).name() << endl;
 
-	cout << typeid(mtl::ashape::ashape<int>::type).name() << endl;
-	cout << typeid(mtl::ashape::ashape<mtl::compressed2D<int> >::type).name() << endl;
-	cout << typeid(mtl::ashape::ashape<typename mtl::compressed2D<mtl::dense2D<int> >::value_type >::type).name() << endl;
+    cout << typeid(mtl::ashape::ashape<int>::type).name() << endl;
+    cout << typeid(mtl::ashape::ashape<mtl::compressed2D<int> >::type).name() << endl;
+    cout << typeid(mtl::ashape::ashape<typename mtl::compressed2D<mtl::dense2D<int> >::value_type >::type).name() << endl;
 #endif
 
     // Define a 6x5 sparse matrix in a 3x3 block-sparse
-	typedef mtl::dense2D<double>    m_t;
-	typedef mtl::compressed2D<m_t>  matrix_t;
+    typedef mtl::dense2D<double>    m_t;
+    typedef mtl::compressed2D<m_t>  matrix_t;
     matrix_t                        A(3, 3);
     {
-		mtl::matrix::inserter<matrix_t> ins(A);
+	mtl::matrix::inserter<matrix_t> ins(A);
 
-		// First block
-		m_t  b1(1, 1);
-		b1(0, 0)= 1.0;
-		ins(0, 2) << b1;
+	// First block
+	m_t  b1(1, 1);
+	b1(0, 0)= 1.0;
+	ins(0, 2) << b1;
 
-		// Second block
-		m_t  b2(2, 3);
-		b2=       0.0;
-		b2[0][1]= 2.0;
-		b2[1][2]= 3.0;
-		ins(1, 0) << b2;
+	// Second block
+	m_t  b2(2, 3);
+	b2=       0.0;
+	b2[0][1]= 2.0;
+	b2[1][2]= 3.0;
+	ins(1, 0) << b2;
 
-		m_t b3(3, 1);
-		b3= 0.0;
-		b3[1][0]= 4.0;
-		ins(2, 1) << b3;
+	m_t b3(3, 1);
+	b3= 0.0;
+	b3[1][0]= 4.0;
+	ins(2, 1) << b3;
     }
-    cout << "A is " << A << endl; // works but is completely unreadable
+    // cout << "A is " << A << endl; // doesn't works and before it was completely unreadable anyway
 
     /* Should be something like this:
 
-    [                   [ 1]] // b1
-    [[  0   2   0]          ] // b2
-    [[  0   0   3]          ]
-    [             [  0]     ] // b3
-    [             [  4]     ]
-    [             [  0]     ]
+       [                   [ 1]] // b1
+       [[  0   2   0]          ] // b2
+       [[  0   0   3]          ]
+       [             [  0]     ] // b3
+       [             [  4]     ]
+       [             [  0]     ]
 
     */
 
@@ -98,8 +98,8 @@ int test_main(int argc, char* argv[])
     cout << "In block A[1][0], the element [0][1] is " << A[1][0][0][1] << endl << endl;
 
 
-	typedef mtl::dense_vector<double> v_t;
-	typedef mtl::dense_vector<v_t>    vector_t;
+    typedef mtl::dense_vector<double> v_t;
+    typedef mtl::dense_vector<v_t>    vector_t;
     vector_t                          x(3), y(3);
 
     // x= [[0, 5, 3], [1], [8]]^T
@@ -119,7 +119,7 @@ int test_main(int argc, char* argv[])
     y= A*x;
 
     cout << "y after multiplication is " << y << endl
-		 << "Should be [[8], [10, 9], [0, 4, 0]]^T." << endl; 
+	 << "Should be [[8], [10, 9], [0, 4, 0]]^T." << endl; 
 
     return 0;
 }
