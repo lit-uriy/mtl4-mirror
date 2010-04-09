ac88be963232c2b23584b4d76ddf8a851988eccf
cornelius <cornelius@df6d15db-99ea-0310-92f0-9d27c566f14b>
2010-04-09 11:43:15 +0000

    svd for quadratic matrix

--- trunk/mtl4/branches/multigrid/boost/numeric/mtl/operation/svd.hpp
+++ trunk/mtl4/branches/multigrid/boost/numeric/mtl/operation/svd.hpp
@@ -15,76 +15,67 @@
 #define MTL_MATRIX_SVD_INCLUDE
 
 #include <cmath>
-#include <boost/numeric/linear_algebra/identity.hpp>
-#include <boost/numeric/linear_algebra/inverse.hpp>
-#include <boost/numeric/mtl/utility/exception.hpp>
-#include <boost/numeric/mtl/utility/irange.hpp>
-#include <boost/numeric/mtl/concept/collection.hpp>
-#include <boost/numeric/mtl/concept/magnitude.hpp>
-#include <boost/numeric/mtl/operation/rank_one_update.hpp>
+#include <boost/numeric/mtl/matrix/strict_upper.hpp>
+#include <boost/numeric/mtl/operation/diagonal.hpp>
+#include <boost/numeric/mtl/operation/one_norm.hpp>
 #include <boost/numeric/mtl/operation/trans.hpp>
+#include <boost/numeric/mtl/operation/two_norm.hpp>
 #include <boost/tuple/tuple.hpp>
-
 
 namespace mtl { namespace matrix {
 
-// QR-Factorization of matrix A
-// Return A=S*V*D' 
+/// QR-Factorization of matrix A
+/// Return A=S*V*D' at the moment only for quadratic matrix A
 template <typename Matrix>
 boost::tuple<typename mtl::dense2D<typename Collection<Matrix>::value_type>,
-      typename mtl::dense2D<typename Collection<Matrix>::value_type>,
-      typename mtl::dense2D<typename Collection<Matrix>::value_type> >
+	     typename mtl::dense2D<typename Collection<Matrix>::value_type>,
+	     typename mtl::dense2D<typename Collection<Matrix>::value_type> >
 inline svd(const Matrix& A, double tol)
 {
     typedef typename Collection<Matrix>::value_type   value_type;
     typedef typename Collection<Matrix>::size_type    size_type;
-    size_type        ncols = num_cols(A), nrows = num_rows(A), loops;
+    size_type        ncols = num_cols(A), nrows = num_rows(A), loops, i(0);
     value_type       zero= math::zero(A[0][0]), one= math::one(A[0][0]);
-    Matrix           S(nrows,nrows), V(A), VT(ncols,nrows), D(ncols,ncols), a(A), Q(ncols,ncols), R(ncols,nrows), AT(ncols,nrows);
-    double 	     err(10000);
-
-    
-    //if ( nrows > ncols ) throw mtl::logic_error("underdetermined system, use trans(A) instead of A");
+    Matrix           S(nrows,nrows), V(A), E(nrows,ncols), D(ncols,ncols), Q(ncols,ncols), R(ncols,nrows);
+    double 	     err(10000), e, f;
+ 
+    if ( nrows > ncols ) throw mtl::logic_error("underdetermined system, use trans(A) instead of A");
     
     //init
     loops= 100* std::max(nrows,ncols);
-    std::cout<< "loops=" << loops << "\n";
-    S= one; D= one; VT= trans(A);
-    std::cout<< "S=\n" << S << "\n";
-    std::cout<< "V=\n" << V << "\n";
-    std::cout<< "D=\n" << D << "\n";
+    S= one; D= one; V= A;
    
+    while (err > tol && i < loops ) {
+	boost::tie(Q, R)= qr((V));
+	S*= Q;
+	V= trans(R);
+	boost::tie(Q, R)= qr((V));
+	D*= Q;
 
-    for (size_type i = 0; i < 3; i++) {
-	std::cout<< "i=" << i << "\n";
-	boost::tie(Q, R)= qr((VT));
-        std::cout<< "Q=\n" << Q << "\n";
-        std::cout<< "R=\n" << R << "\n";
-	AT=Q*R;
-        std::cout<< "AT=\n" << trans(R)*trans(Q) << "\n";
-	
-	
-	S= S*trans(Q);
-	std::cout<< "S=\n" << S << "\n";
-	a= Q*R;
-	std::cout<< "a=\n" << a << "\n";
-	V= V*Q;
-	std::cout<< "V=\n" << V << "\n";
-	
-	//ready for exit???
-	
-
+	//ready for exit when upper(R)=0
+	E= triu(R,1);
+	e= one_norm(E);
+	f= two_norm(diagonal(R));
+	if ( f== zero ) f= 1;
+	err= e/f;
+	i++;
+	V= trans(R);
+    } //end while
+    
+    //fix signs in V
+    V= 0;
+    for (size_type i= 0; i < ncols; i++) {
+	V[i][i]= std::abs(R[i][i]);
+	if (R[i][i] < zero) {
+	    for (size_type j= 0; j < nrows; j++) {
+		S[j][i]= -S[j][i];
+	    }
+	}
     }
-    std::cout<< "Fertig inline\n";
+
     return boost::make_tuple(S,V,D);
 }
 
-
-
-
-
 }} // namespace mtl::matrix
 
-
 #endif // MTL_MATRIX_SVD_INCLUDE
-
