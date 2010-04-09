8bf0589b885bd3c957098ee4e4276793905aed99
cornelius <cornelius@df6d15db-99ea-0310-92f0-9d27c566f14b>
2010-04-09 11:43:15 +0000

    svd for quadratic matrix

--- mtl4/branches/multigrid/libs/numeric/mtl/test/svd_test.cpp
+++ mtl4/branches/multigrid/libs/numeric/mtl/test/svd_test.cpp
@@ -19,7 +19,7 @@ using namespace std;
 int test_main(int argc, char* argv[])
 {
     using namespace mtl;
-    unsigned size=3, row= size, col=size+1;
+    unsigned size=3, row= size, col=size;
 
     double b;
     dense_vector<double>                    vec(size), vec1(size);
@@ -29,15 +29,15 @@ int test_main(int argc, char* argv[])
     dr[0][0]=1;
     dr[0][1]=1;
     dr[0][2]=1;
-    dr[0][3]=4;
+    //dr[0][3]=4;
     dr[1][0]=1;
     dr[1][1]=2;
     dr[1][2]=2;
-    dr[1][3]=3;
+    //dr[1][3]=3;
     dr[2][0]=9;
     dr[2][1]=3;
     dr[2][2]=2;
-    dr[2][3]=4;
+    //dr[2][3]=4;
     std::cout<<"A=\n"<< dr <<"\n";
     std::cout<<"START--------------\n";
 
@@ -50,7 +50,8 @@ int test_main(int argc, char* argv[])
 //    std::cout<<"MAtrix  A=\n"<< dr <<"\n";
 	dr_t= S*V*trans(D);
     std::cout<<"MAtrix  A=S*V*D'=\n"<< dr_t <<"\n";
-
+    std::cout<<"Original A==\n"<< dr <<"\n";
+    
     return 0;
 }
 
