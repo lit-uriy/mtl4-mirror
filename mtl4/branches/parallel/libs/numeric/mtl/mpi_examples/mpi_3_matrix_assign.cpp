1704c619481f0b3d74f46be455df1874d011e086
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2010-08-03 11:14:33 +0000

    Matrix assignment with move semantics.

--- mtl4/branches/parallel/libs/numeric/mtl/mpi_examples/mpi_3_matrix_assign.cpp
+++ mtl4/branches/parallel/libs/numeric/mtl/mpi_examples/mpi_3_matrix_assign.cpp
@@ -15,24 +15,9 @@ int main(int argc, char* argv[])
     typedef mtl::matrix::distributed<mtl::compressed2D<float> >  matrix_type;
     matrix_type A(7, 7, row_dist, col_dist), B(7, 7, row_dist, col_dist);
 
-    {
-	mtl::matrix::inserter<matrix_type> ins(A);
-	if (world.rank() == 0) {
-	    ins[0][0] << 1.0;
-	    ins[1][3] << 3.0;
-	    ins[4][1] << 2.0; // remote
-	    ins[6][5] << 4.0; // remote
-	} else if (world.rank() == 2) {
-	    ins[2][6] << 5.0; // remote
-	    ins[3][2] << 6.0; // remote
-	    ins[5][4] << 8.0; // remote
-	}
-    }
-
-    mtl::par::sout << "The matrix A is\n" << A << "\n";
-
+    A= 6;
     B= A;
     mtl::par::sout << "The matrix B is\n" << B << "\n";
-
+ 
     return 0;
 }
