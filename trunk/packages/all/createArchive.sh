0177dc81aa5bb3b14e8b7245984b3281f57857ee
anaum <anaum@df6d15db-99ea-0310-92f0-9d27c566f14b>
2010-11-01 19:35:12 +0000

    extended all and download table with zip archives

--- trunk/packages/all/createArchive.sh
+++ trunk/packages/all/createArchive.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-cp ../standard/MTL-*.tar.gz ../examples/MTL-examples*.tar.gz ../tests/MTL-test*.tar.gz . ;
+cp ../standard/MTL-*.tar.gz ../examples/MTL-examples*.tar.gz ../tests/MTL-tests*.tar.gz . ;
 LONGNAME=`ls MTL-all-*.tar.gz | sed 's/\.gz//'` ;
-gunzip MTL-*.tar.gz ;
+ALL_DIRNAME=`echo ${LONGNAME} | sed 's/\.tar//'` ;
+
 rm MTL-all*.tar.bz2 ;
+rm MTL-all*.tar.gz ;
 
-mv MTL-4*.tar ${LONGNAME} ;
-tar -Af ${LONGNAME} MTL-examples-*.tar ;
-tar -Af ${LONGNAME} MTL-test*.tar ;
+mkdir ${ALL_DIRNAME} ;
+for d in $(ls MTL-*.tar.gz); do
+tar -xf ${d} ;
+rm -rf ${d} ;
+curdirname=`echo ${d} | sed 's/\.tar\.gz//'` ;
+cp -r ${curdirname}/* ${ALL_DIRNAME}/ ;
+rm -rf ${curdirname} ;
+done
 
-gzip -c ${LONGNAME} > ${LONGNAME}.gz ;
-bzip2 ${LONGNAME} ;
-
-rm MTL-examp*.tar ;
-rm MTL-test*.tar ;
+tar -czf ${ALL_DIRNAME}.tar.gz ${ALL_DIRNAME}
+tar -cjf ${ALL_DIRNAME}.tar.bz2 ${ALL_DIRNAME}
+zip -q -r ${ALL_DIRNAME}.zip ${ALL_DIRNAME}
