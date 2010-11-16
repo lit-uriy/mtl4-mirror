c8d46c8c20d53748d3b9e1b30a4b58187a23c899
cornelius <cornelius@df6d15db-99ea-0310-92f0-9d27c566f14b>
2010-11-16 15:59:59 +0000

    rpm signing

--- packages/createRpmRepo.sh
+++ packages/createRpmRepo.sh
@@ -5,4 +5,6 @@ cp tests/*.rpm mtl_rpm_repo/main/
 cp all/*.rpm mtl_rpm_repo/main/
 cd mtl_rpm_repo/main
 createrepo .
+gpg -a --detach-sign repodata/repomd.xml
+gpg -a --export > repodata/repomd.xml.key
 cd ../../
