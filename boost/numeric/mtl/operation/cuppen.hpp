f98d83b2cdc44449f036a0e690f2ee57d0de0c53
cornelius <cornelius@df6d15db-99ea-0310-92f0-9d27c566f14b>
2011-01-10 10:48:42 +0000

    first cleanup and refactoring cuppen

--- boost/numeric/mtl/operation/cuppen.hpp
+++ boost/numeric/mtl/operation/cuppen.hpp
@@ -43,15 +43,16 @@ void inline cuppen(const Matrix& A, Matrix& Q, Matrix& L, Vector& p)
     typedef typename Collection<Matrix>::value_type   value_type;
     typedef typename Magnitude<value_type>::type      magnitude_type; // to multiply with 2 not 2+0i
     typedef typename Collection<Matrix>::size_type    size_type;
+    typedef typename Collection<Vector>::value_type    vec_value_type;
     size_type        ncols = num_cols(A), nrows = num_rows(A), m, n;
     value_type       zero= math::zero(A[0][0]), one= math::one(A[0][0]);//, h00, h10, h11, beta, mu, a, b, tol;
+    vec_value_type   zerovec= math::zero(p[0]);
   //  const magnitude_type two(2);
     Matrix           T(nrows,ncols);
     
     MTL_THROW_IF(ncols != nrows , matrix_not_square());
-std::cout<< "start p=" << p <<"\n";
-     dense_vector<value_type>    v(nrows, zero), v1(nrows, zero), diag(nrows, zero), lambda(nrows, zero);
-     Vector     perm(nrows, zero), permdiag(nrows, zero);;
+    dense_vector<value_type>    v(nrows, zero), v1(nrows, zero), diag(nrows, zero), lambda(nrows, zero);
+    Vector     perm(nrows, zerovec), permdiag(nrows, zerovec);;
     
     if (ncols == 1){
       L[0][0]= A[0][0];
@@ -59,34 +60,34 @@ std::cout<< "start p=" << p <<"\n";
     } else {
       m= size_type(nrows/2);
       n= nrows - m;
-      Vector   perm1(m, zero), perm2(n, zero), perm_intern(nrows, zero);
+      Vector   perm1(m, zerovec), perm2(n, zerovec), perm_intern(nrows, zerovec);
       Matrix           T1(m, m), T2(n, n), Q1(m,m),Q2(n,n),L1(m,m), L2(n,n);
       //DIVIDE
       value_type b(A[m-1][m]);
       T1= sub_matrix(A, 0, m, 0, m);
       T1[m-1][m-1]-= abs(b);
-      std::cout<< "n=" << n <<"\n";
+//       std::cout<< "n=" << n <<"\n";
       T2= sub_matrix(A, m, nrows, m, nrows);
       T2[0][0]-= abs(b);
-
+#if 0
       std::cout << "A is\n" << A;
       std::cout << "T1 is\n" << T1;
       std::cout << "T2 is\n" << T2;
-
+#endif
 
 
 //       std::cout<< "b=" << b <<"\n";
       v[m-1]= b > zero ? one : -one;
       v[m]= one;
-       std::cout<< "v_=" << v <<"\n";
+//        std::cout<< "v_=" << v <<"\n";
       for (size_type i = 0; i < m; i++)
 	perm1[i]= i;
       for (size_type i = 0; i < n; i++)
 	perm2[i]= i;
       cuppen(T1, Q1, L1, perm1);
       cuppen(T2, Q2, L2, perm2);
-       std::cout<< "perm1=" << perm1 <<"\n";
-       std::cout<< "perm2=" << perm2 <<"\n";
+//        std::cout<< "perm1=" << perm1 <<"\n";
+//        std::cout<< "perm2=" << perm2 <<"\n";
       mtl::matrix::traits::permutation<>::type P1= mtl::matrix::permutation(perm1);
       mtl::matrix::traits::permutation<>::type P2= mtl::matrix::permutation(perm2);
 
@@ -96,7 +97,7 @@ std::cout<< "start p=" << p <<"\n";
      // perm2+= m;
       perm_intern[irange(0,m)]= perm1;
       perm_intern[irange(m, imax)]= perm2;
-      std::cout<< "perm_intern=" << perm_intern << "\n";
+//       std::cout<< "perm_intern=" << perm_intern << "\n";
       perm= perm_intern;
       
 //       std::cout<< "v_2=" << v <<"\n";
@@ -116,14 +117,15 @@ std::cout<< "start p=" << p <<"\n";
 //  std::cout<< "v_3=" << v <<"\n";
        
       sort(diag, perm);
-       std::cout << "diag=" << diag << "\n";
-       std::cout << "perm=" << perm << "\n";
+//        std::cout << "diag=" << diag << "\n";
+//        std::cout << "perm=" << perm << "\n";
 //       std::cout<< "T=" << T <<"\n";
       //permutation on Matrix T
       mtl::matrix::traits::permutation<>::type P= mtl::matrix::permutation(perm);
      mtl::matrix::traits::permutation<>::type PV= mtl::matrix::permutation(perm_intern);
-    //       std::cout << "\nP =\n" << P;    
-std::cout << "\nP1 =\n" << P1;   
+#if 0
+       std::cout << "\nP =\n" << P;    
+      std::cout << "\nP1 =\n" << P1;   
        std::cout << "\nP2 =\n" << P2;   
       std::cout<< "Q1=" << Q1 <<"\n";
       std::cout<< "Q2=" << Q2 <<"\n";
@@ -132,36 +134,37 @@ std::cout << "\nP1 =\n" << P1;
       if (nrows > 2){
       Matrix Q1P( P1*Q1*P1 ), Q2P( P2*Q2*P2 );
       
-        std::cout << "\nQ1P =\n" << Q1P;   
-        std::cout << "\nQ2P =\n" << Q2P;   
+//         std::cout << "\nQ1P =\n" << Q1P;   
+//         std::cout << "\nQ2P =\n" << Q2P;   
       }
-     
-       std::cout << "perm1 =" << perm1 << "\n"; 
-       std::cout << "perm2 =" << perm2 << "\n";
+
+//        std::cout << "perm1 =" << perm1 << "\n"; 
+//        std::cout << "perm2 =" << perm2 << "\n";
 //       std::cout << "v =" << v;
 //       std::cout<< "Q1=" << Q1 <<"\n";
 //       std::cout<< "Q2=" << Q2 <<"\n";
 //       std::cout<< "m=" << m <<"\n";
+#endif
        v[irange(0,m)]= Q1[irange(0,m)][m-1];
        v[irange(m,imax)]=Q2[irange(0,n)][0];
-       std::cout<< "PV=" << PV << "\n";
- v1=PV*v;
   
-       std::cout << "Q1 is\n" << Q1;
-       std::cout << "Q2 is\n" << Q2;
+	v1=PV*v;
+  
+//        std::cout << "Q1 is\n" << Q1;
+//        std::cout << "Q2 is\n" << Q2;
 
 #if 0 // transposed yields the same
        v[irange(0,m)]= trans(Q1[m-1][irange(0,m)]);
        v[irange(m,imax)]= trans(Q2[0][irange(0,n)]);
 #endif
 
-      std::cout << "Vector perm is " << perm << '\n';
-std::cout << "Vector v1 is " << v1 << '\n';
-std::cout << "Vector v is " << v << '\n';
-std::cout << "Vector diag is " << diag << '\n';
-permdiag=perm;
-sort(permdiag, v1);
-std::cout << "Vector v1 is " << v1 << '\n';
+//       std::cout << "Vector perm is " << perm << '\n';
+// std::cout << "Vector v1 is " << v1 << '\n';
+// std::cout << "Vector v is " << v << '\n';
+// std::cout << "Vector diag is " << diag << '\n';
+	permdiag=perm;
+	sort(permdiag, v1);
+// std::cout << "Vector v1 is " << v1 << '\n';
 //        sort(perm, v); //permutation on v
 //       std::cout << "QQQ   v =" << v;
 //       std::cout<< "diag= " << diag << "\n";
@@ -182,30 +185,30 @@ std::cout << "Vector v1 is " << v1 << '\n';
       for(size_type i = 0; i < nrows; i++){
 	  dense_vector<value_type>    test(nrows, zero), lambda_i(nrows, lambda[i]);
 	  test=diag-lambda_i;
-	  std::cout<< "test=" << test << "\n";
+// 	  std::cout<< "test=" << test << "\n";
 	  for(size_type k = 0; k < nrows; k++)
 	    test[k]=1/test[k];
 	  lambda_i= ele_prod(test, v);
-	  std::cout<< "lambda_i=" << lambda_i << "\n";
-	  test=TP*lambda_i;  //Permutation von Q beachten
-	  std::cout<< "TESTing.......=" << test << "\n";
+// 	  std::cout<< "lambda_i=" << lambda_i << "\n";
+// 	  test=TP*lambda_i;  //Permutation von Q beachten
+// 	  std::cout<< "TESTing.......=" << test << "\n";
 	  test=lambda[i]*lambda_i;  
-	  std::cout<< "TESTing.......=" << test << "\n";
+// 	  std::cout<< "TESTing.......=" << test << "\n";
 	  
           lambda_i/=two_norm(lambda_i);
 	  Q[irange(0, imax)][i]= lambda_i;
 	 
       }
        L=mtl::vector::diagonal(lambda);
-       std::cout<< "L=\n" << L << "\n";
-       std::cout<< "Q=\n" << Q << "\n";
+//        std::cout<< "L=\n" << L << "\n";
+//        std::cout<< "Q=\n" << Q << "\n";
   
      
     }
     
     p=perm;
    
-    std::cout<< "end p=" << p <<"\n";
+//     std::cout<< "end p=" << p <<"\n";
 }
 
 }} // namespace mtl::matrix
