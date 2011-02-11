5ad07634ff51645daf934da1ea7dbbfcca544155
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2011-02-11 12:00:33 +0000

    divide_by_inplace with functor instead of lambda.

--- trunk/boost/numeric/mtl/operation/divide_by_inplace.hpp
+++ trunk/boost/numeric/mtl/operation/divide_by_inplace.hpp
@@ -12,18 +12,20 @@
 #include <boost/numeric/mtl/mtl_fwd.hpp>
 #include <boost/numeric/mtl/utility/tag.hpp>
 #include <boost/numeric/mtl/operation/assign_each_nonzero.hpp>
+#include <boost/numeric/mtl/operation/divide_by.hpp>
 
-#include <boost/lambda/lambda.hpp>
+// #include <boost/lambda/lambda.hpp>
 
 
 namespace mtl {
 	
 	
 	/// Divide collection \p c (from right) by scalar factor \p alpha; \p c is altered
-	template <typename Factor, typename Collection>
-	void divide_by_inplace(Collection& c, const Factor& alpha, tag::scalar)
+	template <typename Factor, typename Coll>
+	void divide_by_inplace(Coll& c, const Factor& alpha, tag::scalar)
 	{
-		assign_each_nonzero(c, boost::lambda::_1 / alpha);
+	    // assign_each_nonzero(c, boost::lambda::_1 / alpha);
+	    assign_each_nonzero(c, tfunctor::divide_by<typename Collection<Coll>::value_type, Factor>(alpha));
 	}
 	
 	/// Divide collection \p c (from right) by factor \p alpha; \p c is altered
