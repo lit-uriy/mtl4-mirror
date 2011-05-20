a378fc90b26f6d67a2abf0cfce17a5a55edcf89d
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2011-05-20 07:10:21 +0000

    Empty lines removed.

--- trunk/libs/numeric/mtl/examples/matrix_indirect.cpp
+++ trunk/libs/numeric/mtl/examples/matrix_indirect.cpp
@@ -1,5 +1,4 @@
 // Filename: matrix_indirect.cpp (part of MTL4)
-
 #include <iostream>
 #include <boost/numeric/mtl/mtl.hpp>
 
@@ -14,7 +13,6 @@ int main(int, char**)
     rows= 2, 0, 3; cols= 2, 1;
 
     cout << "rows = " << rows << ", cols = " << cols << "\n"   
-	 << "The sub-matrix A[{2, 0, 3}][{2, 1}] is\n" << A[rows][cols];
-	
+	 << "The sub-matrix A[{2, 0, 3}][{2, 1}] is\n" << A[rows][cols];	
     return 0;
 }
