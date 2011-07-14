90ba7bc6b28a865e9bb70e910f78bb33a17c0e35
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2011-07-14 07:45:12 +0000

    Do not depend on preceeding 0s.

--- make_vampir_groups.py
+++ make_vampir_groups.py
@@ -8,15 +8,14 @@ group_names=['mtl_utilities', 'mtl_fsize', 'mtl_vector', 'mtl_vecmat', 'mtl_matr
 groups=[[],[],[],[],[],[],[],[],[],[]]
 assert len(group_names) == len(groups)
 
-pat= re.compile('template <> std::string vampir_trace<(\d).+>::name\("(.*)"\);')
+pat= re.compile('template <> std::string vampir_trace<(\d+)>::name\("(.*)"\);')
 
 for s in vptfile.readlines() : 
     mm= pat.search(s)
     if mm and mm.group(2):
-        groups[int(mm.group(1))].append(mm.group(2))
+        groups[int(mm.group(1))/1000].append(mm.group(2))
 
 for i in range(len(groups)):
     if group_names[i]:
         groupfile.write(group_names[i]+"="+";".join(groups[i])+'\n')
-        #print group_names[i]+"="+";".join(groups[i])
 groupfile.close()
