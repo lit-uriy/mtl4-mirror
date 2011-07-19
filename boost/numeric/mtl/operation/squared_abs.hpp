21edfaec3fac5ebc678f8db37b1f937c8ad7ba01
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2011-07-19 09:00:08 +0000

    squared_abs uses std::norm (might use assembler functions

--- boost/numeric/mtl/operation/squared_abs.hpp
+++ boost/numeric/mtl/operation/squared_abs.hpp
@@ -14,6 +14,7 @@
 #define MTL_SQUARED_ABS_INCLUDE
 
 #include <cmath>
+#include <complex>
 #include <boost/mpl/or.hpp>
 #include <boost/utility/enable_if.hpp>
 #include <boost/type_traits/is_floating_point.hpp>
@@ -28,7 +29,7 @@ template <typename T>
 typename boost::enable_if<boost::mpl::or_<boost::is_integral<T>, boost::is_floating_point<T> >, T>::type
 inline squared_abs(const T& x)
 {
-	vampir_trace<10> tracer;
+    vampir_trace<10> tracer;
     return x * x;
 }
 
@@ -36,17 +37,17 @@ inline squared_abs(const T& x)
 template <typename T>
 T inline squared_abs(const std::complex<T>& z)
 {
-	vampir_trace<11> tracer;
-    T x= real(z), y= imag(z);
-    return x * x + y * y;
+    vampir_trace<11> tracer;
+    using std::norm;
+    return norm(z);
 }
 
-/// When squaring magnitudes of intrinsic types the abs can be omitted 
+/// When squaring magnitudes of non-intrinsic and non-complex types we use abs for security 
 template <typename T>
 typename boost::disable_if<boost::mpl::or_<boost::is_integral<T>, boost::is_floating_point<T> >, T>::type
 inline squared_abs(const T& x)
 {
-	vampir_trace<12> tracer;
+    vampir_trace<12> tracer;
     using std::abs;
     T a= abs(x);
     return a * a;
