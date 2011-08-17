30a35f03f5f1c7f0d886d0efcb5514fec2a2daa8
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2011-08-17 10:02:24 +0000

    lower_trisolve implemented with class.

--- trunk/boost/numeric/mtl/operation/lower_trisolve.hpp
+++ trunk/boost/numeric/mtl/operation/lower_trisolve.hpp
@@ -19,7 +19,6 @@
 #include <boost/numeric/mtl/utility/range_generator.hpp>
 #include <boost/numeric/mtl/utility/category.hpp>
 #include <boost/numeric/mtl/concept/collection.hpp>
-#include <boost/numeric/mtl/operation/adjust_cursor.hpp>
 
 #include <boost/numeric/linear_algebra/identity.hpp>
 #include <boost/numeric/linear_algebra/inverse.hpp>
@@ -30,6 +29,121 @@ namespace mtl { namespace matrix {
 
 namespace detail {
 
+    // CompactStorage means that matrix contains only upper entries (strict upper when DiaTag == unit_diagonal)
+    template <typename Matrix, typename DiaTag, bool CompactStorage= false>
+    struct lower_trisolve_t
+    {
+	typedef typename Collection<Matrix>::value_type         	       	    value_type;
+	typedef typename Collection<Matrix>::size_type          	       	    size_type;
+	typedef typename OrientedCollection<Matrix>::orientation	       	    my_orientation;
+	typedef typename mtl::traits::category<Matrix>::type    	       	    my_category;
+	typedef typename mtl::traits::range_generator<tag::major, Matrix>::type     a_cur_type; // row or col depending on Matrix    
+	typedef typename mtl::traits::range_generator<tag::nz, a_cur_type>::type    a_icur_type;   
+
+	lower_trisolve_t(const Matrix& A) : A(A), value_a(A), col_a(A), row_a(A)
+	{    MTL_THROW_IF(num_rows(A) != num_cols(A), matrix_not_square());	}
+	
+	template <typename VectorIn, typename VectorOut>
+	void operator()(const VectorIn& v, VectorOut& w)
+	{   apply(v, w, my_orientation(), DiaTag());	}
+	
+
+      private:
+	template <typename Value>
+	Value inline lower_trisolve_diavalue(const Value& v, tag::regular_diagonal)
+	{   using math::reciprocal; return reciprocal(v); }
+
+	template <typename Value>
+	Value lower_trisolve_diavalue(const Value& v, tag::inverse_diagonal)
+	{  return v;	}    
+
+	template <typename Tag> int dia_inc(Tag) { return 0; }
+	int dia_inc(tag::unit_diagonal) { return 1; }
+
+	template <typename VectorIn, typename VectorOut>
+	void apply(const VectorIn& v, VectorOut& w, tag::row_major, tag::unit_diagonal)
+	{
+	    using namespace tag; 
+	    w= v;
+	    a_cur_type ac= begin<row>(A), aend= end<row>(A); // search for end ???
+	    ++ac;
+	    for (size_type r= 1; ac != aend; ++r, ++ac) {
+		a_icur_type aic= begin<nz>(ac), aiend= lower_bound<nz>(ac, r);
+		typename Collection<VectorOut>::value_type rr= w[r];
+		for (; aic != aiend; ++aic) {
+		    MTL_DEBUG_THROW_IF(col_a(*aic) >= r, logic_error("Matrix entries must be sorted for this."));
+		    rr-= value_a(*aic) * w[col_a(*aic)];
+		}
+		w[r]= rr;
+	    }
+	}
+
+	template <typename VectorIn, typename VectorOut, typename DDiaTag>
+	void apply(const VectorIn& v, VectorOut& w, tag::row_major, DDiaTag)
+	{
+	    using namespace tag; 
+	    w= v;
+	    a_cur_type ac= begin<row>(A), aend= end<row>(A); 
+	    for (size_type r= 0; ac != aend; ++r, ++ac) {
+		a_icur_type aic= begin<nz>(ac), aiend= lower_bound<nz>(ac, r+1);
+		MTL_THROW_IF(aic == aiend, missing_diagonal());
+		--aiend;
+		MTL_THROW_IF(col_a(*aiend) != r, missing_diagonal());
+
+		value_type dia= value_a(*aiend);
+		typename Collection<VectorOut>::value_type rr= w[r];
+
+		for (; aic != aiend; ++aic) {
+		    MTL_DEBUG_THROW_IF(col_a(*aic) >= r, logic_error("Matrix entries must be sorted for this."));
+		    rr-= value_a(*aic) * w[col_a(*aic)];
+		}
+		w[r]= rr * lower_trisolve_diavalue(dia, DDiaTag());
+	    }
+	}	
+
+	template <typename VectorIn, typename VectorOut>
+	void apply(const VectorIn& v, VectorOut& w, tag::col_major, tag::unit_diagonal)
+	{
+	    using namespace tag; 
+	    w= v;
+	    a_cur_type ac= begin<col>(A), aend= end<col>(A); 
+	    for (size_type r= 0; ac != aend; ++r, ++ac) {
+		a_icur_type aic= lower_bound<nz>(ac, r+1), aiend= end<nz>(ac); // search for end ???
+		typename Collection<VectorOut>::value_type rr= w[r];
+
+		for (; aic != aiend; ++aic) {
+		    MTL_DEBUG_THROW_IF(row_a(*aic) <= r, logic_error("Matrix entries must be sorted for this."));
+		    w[row_a(*aic)]-= value_a(*aic) * rr;
+		}
+	    }
+	}
+
+	template <typename VectorIn, typename VectorOut, typename DDiaTag>
+	void apply(const VectorIn& v, VectorOut& w, tag::col_major, DDiaTag)
+	{
+	    using namespace tag;
+	    w= v;
+	    a_cur_type ac= begin<col>(A), aend= end<col>(A); 
+	    for (size_type r= 0; ac != aend; ++r, ++ac) {
+		a_icur_type aic= lower_bound<nz>(ac, r), aiend= end<nz>(ac);
+		MTL_DEBUG_THROW_IF(aic == aiend || row_a(*aic) != r, missing_diagonal());
+		typename Collection<VectorOut>::value_type rr= w[r]*= lower_trisolve_diavalue(value_a(*aic), DDiaTag());
+
+		for (++aic; aic != aiend; ++aic) {
+		    MTL_DEBUG_THROW_IF(row_a(*aic) <= r, logic_error("Matrix entries must be sorted for this."));
+		    w[row_a(*aic)]-= value_a(*aic) * rr;
+		}
+	    }
+	}
+
+	const Matrix&                                    A;
+	typename mtl::traits::const_value<Matrix>::type  value_a; 
+	typename mtl::traits::col<Matrix>::type          col_a; 
+	typename mtl::traits::row<Matrix>::type          row_a;
+    };
+
+
+#if 0 // =============================
     template <typename Value>
     Value inline lower_trisolve_diavalue(const Value& v, tag::regular_diagonal)
     {
@@ -48,7 +162,7 @@ namespace detail {
     {
 	vampir_trace<5022> tracer;
 	namespace traits = mtl::traits;
-	using namespace tag; using traits::range_generator; using math::one; using mtl::detail::adjust_cursor;
+	using namespace tag; using traits::range_generator; using mtl::detail::adjust_cursor;
 
 	typedef typename Collection<Matrix>::value_type           value_type;
 	typedef typename Collection<Matrix>::size_type            size_type;
@@ -155,7 +269,7 @@ namespace detail {
 	    }
 	}
     }
-
+#endif
 
 }  // detail
 
@@ -165,7 +279,8 @@ template <typename Matrix, typename Vector>
 Vector inline lower_trisolve(const Matrix& A, const Vector& v)
 {
     Vector w(resource(v));
-    detail::lower_trisolve(A, v, w, typename OrientedCollection<Matrix>::orientation(), tag::regular_diagonal());
+    detail::lower_trisolve_t<Matrix, tag::regular_diagonal> solver(A); 
+    solver(v, w);
     return w;
 }
 
@@ -173,7 +288,8 @@ Vector inline lower_trisolve(const Matrix& A, const Vector& v)
 template <typename Matrix, typename VectorIn, typename VectorOut>
 inline void lower_trisolve(const Matrix& A, const VectorIn& v, VectorOut& w)
 {
-    detail::lower_trisolve(A, v, w, typename OrientedCollection<Matrix>::orientation(), tag::regular_diagonal());
+    detail::lower_trisolve_t<Matrix, tag::regular_diagonal> solver(A); 
+    solver(v, w);
 }
 
 /// Solves the lower triangular matrix A (only one's in the diagonal) with the rhs v and returns the solution vector
@@ -181,7 +297,8 @@ template <typename Matrix, typename Vector>
 Vector inline unit_lower_trisolve(const Matrix& A, const Vector& v)
 {
     Vector w(resource(v));
-    detail::lower_trisolve(A, v, w, typename OrientedCollection<Matrix>::orientation(), tag::unit_diagonal());
+    detail::lower_trisolve_t<Matrix, tag::unit_diagonal> solver(A); 
+    solver(v, w);
     return w;
 }
 
@@ -189,7 +306,8 @@ Vector inline unit_lower_trisolve(const Matrix& A, const Vector& v)
 template <typename Matrix, typename VectorIn, typename VectorOut>
 inline void unit_lower_trisolve(const Matrix& A, const VectorIn& v, VectorOut& w)
 {
-    detail::lower_trisolve(A, v, w, typename OrientedCollection<Matrix>::orientation(), tag::unit_diagonal());
+    detail::lower_trisolve_t<Matrix, tag::unit_diagonal> solver(A); 
+    solver(v, w);
 }
 
 /// Solves the lower triangular matrix A (inverse the diagonal) with the rhs v and returns the solution vector
@@ -197,7 +315,8 @@ template <typename Matrix, typename Vector>
 Vector inline inverse_lower_trisolve(const Matrix& A, const Vector& v)
 {
     Vector w(resource(v));
-    detail::lower_trisolve(A, v, w, typename OrientedCollection<Matrix>::orientation(), tag::inverse_diagonal());
+    detail::lower_trisolve_t<Matrix, tag::inverse_diagonal> solver(A); 
+    solver(v, w);
     return w;
 }
 
@@ -205,21 +324,24 @@ Vector inline inverse_lower_trisolve(const Matrix& A, const Vector& v)
 template <typename Matrix, typename VectorIn, typename VectorOut>
 inline void inverse_lower_trisolve(const Matrix& A, const VectorIn& v, VectorOut& w)
 {
-    detail::lower_trisolve(A, v, w, typename OrientedCollection<Matrix>::orientation(), tag::inverse_diagonal());
+    detail::lower_trisolve_t<Matrix, tag::inverse_diagonal> solver(A); 
+    solver(v, w);
 }
 
 template <typename Matrix, typename Vector, typename DiaTag>
 Vector inline lower_trisolve(const Matrix& A, const Vector& v, DiaTag)
 {
     Vector w(resource(v));
-    detail::lower_trisolve(A, v, w, typename OrientedCollection<Matrix>::orientation(), DiaTag());
+    detail::lower_trisolve_t<Matrix, DiaTag> solver(A); 
+    solver(v, w);
     return w;
 }
 
 template <typename Matrix, typename VectorIn, typename VectorOut, typename DiaTag>
 inline void lower_trisolve(const Matrix& A, const VectorIn& v, VectorOut& w, DiaTag)
 {
-    detail::lower_trisolve(A, v, w, typename OrientedCollection<Matrix>::orientation(), DiaTag());
+    detail::lower_trisolve_t<Matrix, DiaTag> solver(A); 
+    solver(v, w);
 }
 
 
