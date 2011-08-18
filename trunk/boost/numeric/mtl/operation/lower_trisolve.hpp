0613351a0d10c74465d1038aa3933dc79ab7ac0e
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2011-08-18 17:51:26 +0000

    Deleted debug noise.

--- trunk/boost/numeric/mtl/operation/lower_trisolve.hpp
+++ trunk/boost/numeric/mtl/operation/lower_trisolve.hpp
@@ -87,18 +87,13 @@ namespace detail {
 	    using namespace tag; 
 	    a_cur_type ac= begin<row>(A), aend= end<row>(A); 
 	    for (size_type r= 0; ac != aend; ++r, ++ac) {
-		// std::cout << "row " << r << '\n'; 
 		a_icur_type aic= begin<nz>(ac), aiend= CompactStorage ? end<nz>(ac) : lower_bound<nz>(ac, r);
 		typename Collection<VectorOut>::value_type rr= v[r];
-		// std::cout << "rr[init] " << rr << '\n'; 
 		for (; aic != aiend; ++aic) {
 		    MTL_DEBUG_THROW_IF(col_a(*aic) >= r, logic_error("Matrix entries must be sorted for this."));
 		    rr-= value_a(*aic) * w[col_a(*aic)];
-		    // std::cout << "dec " << value_a(*aic) << " * " << w[col_a(*aic)] << '\n';
-		    // std::cout << "rr " << rr << '\n';
 		}
 		w[r]= rr;
-		// std::cout << "w[r] " << w[r] << '\n'; 
 	    }
 	}
 
@@ -188,135 +183,6 @@ namespace detail {
 	typename mtl::traits::row<Matrix>::type          row_a;
     };
 
-
-#if 0 // =============================
-    template <typename Value>
-    Value inline lower_trisolve_diavalue(const Value& v, tag::regular_diagonal)
-    {
-	using math::reciprocal;
-	return reciprocal(v);
-    }
-
-    template <typename Value>
-    Value inline lower_trisolve_diavalue(const Value& v, tag::inverse_diagonal)
-    {
-	return v;
-    }    
-
-    template <typename Matrix, typename VectorIn, typename VectorOut>
-    inline void lower_trisolve(const Matrix& A, const VectorIn& v, VectorOut& w, tag::row_major, tag::unit_diagonal)
-    {
-	vampir_trace<5022> tracer;
-	namespace traits = mtl::traits;
-	using namespace tag; using traits::range_generator; using mtl::detail::adjust_cursor;
-
-	typedef typename Collection<Matrix>::value_type           value_type;
-	typedef typename Collection<Matrix>::size_type            size_type;
-	typedef typename range_generator<row, Matrix>::type       a_cur_type;    
-	typedef typename range_generator<nz, a_cur_type>::type    a_icur_type;            
-	typename traits::col<Matrix>::type                        col_a(A); 
-	typename traits::const_value<Matrix>::type                value_a(A); 
-
-	w= v;
-	a_cur_type ac= begin<row>(A), aend= end<row>(A);
-	++ac;
-	for (size_type r= 1; ac != aend; ++r, ++ac) {
-	    a_icur_type aic= begin<nz>(ac), aiend= lower_bound<nz>(ac, r);
-	    typename Collection<VectorOut>::value_type rr= w[r];
-
-	    for (; aic != aiend; ++aic) {
-		MTL_DEBUG_THROW_IF(col_a(*aic) >= r, logic_error("Matrix entries must be sorted for this."));
-		rr-= value_a(*aic) * w[col_a(*aic)];
-	    }
-	    w[r]= rr;
-	}
-    }	
-
-
-    template <typename Matrix, typename VectorIn, typename VectorOut, typename DiaTag>
-    inline void lower_trisolve(const Matrix& A, const VectorIn& v, VectorOut& w, tag::row_major,
-				 DiaTag)
-    {
-	vampir_trace<5022> tracer;
-	namespace traits = mtl::traits;
-	using namespace tag; using traits::range_generator; using math::one; using mtl::detail::adjust_cursor;
-
-	typedef typename Collection<Matrix>::value_type           value_type;
-	typedef typename range_generator<row, Matrix>::type       a_cur_type;    
-	typedef typename range_generator<nz, a_cur_type>::type    a_icur_type;            
-	typename traits::col<Matrix>::type                        col_a(A); 
-	typename traits::const_value<Matrix>::type                value_a(A); 
-
-	w= v;
-	a_cur_type ac= begin<row>(A), aend= end<row>(A); 
-	for (typename Collection<Matrix>::size_type r= 0; ac != aend; ++r, ++ac) {
-	    a_icur_type aic= begin<nz>(ac), aiend= lower_bound<nz>(ac, r+1);
-	    MTL_THROW_IF(aic == aiend, missing_diagonal());
-	    --aiend;
-	    MTL_THROW_IF(col_a(*aiend) != r, missing_diagonal());
-
-	    value_type dia= value_a(*aiend);
-	    typename Collection<VectorOut>::value_type rr= w[r];
-
-	    for (; aic != aiend; ++aic) {
-		MTL_DEBUG_THROW_IF(col_a(*aic) >= r, logic_error("Matrix entries must be sorted for this."));
-		rr-= value_a(*aic) * w[col_a(*aic)];
-	    }
-	    w[r]= rr * lower_trisolve_diavalue(dia, DiaTag());
-	}
-    }	
-
-
-    template <typename Matrix, typename VectorIn, typename VectorOut>
-    inline void lower_trisolve(const Matrix& A, const VectorIn& v, VectorOut& w, tag::col_major, tag::unit_diagonal)
-    {
-	vampir_trace<5022> tracer;
-	using namespace tag; using mtl::traits::range_generator; using mtl::detail::adjust_cursor;
-
-	typedef typename range_generator<col, Matrix>::type       a_cur_type;    
-	typedef typename range_generator<nz, a_cur_type>::type    a_icur_type;            
-	typename mtl::traits::row<Matrix>::type                        row_a(A); 
-	typename mtl::traits::const_value<Matrix>::type                value_a(A); 
-
-	w= v;
-	a_cur_type ac= begin<col>(A), aend= end<col>(A); 
-	for (typename Collection<Matrix>::size_type r= 0; ac != aend; ++r, ++ac) {
-	    a_icur_type aic= lower_bound<nz>(ac, r+1), aiend= end<nz>(ac);
-	    typename Collection<VectorOut>::value_type rr= w[r];
-
-	    for (; aic != aiend; ++aic) {
-		MTL_DEBUG_THROW_IF(row_a(*aic) <= r, logic_error("Matrix entries must be sorted for this."));
-		w[row_a(*aic)]-= value_a(*aic) * rr;
-	    }
-	}
-    }
-
-    template <typename Matrix, typename VectorIn, typename VectorOut, typename DiaTag>
-    inline void lower_trisolve(const Matrix& A, const VectorIn& v, VectorOut& w, tag::col_major, DiaTag)
-    {
-	vampir_trace<5022> tracer;
-	using namespace tag; using mtl::traits::range_generator; using mtl::detail::adjust_cursor;
-
-	typedef typename range_generator<col, Matrix>::type       a_cur_type;    
-	typedef typename range_generator<nz, a_cur_type>::type    a_icur_type;            
-	typename mtl::traits::row<Matrix>::type                        row_a(A); 
-	typename mtl::traits::const_value<Matrix>::type                value_a(A); 
-
-	w= v;
-	a_cur_type ac= begin<col>(A), aend= end<col>(A); 
-	for (typename Collection<Matrix>::size_type r= 0; ac != aend; ++r, ++ac) {
-	    a_icur_type aic= lower_bound<nz>(ac, r), aiend= end<nz>(ac);
-	    MTL_DEBUG_THROW_IF(aic == aiend || row_a(*aic) != r, missing_diagonal());
-	    typename Collection<VectorOut>::value_type rr= w[r]*= lower_trisolve_diavalue(value_a(*aic), DiaTag());
-
-	    for (++aic; aic != aiend; ++aic) {
-		MTL_DEBUG_THROW_IF(row_a(*aic) <= r, logic_error("Matrix entries must be sorted for this."));
-		w[row_a(*aic)]-= value_a(*aic) * rr;
-	    }
-	}
-    }
-#endif
-
 }  // detail
 
 
@@ -389,8 +255,6 @@ inline void lower_trisolve(const Matrix& A, const VectorIn& v, VectorOut& w, Dia
     detail::lower_trisolve_t<Matrix, DiaTag> solver(A); 
     solver(v, w);
 }
-
-
 
 }} // namespace mtl::matrix
 
