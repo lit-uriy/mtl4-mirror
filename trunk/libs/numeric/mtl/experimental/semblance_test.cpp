c059d0c3b97b236507a3c0a88eca5c75a8c86846
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2011-11-20 01:40:10 +0000

    Semblance test.

---
+++ trunk/libs/numeric/mtl/experimental/semblance_test.cpp
@@ -0,0 +1,66 @@
+// Software License for MTL
+// 
+// Copyright (c) 2007 The Trustees of Indiana University.
+//               2008 Dresden University of Technology and the Trustees of Indiana University.
+//               2010 SimuNova UG (haftungsbeschrÃ¤nkt), www.simunova.com.
+// All rights reserved.
+// Authors: Peter Gottschling and Andrew Lumsdaine
+// 
+// This file is part of the Matrix Template Library
+// 
+// See also license.mtl.txt in the distribution.
+
+#include <iostream>
+#include <boost/numeric/mtl/mtl.hpp>
+
+using namespace std;
+using namespace mtl;
+
+template <typename Matrix>
+void init_data(Matrix& T)
+{
+    for (unsigned i= 0; i < num_rows(T); i++)
+	for (unsigned j= 0; j < num_cols(T); j++)
+	    T[i][j]= i + j + 2; // add 2 because i and j are zero-based
+}
+
+template <typename Matrix, typename Vector>
+void w_semlance(const Matrix& T, Vector& semb, int nwin, int lwin, int linc)
+{
+    typedef dense_vector<float, mtl::vector::parameters<row_major> > rtype;
+    int nsamp= num_rows(T), ntr= num_cols(T);
+    dense_vector<double> sumsqr(nsamp), sqrsum(nsamp);
+
+    for (int i= 0; i < nsamp; i++) {
+	const rtype& rowi= T[i][iall];
+	sumsqr[i]= sum(rtype(ele_prod(rowi, rowi)));  // rtype won't be needed here in future
+	sqrsum[i]= square(sum(rowi));
+    }
+    
+    for (int i= 1, ll= 0; i <= nwin; i++, ll+= linc) {
+	int kkend= ll + kkend > nsamp ? nsamp - ll : lwin;
+	double sumsq= sum(sumsqr[irange(ll, kkend)]),
+	       sumampsq= sum(sqrsum[irange(ll, kkend)]),
+	       value= ntr * sumsq;
+	semb[i]= value != 0.0 ? sumampsq / value : 0.0;
+    }
+}
+
+
+int main() 
+{
+    //int nsamples= 30001, ntrc= 2000;
+    int nsamples= 8, ntrc= 6;
+
+    dense2D<float> traces(nsamples, ntrc);
+    init_data(traces);
+    // cout << "T is\n" << traces;
+
+    dense_vector<double> semb(nsamples/2 + 1, 0.0);
+    int lwin= 5, linc= 2, nwin= size(semb) - linc;
+    w_semlance(traces, semb, nwin, lwin, linc);
+    
+    cout << "Semblance is " << semb << '\n';
+
+    return 0;
+}
