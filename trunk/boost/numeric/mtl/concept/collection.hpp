7ea167f1d3209a417fb22a757f7320750c851885
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2012-06-29 14:13:29 +0000

    Collection of imag_view \& real_view.

--- trunk/boost/numeric/mtl/concept/collection.hpp
+++ trunk/boost/numeric/mtl/concept/collection.hpp
@@ -526,6 +526,28 @@ namespace mtl {
 #endif
 
 #ifdef __GXX_CONCEPTS__
+#else
+    template <typename Coll>
+    struct Collection<vector::real_view<Coll> >
+    {
+	typedef typename vector::real_view<Coll>::value_type        value_type;
+	typedef typename vector::real_view<Coll>::const_reference   const_reference;
+	typedef typename vector::real_view<Coll>::size_type         size_type;
+    };
+#endif
+
+#ifdef __GXX_CONCEPTS__
+#else
+    template <typename Coll>
+    struct Collection<vector::imag_view<Coll> >
+    {
+	typedef typename vector::imag_view<Coll>::value_type        value_type;
+	typedef typename vector::imag_view<Coll>::const_reference   const_reference;
+	typedef typename vector::imag_view<Coll>::size_type         size_type;
+    };
+#endif
+
+#ifdef __GXX_CONCEPTS__
     template <typename Coll>
     concept_map Collection<vector::negate_view<Coll> >
     {
