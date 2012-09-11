7c3338615827c80a7b2ba9e661760a43894a6ba7
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2012-09-11 11:58:09 +0000

    Element access in sparse banded matrix, details elsewhere.

--- trunk/boost/numeric/itl/krylov/bicgstab_2.hpp
+++ trunk/boost/numeric/itl/krylov/bicgstab_2.hpp
@@ -27,7 +27,7 @@
 
 namespace itl {
 
-#if 1
+#if 0
 /// Bi-Conjugate Gradient Stabilized(2)
 template < typename LinearOperator, typename Vector, 
 	   typename Preconditioner, typename Iteration >
@@ -117,73 +117,8 @@ int bicgstab_2(const LinearOperator &A, Vector &x, const Vector &b,
     return iter;
 }
 
-#endif
-
-#if 0
-
-/// Bi-Conjugate Gradient Stabilized(2)
-template < typename LinearOperator, typename Vector, 
-	   typename Preconditioner, typename Iteration >
-int bicgstab_2(const LinearOperator& A, Vector& x, const Vector& b,
-	       const Preconditioner& L, Iteration& iter)
-{
-    typedef typename mtl::Collection<Vector>::value_type Scalar;
-    const Scalar zero= math::zero(Scalar()), one= math::one(Scalar());
-    Scalar     alpha(zero), beta, gamma, mu, nu, rho_0(one), rho_1, tau, omega_1, omega_2(one);
-    Vector     r(b - A * x), r_0(r), r_i(r), x_i(x), p(resource(x)), v_tilde(resource(x)),
-	       s(resource(x)), t(resource(x)), u(resource(x), zero), v(resource(x)), w(resource(x));
-
-    if (size(b) == 0) throw mtl::logic_error("empty rhs vector");
-    while ( ! iter.finished(r)) {
-	++iter;
-	rho_0*= -omega_2;
-	// z= solve(M, r); z_tilde= solve(M, r_tilde); ???
-
-	r_i= solve(L, r_0); // pg
-
-	rho_1= dot(r_0, r_i);       // or rho_1= dot(z, r_tilde) ???
-	beta= alpha * rho_1 / rho_0; rho_0= rho_1;
-	u= r_i - beta * u;
-	v= A * u;
-	
-	v_tilde= solve(L, v);
-
-
-	gamma= dot(v_tilde, r_0); alpha= rho_0 / gamma;
-	r= r_i - alpha * v_tilde;
-	
-
-	s= A * r;
-	s_tilde= solve(L, s); // s === y
-
-	x= x_i + alpha * u;
-
-	rho_1= dot(r_0, s_tilde); beta= alpha * rho_1 / rho_0; rho_0= rho_1;
-	v= s - beta * v;
-	w= A * v;  // w === y
-	w_tilde= solve(L, w);
-	
-	gamma= dot(w_tilde, r_0); alpha= rho_0 / gamma;
-	u= r - beta * u;
-	r-= alpha * v;
-	s-= alpha * w;
-	t= A * s;
-	
-
-	omega_1= dot(r, s); mu= dot(s, s); nu= dot(s, t); tau= dot(t, t);
-	omega_2= dot(r, t); tau-= nu * nu / mu; omega_2= (omega_2 - nu * omega_1 / mu) / tau;
-	omega_1= (omega_1 - nu * omega_2) / mu;
-	x_i= x + omega_1 * r + omega_2 * s + alpha * u;
-	r_i= r - omega_1 * s - omega_2 * t;
-	u-= omega_1 * v + omega_2 * w;
-    }
-    return iter;
-}
-#endif
-
-
 //Vorschlag von Cornelius
-#if 0
+#else
 
 /// Bi-Conjugate Gradient Stabilized(2)
 template < typename LinearOperator, typename Vector, 
