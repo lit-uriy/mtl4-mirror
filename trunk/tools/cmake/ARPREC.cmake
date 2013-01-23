431cf207dd57d8f9b2b6ca1a303cf5e6c4497271
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2013-01-23 16:53:39 +0000

    With hints.

--- trunk/tools/cmake/ARPREC.cmake
+++ trunk/tools/cmake/ARPREC.cmake
@@ -1,5 +1,5 @@
-find_file(MP_REAL_H "arprec/mp_real.h" DOC "The arprec header mp_real.h")
-find_library(ARPREC_LIBRARY arprec DOC "the arprec library arprec")
+find_file(MP_REAL_H "arprec/mp_real.h" PATHS $ENV{ARPREC_HEADER_HINT} DOC "The arprec header mp_real.h; you can provide the path of it in the environment variable ARPREC_HEADER_HINT.")
+find_library(ARPREC_LIBRARY arprec PATHS $ENV{ARPREC_LIBRARY_HINT} DOC "the arprec library arprec; you can provide the path of it in the environment variable ARPREC_LIBRARY_HINT.")
 set(HAVE_ARPREC false)
 if(MP_REAL_H AND ARPREC_LIBRARY)
 	get_filename_component(ARPREC_INCLUDE_DIRS ${MP_REAL_H} PATH)
