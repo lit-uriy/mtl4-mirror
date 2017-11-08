badcfdbfa65daed0decbfd40bc4e5960272c0ead
pgottsch <pgottsch@df6d15db-99ea-0310-92f0-9d27c566f14b>
2017-11-08 15:16:22 +0000

    Vector permute function with test.

---
+++ libs/numeric/mtl/test/permute_test.cpp
@@ -0,0 +1,35 @@
+// Software License for MTL
+// 
+// Copyright (c) 2007 The Trustees of Indiana University. 
+//               2008 Dresden University of Technology and the Trustees of Indiana University.
+//               2010 SimuNova UG, www.simunova.com. 
+// All rights reserved.
+// Authors: Peter Gottschling and Andrew Lumsdaine
+// 
+// This file is part of the Matrix Template Library
+// 
+// See also tools/license/license.mtl.txt in the distribution.
+
+#include <boost/numeric/mtl/mtl.hpp>
+#include <boost/numeric/mtl/operation/permute.hpp>
+
+using namespace std;
+
+
+int main(int, char**)
+{
+    using namespace mtl;
+
+    dense_vector<int> v(4), p(4), ref(4);
+    v = 7, 9, 10, 11;
+    cout << "v = " << v << endl;
+    p = 3, 1, 2, 0;
+    cout << "p = " << p << endl;
+    ref = 11, 9, 10, 7;
+    
+    dense_vector<int> vp = permute(p, v);
+    cout << "vp = " << vp << endl;
+    MTL_THROW_IF(vp != ref, mtl::runtime_error("Wrong value after permute"));
+    
+    return 0;
+}
